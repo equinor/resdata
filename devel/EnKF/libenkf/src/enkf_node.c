d195e68329baa5555939aee5e9e1d42b97ad95cb
a152128 <a152128@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2007-04-10 07:45:05 +0000

    Have added simple enkf_node
    git-svn-id: http://ert.nr.no/repos/ert@242 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/EnKF/libenkf/src/enkf_node.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdlib.h>
+#include <enkf_node.h>
+
+
+struct enkf_node_struct {
+};
+
+
+
---
+++ devel/EnKF/libenkf/src/enkf_node.h
@@ -0,0 +1,7 @@
+#ifndef __ENKF_NODE_H__
+#define __ENKF_NODE_H__
+
+
+typedef struct enkf_node_struct enkf_node_type;
+
+#endif
