1aeeff0360efdc043f8915cf787fd6d3194551cf
a152128 <a152128@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2007-04-18 14:26:50 +0000

    Added enkf_util in libenkf
    git-svn-id: http://ert.nr.no/repos/ert@256 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/EnKF/libenkf/src/enkf_util.c
@@ -0,0 +1,76 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <enkf_util.h>
+
+
+
+void * enkf_util_malloc(int byte_size , const char * caller) {
+  void *tmp = malloc(byte_size);
+  if (tmp == NULL) {
+    fprintf(stderr,"%s: failed to allocate %d bytes - aborting \n",caller , byte_size);
+    abort();
+  }
+  return tmp;
+}
+
+
+void * enkf_util_realloc(void *ptr , int byte_size , const char * caller) {
+  void *tmp = realloc(ptr , byte_size);
+  if (tmp == NULL) {
+    fprintf(stderr,"%s: failed to allocate %d bytes - aborting \n",caller , byte_size);
+    abort();
+  }
+  return tmp;
+}
+
+
+
+static FILE * enkf_util_fopen(const char * filename , const char * mode , const char * text_mode , const char * caller) {
+  FILE *stream = fopen(filename , mode);
+  if (stream == NULL) {
+    fprintf(stderr,"%s: failed to open:%s for %s - aborting \n",caller , filename , text_mode);
+    abort();
+  }
+  return stream;
+}
+  
+FILE * enkf_util_fopen_w(const char * filename , const char * caller) {
+  return enkf_util_fopen(filename , "w" , "writing" , caller);
+}
+
+FILE * enkf_util_fopen_r(const char * filename , const char * caller) {
+  return enkf_util_fopen(filename , "r" , "reading" , caller);
+}
+
+
+void enkf_util_fwrite(const void *ptr , int item_size, int items , FILE *stream , const char * caller) {
+  if (fwrite(ptr , item_size , items , stream) != items) {
+    fprintf(stderr,"%s: failed to write : %d bytes - aborting \n",caller , (item_size * items));
+    abort();
+  }
+}
+
+void enkf_util_fread(void *ptr , int item_size, int items , FILE *stream , const char * caller) {
+  if (fread(ptr , item_size , items , stream) != items) {
+    fprintf(stderr,"%s: failed to read : %d bytes - aborting \n",caller , (item_size * items));
+    abort();
+  }
+}
+
+
+/*****************************************************************/
+
+static void enkf_util_rand_dbl(int N , double max , double *R) {
+  int i;
+  for (i=0; i < N; i++) 
+    R[i] = rand() * max / RAND_MAX;
+}
+
+
+double enkf_util_rand_normal(double mean , double std) {
+  const double pi = 3.141592653589;
+  double R[2];
+  enkf_util_rand_dbl(2 , 1.0 , R);
+  return mean + std * sqrt(-2.0 * log(R[0])) * cos(2.0 * pi * R[1]);
+}
---
+++ devel/EnKF/libenkf/src/enkf_util.h
@@ -0,0 +1,159 @@
+#ifndef __ENKF_UTIL_H__
+#define __ENKF_UTIL_H__
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum {parameter , ecl_restart , ecl_summary , ecl_static} enkf_var_type;
+
+#define CONFIG_STD_FIELDS int size; const char * ecl_kw_name; enkf_var_type var_type;
+
+/*****************************************************************/
+
+#define VOID_FUNC_CONST(func,type) \
+void func ## __(const void *void_arg) { \
+   const type *arg = (const type *) void_arg; \
+   func(arg);                             \
+}
+
+
+#define VOID_FUNC(func,type) \
+void func ## __(void *void_arg) { \
+   type *arg = (type *) void_arg; \
+   func(arg);                     \
+}
+
+#define VOID_SERIALIZE(func , type)     \
+void func ## __(const void *void_arg, double *serial_data , size_t *_offset) { \
+   type *arg = (type *) void_arg;       \
+   func(arg , serial_data , _offset);   \
+}
+
+
+#define VOID_SCALE_FUNC(func , type) \
+void func ## __(void *void_arg, double scale_factor) {    \
+   type *arg = (type *) void_arg;                         \
+ func(arg , scale_factor);                                \
+}
+
+#define VOID_ADD_FUNC(func , type) \
+void func ## __(void *void_arg, const void *void_delta) {   \
+   type *arg   = (type *) void_arg;                         \
+   type *delta = (type *) void_delta;                       \
+   func(arg , delta);                                       \
+}
+
+
+#define GET_SIZE_FUNC(type)        int type ## _get_size (const type ## _type *arg) { return arg->size; }
+#define GET_SIZE_FUNC_HEADER(type) int type ## _get_size (const type ## _type *)
+
+
+#define VOID_FUNC_HEADER_CONST(func) void func ## __(const void *void_arg)
+#define VOID_FUNC_HEADER(func) void func ## __(void *void_arg)
+#define VOID_SERIALIZE_HEADER(func) void func ## __(const void *void_arg , double *, size_t *)
+#define VOID_SCALE_FUNC_HEADER(func) void func ## __(void *, double )
+#define VOID_ADD_FUNC_HEADER(func) void func ## __(void *, const void *) 
+
+/*****************************************************************/
+
+#define SQRT_FUNC(prefix)                           \
+void prefix ## _isqrt(void * void_arg) {            \
+prefix ## _type *arg = (prefix ## _type *) void_arg;\
+const prefix ## _config_type *config = arg->config; \
+const int size = config->size;                      \
+int i;                                              \
+for (i=0; i < size; i++)                            \
+ arg->data[i] = sqrt(arg->data[i]);                 \
+}
+#define SQRT_FUNC_HEADER(prefix) void prefix ## _isqrt(void * )
+
+/*****************************************************************/
+
+#define SCALE_FUNC(prefix)                                                 \
+void prefix ## _iscale(void *void_arg , double scale_factor) {             \
+prefix ## _type *arg = (prefix ## _type *) void_arg;                       \
+const prefix ## _config_type *config = arg->config; 			   \
+const int size = config->size;                      			   \
+int i;                                              			   \
+for (i=0; i < size; i++)                            			   \
+ arg->data[i] *= scale_factor;                  			   \
+}
+#define SCALE_FUNC_HEADER(prefix) void prefix ## _iscale(void * , double)
+
+/*****************************************************************/
+
+#define RESET_FUNC(prefix)                                                 \
+void prefix ## _ireset(void *void_arg) {             \
+prefix ## _type *arg = (prefix ## _type *) void_arg;                       \
+const prefix ## _config_type *config = arg->config; 			   \
+const int size = config->size;                      			   \
+int i;                                              			   \
+for (i=0; i < size; i++)                            			   \
+ arg->data[i] = 0;                               			   \
+}
+#define RESET_FUNC_HEADER(prefix) void prefix ## _ireset(void *)
+
+/*****************************************************************/
+
+#define ADD_FUNC(prefix)                                                       \
+void prefix ## _iadd(void *void_arg , const void *void_delta) {                \
+      prefix ## _type *arg   = (prefix ## _type *)       void_arg;  	       \
+const prefix ## _type *delta = (const prefix ## _type *) void_delta;	       \
+const prefix ## _config_type *config = arg->config; 			       \
+const int size = config->size;                      			       \
+int i;                                              			       \
+if (config != delta->config) {                                                 \
+    fprintf(stderr,"%s:two multz object have different config objects - aborting \n",__func__);\
+    abort();                                                                   \
+}                                                                              \
+for (i=0; i < size; i++)                            			       \
+ arg->data[i] += delta->data[i];                                               \
+}
+#define ADD_FUNC_HEADER(prefix) void prefix ## _iadd(void * , const void *)
+/*****************************************************************/
+
+#define SUB_FUNC(prefix)                                                       \
+void prefix ## _isub(void *void_arg , const void *void_diff) {                \
+      prefix ## _type *arg   = (prefix ## _type *)       void_arg;  	       \
+const prefix ## _type *diff = (const prefix ## _type *) void_diff;	       \
+const prefix ## _config_type *config = arg->config; 			       \
+const int size = config->size;                      			       \
+int i;                                              			       \
+if (config != diff->config) {                                                 \
+    fprintf(stderr,"%s:two multz object have different config objects - aborting \n",__func__);\
+    abort();                                                                   \
+}                                                                              \
+for (i=0; i < size; i++)                            			       \
+ arg->data[i] -= diff->data[i];                                               \
+}
+#define SUB_FUNC_HEADER(prefix) void prefix ## _isub(void * , const void *)
+/*****************************************************************/
+
+
+#define MATH_OPS(prefix) \
+SQRT_FUNC (prefix) \
+SCALE_FUNC(prefix) \
+ADD_FUNC  (prefix) \
+SUB_FUNC  (prefix) \
+RESET_FUNC(prefix) 
+
+#define MATH_OPS_HEADER(prefix) \
+SQRT_FUNC_HEADER (prefix);  \
+SCALE_FUNC_HEADER(prefix); \
+ADD_FUNC_HEADER  (prefix); \
+SUB_FUNC_HEADER  (prefix); \
+RESET_FUNC_HEADER(prefix);
+
+/*****************************************************************/
+
+
+void * enkf_util_malloc (int , const char * );
+void * enkf_util_realloc(void * , int , const char * );
+FILE * enkf_util_fopen_r(const char * , const char * );
+FILE * enkf_util_fopen_w(const char * , const char * );
+void   enkf_util_fwrite(const void *, int , int , FILE *, const char * );
+void   enkf_util_fread ( void *, int , int , FILE *, const char * );
+
+double enkf_util_rand_normal(double , double );
+
+
+#endif
