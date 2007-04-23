630d3d70efa77f6d4f41c81607bcad224235e659
a152128 <a152128@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2007-04-23 13:09:39 +0000

    Have added some documentation of the matlab functions
    git-svn-id: http://ert.nr.no/repos/ert@258 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/EnKF/matlab/src/diag_plot.m
+++ devel/EnKF/matlab/src/diag_plot.m
@@ -1,7 +1,48 @@
 function diag_plot(path , VAR , NList , out_path, in_device)
+%The diag_plot() function can be used to plot time-series of scalar 
+%enkf variables like fault multipliers and the WOC. 
+%
+% o Prior to calling diag_plot() yous *must* use the EnKF program
+%   to extract the mean and standard deviation of the ensemble. 
+%   This is done by using menu option 11-b and then analyse the
+%   ensaveA and ensstdA files. 
+%
+%   The diag_plot() function currently *only* considers the analyzed
+%   files.
+%
+% o The function has five arguments, only the three first are
+%   mandatory:
+%
+%   [path:] This is the path to where the diagnostica variable from
+%           EnKF are stored. If you have run EnKF in the directory
+%  
+%                /path/to/EnKF_model/A
+%            
+%           you should enter '/path/to/EnKF_model/A/Diag' as first 
+%           argument.
+%
+%   [VAR]: This is the name of the variable you want to plot, it is
+%          typically 'multz', 'WOC' or 'multflt'.
+%
+%   [NList]: The scalar parameters are typically 'vectors' - i.e. 
+%            are for instance many fault multipliers. The NList 
+%            argument is to designate which (e.g.) fault multipliers
+%            to plot. For instance to fault multpliers 1 and 3 use 
+%            [1,3] as NLIst argument.
+%
+%   The two last arguments are for generating hard copies:
+%
+%   [out_path]: Path were hard copies are stored.
+%
+%   [in_device]: Type of files to plot, e.g. 'png', 'eps',..., defaults
+%                to 'png'.
+
+
+
     def_device = 'png';
-    aveFile = strcat(path , '\' , VAR , 'aveA.dat');
-    stdFile = strcat(path , '\' , VAR , 'stdA.dat');
+    sep = filesep;
+    aveFile = strcat(path , sep , VAR , 'aveA.dat');
+    stdFile = strcat(path , sep , VAR , 'stdA.dat');
     ave = load(aveFile);
     std = load(stdFile);
     days = ave(:,3);
@@ -18,7 +59,7 @@ function diag_plot(path , VAR , NList , out_path, in_device)
                 out_path = path;
             end
             if out_path ~= 0,
-                out_file = sprintf('%s\\%s-%d',out_path , VAR , N);
+                out_file = sprintf('%s%s%s-%d',out_path , sep , VAR , N);
                 if nargin == 4,
                     device = def_device;
                 else
--- devel/EnKF/matlab/src/ens_plot.m
+++ devel/EnKF/matlab/src/ens_plot.m
@@ -1,9 +1,29 @@
-function ens_plot(prior_path , posterior_path , well_list , var_list , unit_list , out_path , device)
+function ens_plot(prior_path , posterior_path , well_list , var_list , unit_list , out_path , in_device)
+% The ens_plot() function is designed to plot the results of ensemble
+% experiments. Before calling ens_plot() you must use the EnKF 11-e
+% option to generate ensemble plot files (say 'n' to Tecplot header).
+% 
+%  prior_path    : Path to plot files from the prior ensemble.
+% 
+%  posterior_path: Path to plot files from the posterior ensemble.
+% 
+%  well_list     : A list of the wells you want to consider, the format 
+%                  for this variable is: {'Well1', 'Well2'}.
+% 
+%  var_list      : A list of variables you want to consider, {'WGOR','WOPR'}.
+% 
+%  unit_list     : The units corresponding to the variables in var_list.
+% 
+% The variables out_path and in_device are for saving to file, they are optional, see
+% the documentation of diag_plot() for details.
+
+
    def_device = 'png';
    lw        = 1.00;
    psize     = 2.00;
    nwell     = max(size(well_list));
    nvar      = max(size(var_list));
+   sep       = filesep;
    
    fig_nr = 0;
    for iw=1:nwell,
@@ -12,8 +32,8 @@ function ens_plot(prior_path , posterior_path , well_list , var_list , unit_list
            var  = char(var_list(ivar));
            unit = char(unit_list(ivar));
            fig_nr = fig_nr + 1;
-           prior_file     = strcat(prior_path , '\' , well , '.' , var);
-           posterior_file = strcat(posterior_path , '\' , well , '.' , var);
+           prior_file     = strcat(prior_path , sep , well , '.' , var);
+           posterior_file = strcat(posterior_path , sep , well , '.' , var);
            prior     = load(prior_file);
            posterior = load(posterior_file);
            prior_size     = size(prior , 2) - 2;
@@ -43,7 +63,7 @@ function ens_plot(prior_path , posterior_path , well_list , var_list , unit_list
            
            if nargin >= 6,
                if out_path ~= 0,
-                   out_file = sprintf('%s\\%s-%s',out_path , well , var);
+                   out_file = sprintf('%s%s%s-%s',out_path , sep , well , var);
                    if nargin == 6,
                        device = def_device;
                    else
