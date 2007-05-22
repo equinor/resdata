0a43f542a70cbdaede23e3d2b975598e8d9e2593
a152128 <a152128@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2007-05-22 08:03:37 +0000

    Added (simple) tstep functionality
    git-svn-id: http://ert.nr.no/repos/ert@307 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/EnKF/libsched/src/sched_kw_tstep.h
@@ -0,0 +1,14 @@
+#ifndef __SCHED_KW_TSTEP__
+#define __SCHED_KW_TSTEP__
+#include <hash.h>
+
+typedef struct sched_kw_tstep_struct sched_kw_tstep_type;
+
+void                  sched_kw_tstep_fprintf(const sched_kw_tstep_type *, FILE *, int , time_t , bool *);
+void                  sched_kw_tstep_add_line(sched_kw_tstep_type *, const char * , bool *);
+sched_kw_tstep_type * sched_kw_tstep_alloc(int * , double *);
+void                  sched_kw_tstep_free(sched_kw_tstep_type * );
+sched_kw_tstep_type * sched_kw_tstep_fread_alloc(int *  , double * , int , time_t , FILE * , bool *);
+void                  sched_kw_tstep_fwrite(const sched_kw_tstep_type * , FILE *);
+void                  sched_kw_tstep_iterate_current(const sched_kw_tstep_type * , int *);
+#endif
