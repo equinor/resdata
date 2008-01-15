083547291221bf120eba79ed5074282d98c2df27
a152128 <a152128@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-01-15 12:49:34 +0000

    ....
    git-svn-id: http://ert.nr.no/repos/ert@501 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/EnKF/libutil/src/msg.c
@@ -0,0 +1,119 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <util.h>
+#include <stdbool.h>
+#include <msg.h>
+
+
+struct msg_struct {
+  char * prompt;
+  char * msg;
+  int    msg_len;
+  bool   visible;
+};
+
+
+static void __msg_assert_visible(const msg_type * msg) {
+  if (!msg->visible) {
+    fprintf(stderr,"%s: you must call msg_show() first - aborting\n",__func__);
+    abort();
+  }
+}
+
+
+static void __blank_string(int len) {
+    int i;
+  for (i = 0; i < len; i++)
+    fputc('\b' , stdout);
+  for (i = 0; i < len; i++)
+    fputc(' ' , stdout);
+  for (i = 0; i < len; i++)
+    fputc('\b' , stdout);
+}
+
+
+void msg_clear_msg(msg_type * msg) {
+  __msg_assert_visible(msg);
+  __blank_string(msg->msg_len);
+  if (msg->msg != NULL) {
+    free(msg->msg);
+    msg->msg_len = 0;
+    msg->msg     = NULL;
+  }
+}
+  
+
+
+static void msg_clear_prompt(const msg_type * msg) {
+  __blank_string(strlen(msg->prompt));
+}
+
+
+void msg_hide(msg_type * msg) {
+  msg_clear_msg(msg);
+  msg_clear_prompt(msg);
+  msg->visible = false;
+}
+
+
+void msg_set_prompt(msg_type * msg , const char * prompt) {
+  msg->prompt = util_realloc_string_copy(msg->prompt , prompt);
+}
+
+
+void msg_print_msg(const msg_type * msg) {
+  if (msg->msg != NULL) {
+    printf("%s" , msg->msg);
+    fflush(stdout);
+  }
+}
+
+
+void msg_show(msg_type * msg) {
+  if (!msg->visible) {
+    printf("%s" , msg->prompt);
+    msg_print_msg(msg);
+    msg->visible = true;
+  }
+}
+
+
+void msg_update(msg_type * msg , const char * new_msg) {
+  __msg_assert_visible(msg);
+  msg_clear_msg(msg);
+  msg->msg = util_realloc_string_copy(msg->msg , new_msg);
+  if (new_msg == NULL)
+    msg->msg_len = 0;
+  else
+    msg->msg_len = strlen(new_msg);
+  msg_print_msg(msg);
+}
+
+
+msg_type * msg_alloc(const char * prompt) {
+  msg_type * msg = malloc(sizeof * msg);
+  msg->prompt = util_alloc_string_copy(prompt);
+  
+  msg->msg     = NULL;
+  msg->msg_len = 0;
+  msg->visible = false;
+  return msg;
+}
+
+
+void msg_free(msg_type * msg , bool clear) {
+  if (clear) 
+    msg_hide(msg);
+  else 
+    printf("\n");
+
+  free(msg->prompt);
+  if (msg->msg != NULL)
+    free(msg->msg);
+  
+  free(msg);
+}
+  
+
+
---
+++ devel/EnKF/libutil/src/msg.h
@@ -0,0 +1,19 @@
+#ifndef __MSG_H__
+#define __MSG_H__
+
+#include <stdbool.h>
+
+
+typedef struct msg_struct msg_type;
+
+
+
+msg_type   * msg_alloc(const char *);
+void         msg_show(msg_type * );
+void         msg_free(msg_type *  , bool);
+void         msg_update(msg_type * , const char * );
+void         msg_hide(msg_type *);
+void         msg_clear_msg(msg_type * msg);
+
+
+#endif
