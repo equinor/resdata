a5b3f01a1ffb3ff17bf8c841acbfc7091a013486
a159251 <a159251@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-01-15 12:11:45 +0000

    jan_test
    git-svn-id: http://ert.nr.no/repos/ert@496 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/EnKF/libenkf/src/jan_atest.c
@@ -0,0 +1,14 @@
+#include <multz_config.h>
+#include <multz.h>
+
+
+
+int main () {
+  /*
+    int nx,ny,nz;
+
+    multz_config_type * config = multz_config_fscanf_alloc("Config_FILE" , nx , ny , nz);
+    multz_type * multz = multz_alloc(config);
+  */
+  multz_TEST();
+} 
