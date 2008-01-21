6e886aa256d937d3df404f888b3d6b02ac28623d
a152128 <a152128@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-01-21 09:06:40 +0000

    Added convert.c
    git-svn-id: http://ert.nr.no/repos/ert@518 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/EnKF/libecl/src/convert.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <ecl_kw.h>
+#include <fortio.h>
+#include <string.h>
+#include <util.h>
+#include <ecl_util.h>
+
+
+
+void file_convert(const char * src_file , const char * target_file) {
+  fortio_type *src , *target;
+  ecl_kw_type * ecl_kw;
+  bool formatted_src;
+
+  printf("Converting %s -> %s \n",src_file , target_file);
+  if (util_fmt_bit8(src_file)) {
+    printf("%s is formatted according to bit8  \n",src_file);
+    formatted_src = true;
+  }  else
+    formatted_src = false;
+
+  target = fortio_open(target_file , "w" , true);
+  src    = fortio_open(src_file , "r" , true);
+  ecl_kw = ecl_kw_fread_alloc(src , formatted_src);
+  if (ecl_kw == NULL) {
+    fprintf(stderr,"Loading: %s failed - maybe you forgot the header? \n", src_file);
+    abort();
+  }
+
+  while (ecl_kw != NULL) {
+    ecl_kw_set_fmt_file(ecl_kw , !formatted_src);
+    ecl_kw_fwrite(ecl_kw , target);
+    
+    ecl_kw_free(ecl_kw);
+    ecl_kw = ecl_kw_fread_alloc(src , formatted_src);
+  }
+  if (ecl_kw != NULL) ecl_kw_free(ecl_kw);
+  fortio_close(src);
+  fortio_close(target);
+}
+
+
+int main (int argc , char **argv) { 
+  char *src_file    = argv[1];
+  char *target_file;
+  
+  int           report_nr;
+  ecl_file_type file_type;
+  bool          fmt_file;
+  ecl_util_get_file_type(src_file , &file_type , &fmt_file , &report_nr);
+  
+  if (file_type == ecl_other_file) {
+    if (argc != 3) {
+      fprintf(stderr,"When the file can not be recognized on the name as an ECLIPSE file you must give output_file as second (and last) argument \n");
+      exit(0);
+    }
+    target_file = argv[2];
+    file_convert(src_file , target_file);
+  } else {
+    int file_nr;
+    for (file_nr = 1; file_nr < argc; file_nr++) {
+      char *path;
+      char *basename;
+      char *extension;
+      src_file    = argv[file_nr];
+      ecl_util_get_file_type(src_file , &file_type , &fmt_file , &report_nr);
+      if (file_type == ecl_other_file) {
+	fprintf(stderr,"File: %s - problem \n",src_file);
+	fprintf(stderr,"In a list of many files ALL must be recognizable by their name. \n");
+	abort();
+      }
+      util_alloc_file_components(src_file , &path , &basename , &extension);
+      
+      target_file = ecl_util_alloc_filename(path, basename , file_type , !fmt_file , report_nr);
+      file_convert(src_file , target_file);
+
+      free(path);
+      free(basename);
+      free(extension);
+      free(target_file);
+    }
+  }
+  return 0;
+}
