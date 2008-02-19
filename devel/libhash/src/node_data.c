046beb0812d8c8cae9cbb928353d95c581f82619
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-02-19 15:46:07 +0000

    Have started system for type-safety
    git-svn-id: http://ert.nr.no/repos/ert@618 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libhash/src/node_data.c
+++ devel/libhash/src/node_data.c
@@ -8,10 +8,14 @@ const void * node_data_copyc(const void *_src) {
   const node_data_type *src = (const node_data_type *) _src;
   node_data_type *new;
   new = malloc(sizeof *new);
+  new->ctype     = src->ctype;
   new->byte_size = src->byte_size;
   new->data = malloc(new->byte_size);
+  if (new->data == NULL) {
+    fprintf(stderr,"%s: allocation of %d bytes failed - aborting \n",__func__ , new->byte_size);
+    abort();
+  }
   memcpy(new->data , src->data , new->byte_size);
-
   return new;
 }
 
--- devel/libhash/src/node_data.h
+++ devel/libhash/src/node_data.h
@@ -4,9 +4,12 @@
 typedef const void * (  copyc_type) (const void *);
 typedef void         (  del_type)   (void *);
 
+typedef enum  {__buffer_value = 1, __int_value = 2, __double_value = 3, __float_value = 4 , __char_value = 5 , __bool_value = 6 , __size_t_value = 7} node_data_ctype;
+
 typedef struct {
-  int    byte_size;
-  void  *data;
+  node_data_ctype   ctype;
+  int               byte_size;
+  void             *data;
 } node_data_type;
 
 
