c7158f35cabf41ea46f9eb09906985547e840e58
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-04-02 08:11:09 +0000

    GG
    git-svn-id: http://ert.nr.no/repos/ert@707 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libhash/src/set.c
+++ devel/libhash/src/set.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <set.h>
+#include <string.h>
 
 
 struct set_struct {
@@ -36,8 +37,13 @@ bool set_add_key(set_type * set, const char * key) {
   }
 }
 
+bool set_has_key(set_type * set, const char * key) {
+  return hash_has_key(set->key_hash, key);
+}
+
 
 int set_get_size(const set_type *set) { return hash_get_size(set->key_hash); }
+
 
 char ** set_alloc_keylist(const set_type * set) { return hash_alloc_keylist(set->key_hash); }
 
@@ -50,7 +56,59 @@ void set_free(set_type * set) {
 
 
 
+/* Wrong(??) depenendency between libutil and libhash ... */
+static void __fwrite_string(const char * s, FILE *stream) {
+  int len = 0;
+  if (s != NULL) {
+    len = strlen(s);
+    fwrite(&len , sizeof len , 1       , stream);
+    fwrite(s    , 1          , len + 1 , stream);
+  } else
+    fwrite(&len , sizeof len , 1       , stream);
+}
+
+
+static char * __fread_alloc_string(FILE *stream) {
+  int len;
+  char *s = NULL;
+  fread(&len , sizeof len , 1 , stream );
+  if (len > 0) {
+    s = malloc(len + 1 );
+    fread(s , 1 , len + 1 , stream );
+  } 
+  return s;
+}
 
 
 
+void set_fwrite(const set_type * set, FILE * stream) {
+  char ** key_list = set_alloc_keylist(set);
+  int size = set_get_size(set);
+  int i;
+  fwrite(&size , sizeof size , 1 , stream);
+  for (i=0; i < size; i++) {
+    __fwrite_string(key_list[i] , stream);
+    free(key_list[i]);
+  }
+  free(key_list);
+}
+
+
+void set_fread(set_type * set , FILE * stream) {
+  int size, i;
+  fread(&size , sizeof size , 1 , stream);
+  for (i=0; i < size; i++) {
+    char * key = __fread_alloc_string(stream);
+    set_add_key(set , key);
+    free(key);
+  }
+}
+
+
+
+set_type * set_fread_alloc(FILE * stream) {
+  set_type * set = set_alloc_empty();
+  set_fread(set , stream);
+  return set;
+}
 
--- devel/libhash/src/set.h
+++ devel/libhash/src/set.h
@@ -11,9 +11,13 @@ typedef struct set_struct set_type;
 set_type   * set_alloc(int , const char ** );
 set_type   * set_alloc_empty();
 bool         set_add_key(set_type * , const char * );
+bool         set_has_key(set_type * set, const char * );
 void         set_free(set_type * );
 int          set_get_size(const set_type *);
 char      ** set_alloc_keylist(const set_type * );
+void         set_fwrite(const set_type * , FILE * );
+void         set_fread(set_type * , FILE * );
+set_type   * set_fread_alloc(FILE *);
 
 
 
