e540c96c25d35894e98e76b30e5d58cce7d54055
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-05-26 14:57:57 +0000

    Made convert.c slightly more robust
    git-svn-id: http://ert.nr.no/repos/ert@817 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libecl/src/convert.c
+++ devel/libecl/src/convert.c
@@ -43,44 +43,50 @@ void file_convert(const char * src_file , const char * target_file) {
 
 
 int main (int argc , char **argv) { 
-  char *src_file    = argv[1];
-  char *target_file;
-  
-  int           report_nr;
-  ecl_file_type file_type;
-  bool          fmt_file;
-  ecl_util_get_file_type(src_file , &file_type , &fmt_file , &report_nr);
-  
-  if (file_type == ecl_other_file) {
-    if (argc != 3) {
-      fprintf(stderr,"When the file can not be recognized on the name as an ECLIPSE file you must give output_file as second (and last) argument \n");
-      exit(0);
-    }
-    target_file = argv[2];
-    file_convert(src_file , target_file);
+  if (argc == 1) {
+    fprintf(stderr,"Usage: convert.x <filename1> <filename2> <filename3> ...\n");
+    exit(1);
   } else {
-    int file_nr;
-    for (file_nr = 1; file_nr < argc; file_nr++) {
-      char *path;
-      char *basename;
-      char *extension;
-      src_file    = argv[file_nr];
-      ecl_util_get_file_type(src_file , &file_type , &fmt_file , &report_nr);
-      if (file_type == ecl_other_file) {
-	fprintf(stderr,"File: %s - problem \n",src_file);
-	fprintf(stderr,"In a list of many files ALL must be recognizable by their name. \n");
-	abort();
-      }
-      util_alloc_file_components(src_file , &path , &basename , &extension);
-      
-      target_file = ecl_util_alloc_filename(path, basename , file_type , !fmt_file , report_nr);
-      file_convert(src_file , target_file);
 
-      free(path);
-      free(basename);
-      free(extension);
-      free(target_file);
+    char *src_file    = argv[1];
+    char *target_file;
+  
+    int           report_nr;
+    ecl_file_type file_type;
+    bool          fmt_file;
+    ecl_util_get_file_type(src_file , &file_type , &fmt_file , &report_nr);
+    
+    if (file_type == ecl_other_file) {
+      if (argc != 3) {
+	fprintf(stderr,"When the file can not be recognized on the name as an ECLIPSE file you must give output_file as second (and last) argument \n");
+	exit(0);
+      }
+      target_file = argv[2];
+      file_convert(src_file , target_file);
+    } else {
+      int file_nr;
+      for (file_nr = 1; file_nr < argc; file_nr++) {
+	char *path;
+	char *basename;
+	char *extension;
+	src_file    = argv[file_nr];
+	ecl_util_get_file_type(src_file , &file_type , &fmt_file , &report_nr);
+	if (file_type == ecl_other_file) {
+	  fprintf(stderr,"File: %s - problem \n",src_file);
+	  fprintf(stderr,"In a list of many files ALL must be recognizable by their name. \n");
+	  abort();
+	}
+	util_alloc_file_components(src_file , &path , &basename , &extension);
+	
+	target_file = ecl_util_alloc_filename(path, basename , file_type , !fmt_file , report_nr);
+	file_convert(src_file , target_file);
+	
+	free(path);
+	free(basename);
+	free(extension);
+	free(target_file);
+      }
     }
+    return 0;
   }
-  return 0;
 }
