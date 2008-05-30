b032e74e16dfe1a9e578741bdd2737989ad7fbc2
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-05-30 21:44:15 +0000

    Added context based filtering on ext_job_fprtinf_python()
    git-svn-id: http://ert.nr.no/repos/ert@832 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libecl_queue/src/ext_job.c
+++ devel/libecl_queue/src/ext_job.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <util.h>
 #include <hash.h>
@@ -21,8 +22,12 @@ jobList = [
 */
 
 
+#define __TYPE_ID__ 763012
+
 
 struct ext_job_struct {
+  int          __type_id;
+  char       * name;
   int  	       priority;
   char 	     * portable_exe;
   char 	     * init_code;
@@ -37,9 +42,21 @@ struct ext_job_struct {
 };
 
 
-ext_job_type * ext_job_alloc(int priority) {
+ext_job_type * ext_job_safe_cast(const void * __ext_job) {
+  ext_job_type * ext_job = (ext_job_type * ) __ext_job;
+  if (ext_job->__type_id != __TYPE_ID__) {
+    util_abort("%s: safe_cast() failed - internal error\n",__func__);
+    return NULL ;
+  }  else
+    return ext_job;
+}
+
+
+ext_job_type * ext_job_alloc(const char * name , int priority) {
   ext_job_type * ext_job = util_malloc(sizeof * ext_job , __func__);
 
+  ext_job->__type_id = __TYPE_ID__;
+  ext_job->name = util_alloc_string_copy(name);
   ext_job->priority     = priority;
   ext_job->portable_exe = NULL;
   ext_job->init_code = NULL;
@@ -57,6 +74,7 @@ ext_job_type * ext_job_alloc(int priority) {
 
 
 void ext_job_free(ext_job_type * ext_job) {
+  free(ext_job->name);
   util_safe_free(ext_job->portable_exe);
   util_safe_free(ext_job->init_code);
   util_safe_free(ext_job->stdout_file);
@@ -104,6 +122,9 @@ void ext_job_add_environment(ext_job_type *ext_job , const char * platform , con
   hash_insert_hash_owned_ref( ext_job->environment , platform , util_alloc_string_copy( exe ) , free);
 }
 
+
+
+
 void ext_job_add_arg(ext_job_type * ext_job , const char * arg) {
   ext_job->argv = util_realloc(ext_job->argv , (ext_job->argc + 1) * sizeof * ext_job->argv , __func__);
   ext_job->argv[ext_job->argc] = util_alloc_string_copy( arg );
@@ -111,20 +132,35 @@ void ext_job_add_arg(ext_job_type * ext_job , const char * arg) {
 }
 
 
-static void __fprintf_python_string(FILE * stream , const char * id , const char * value) {
+static void __fprintf_python_string(FILE * stream , const char * id , const char * value, const hash_type * context_hash) {
   fprintf(stream , "\"%s\" : " , id);
   if (value == NULL)
     fprintf(stream,"None");
-  else
-    fprintf(stream,"\"%s\"",value);
+  else {
+    
+    if (context_hash != NULL) {
+      fprintf(stream,"\"");
+      util_filtered_fprintf( value , strlen(value) , stream , '<' , '>' , context_hash , util_filter_warn0);
+      fprintf(stream,"\"");
+    } else
+      fprintf(stream,"\"%s\"" , value);
+  }
 }
 
-static void __fprintf_python_list(FILE * stream , const char * id , const char ** list , int size) {
+static void __fprintf_python_list(FILE * stream , const char * id , const char ** list , int size, const hash_type * context_hash ) {
   int i;
   fprintf(stream , "\"%s\" : " , id);
   fprintf(stream,"[");
   for (i = 0; i < size; i++) {
-    fprintf(stream,"\"%s\"" , list[i]);
+    const char * value = list[i];
+
+    if (context_hash != NULL) {
+      fprintf(stream,"\"");
+      util_filtered_fprintf( value , strlen(value) , stream , '<' , '>' , context_hash , util_filter_warn0);
+      fprintf(stream,"\"");
+    } else
+      fprintf(stream,"\"%s\"" , value);
+
     if (i < (size - 1))
       fprintf(stream,",");
   }
@@ -132,14 +168,23 @@ static void __fprintf_python_list(FILE * stream , const char * id , const char *
 }
 
 
-static void __fprintf_python_hash(FILE * stream , const char * id , const hash_type * hash) {
+static void __fprintf_python_hash(FILE * stream , const char * id , const hash_type * hash, const hash_type * context_hash) {
   int i;
   int size = hash_get_size(hash);
   char ** key_list = hash_alloc_keylist(hash);
   fprintf(stream , "\"%s\" : " , id);
   fprintf(stream,"{");
   for (i = 0; i < size; i++) {
-    fprintf(stream,"\"%s\":\"%s\"" , key_list[i] , (char *) hash_get(hash , key_list[i]));  
+    const char * value = hash_get(hash , key_list[i]);
+
+    fprintf(stream,"\"%s\":\"" , key_list[i]);
+    if (context_hash != NULL) {
+      fprintf(stream,"\"");
+      util_filtered_fprintf( value , strlen(value) , stream , '<' , '>' , context_hash , util_filter_warn0);
+      fprintf(stream,"\"");
+    } else
+      fprintf(stream,"\"%s\"" , value);
+    
     if (i < (size - 1))
       fprintf(stream,",");
   }
@@ -161,16 +206,33 @@ static void __indent(FILE * stream, int indent) {
 }
 
 
-void ext_job_python_fprintf(const ext_job_type * ext_job, FILE * stream) {
+void ext_job_python_fprintf(const ext_job_type * ext_job, FILE * stream, const hash_type * context_hash) { 
   fprintf(stream," {");
-  __indent(stream, 0); __fprintf_python_string(stream , "portable_exe" 	  , ext_job->portable_exe);    __end_line(stream);
-  __indent(stream, 2); __fprintf_python_string(stream , "init_code"    	  , ext_job->init_code);       __end_line(stream);
-  __indent(stream, 2); __fprintf_python_string(stream , "target_file"  	  , ext_job->target_file);     __end_line(stream);
-  __indent(stream, 2); __fprintf_python_string(stream , "stdout"    	  , ext_job->stdout_file);     __end_line(stream);
-  __indent(stream, 2); __fprintf_python_string(stream , "stderr"    	  , ext_job->stderr_file);     __end_line(stream);
-  __indent(stream, 2); __fprintf_python_string(stream , "stdin"     	  , ext_job->stdin_file);      __end_line(stream);
-  __indent(stream, 2); __fprintf_python_list(stream   , "argList"      	  , (const char **) ext_job->argv , ext_job->argc); __end_line(stream);
-  __indent(stream, 2); __fprintf_python_hash(stream   , "environment"  	  , ext_job->environment);      __end_line(stream);
-  __indent(stream, 2); __fprintf_python_hash(stream   , "platform_exe" 	  , ext_job->platform_exe); 
+  __indent(stream, 0); __fprintf_python_string(stream , "portable_exe" 	  , ext_job->portable_exe , context_hash);    __end_line(stream);
+  __indent(stream, 2); __fprintf_python_string(stream , "init_code"    	  , ext_job->init_code , context_hash);       __end_line(stream);
+  __indent(stream, 2); __fprintf_python_string(stream , "target_file"  	  , ext_job->target_file , context_hash);     __end_line(stream);
+  __indent(stream, 2); __fprintf_python_string(stream , "stdout"    	  , ext_job->stdout_file , context_hash);     __end_line(stream);
+  __indent(stream, 2); __fprintf_python_string(stream , "stderr"    	  , ext_job->stderr_file , context_hash);     __end_line(stream);
+  __indent(stream, 2); __fprintf_python_string(stream , "stdin"     	  , ext_job->stdin_file , context_hash);      __end_line(stream);
+  __indent(stream, 2); __fprintf_python_list(stream   , "argList"      	  , (const char **) ext_job->argv , ext_job->argc , context_hash); __end_line(stream);
+  __indent(stream, 2); __fprintf_python_hash(stream   , "environment"  	  , ext_job->environment , context_hash);      __end_line(stream);
+  __indent(stream, 2); __fprintf_python_hash(stream   , "platform_exe" 	  , ext_job->platform_exe , context_hash); 
   fprintf(stream,"}\n");
 }
+
+
+int ext_job_compare__(const void * __ext_job1, const void * __ext_job2 ) {
+  ext_job_type * job1 = ext_job_safe_cast(__ext_job1);
+  ext_job_type * job2 = ext_job_safe_cast(__ext_job2);
+
+  if (job1->priority < job2->priority)
+    return -1;
+  else if (job1->priority == job2->priority) {
+    util_abort("%s: two jobs %s and %s  have identitcal priority \n",__func__ , job1->name , job2->name);
+    return 0;
+  } else /* if (job1->priority > job2->priority) */
+    return 1;
+}
+
+
+#undef __TYPE_ID__
--- devel/libecl_queue/src/ext_job.h
+++ devel/libecl_queue/src/ext_job.h
@@ -1,10 +1,11 @@
 #ifndef __EXT_JOB_H__
 #define __EXT_JOB_H__
+#include <hash.h>
 
 typedef struct ext_job_struct ext_job_type;
 
 
-ext_job_type * ext_job_alloc(int) ;
+ext_job_type * ext_job_alloc(const char * , int) ;
 void ext_job_free(ext_job_type * ) ;
 
 void ext_job_add_environment(ext_job_type *, const char * , const char * ) ;
@@ -17,6 +18,6 @@ void ext_job_set_target_file(ext_job_type * , const char * );
 void ext_job_set_stderr_file(ext_job_type * , const char * );
 void ext_job_add_platform_exe(ext_job_type *, const char * , const char * ) ;
 void ext_job_add_arg(ext_job_type *  , const char * );
-void ext_job_python_fprintf(const ext_job_type * , FILE * );
+void ext_job_python_fprintf(const ext_job_type * , FILE * , const hash_type *);
 
 #endif
