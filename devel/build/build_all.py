1136039b9fe8318a9a0e513ba0570039f8aa5f55
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-06-20 12:34:46 +0000

    Simplified build_all
    git-svn-id: http://ert.nr.no/repos/ert@922 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/build/build_all.py
+++ devel/build/build_all.py
@@ -22,33 +22,20 @@ def load_path_list(path_config):
 
 
 (path_list , path_dict) = load_path_list("path_config")
-if not os.path.exists(path_dict["ENKF_BIN_PATH"]):
-    os.makedirs(path_dict["ENKF_BIN_PATH"])
-cmd = "cp %s/ecl_submit.py %s/ecl_submit" % ("../Scripts" , path_dict["ENKF_BIN_PATH"])
-print cmd
-os.system(cmd)
-if os.path.exists("%s/ecl_env.py" % "../Scripts"):
-   cmd = "cp %s/ecl_env.py %s/ecl_env.py" % ("../Scripts" , path_dict["ENKF_BIN_PATH"])
-   print "%s\n" % cmd
-   os.system(cmd)
-else:
-   sys.stderr.write("** WARNING : could not find file \"ecl_env.py\" - build might fail to submit eclipse jobs. ** \n")
-
 
 for path in path_list:
     print path
     cwd = os.getcwd()
     if os.path.exists("%s/src/makefile" % path):
         os.chdir("%s/src" % path)
-        for mflag in ["m32" , "m64"]:
-           os.system("make -s clean MFLAG=%s" %  mflag)
-           nCPU = 4
-           if path.find("sample") != -1:
-               nCPU = 1
-           elif path.find("analysis") != -1:
-               nCPU = 1
-           print "make -s -j %d MFLAG=%s" % (nCPU , mflag)
-           os.system("make -s -j %d MFLAG=%s" % (nCPU , mflag)) 
+        os.system("make -s clean")
+        nCPU = 4
+        if path.find("sample") != -1:
+            nCPU = 1
+        elif path.find("analysis") != -1:
+            nCPU = 1
+        print "make -s -j %d " % (nCPU)
+        os.system("make -s -j %d " % (nCPU)) 
         os.chdir(cwd)
 
 
