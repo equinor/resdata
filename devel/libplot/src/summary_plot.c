23d0be0b3141a2c95aec2df9997b42971ab4b2a0
masar <masar@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-07-08 13:04:04 +0000

    Plot up different well in same plot, and plot up sum of all wells in another plot
    git-svn-id: http://ert.nr.no/repos/ert@1005 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libplot/src/summary_plot.c
+++ devel/libplot/src/summary_plot.c
@@ -10,9 +10,8 @@ WOPR:PRO11 WOPR:PRO12 WOPR:PRO15
 */
 
 
-void collect_summary_data(double **x, double **y, int *size) {
+void collect_summary_data(double **x, double **y, int *size, const char *keyword) {
      const char data_file[] = "/d/proj/bg/enkf/EnKF_PUNQS3/PUNQS3/Original/PUNQS3.DATA";
-     const char keyword[] = "WOPR:PRO1";
      char *base;
      char *header_file;
      char **summary_file_list;
@@ -42,11 +41,11 @@ void collect_summary_data(double **x, double **y, int *size) {
 	       int day, month, year;
 	       util_set_date_values(ecl_sum_get_sim_time(ecl_sum , report_step) , &day , &month, &year);
 /*	       fprintf(stdout, "%04d   %02d/%02d/%04d   ", report_step, day, month, year);*/
+
 	       x_tmp[report_step] = (double) report_step; 
 	       y_tmp[report_step] = ecl_sum_get_general_var(ecl_sum, report_step, keyword);
 	  }
      }
-
      *x = x_tmp;
      *y = y_tmp;
 
@@ -62,16 +61,51 @@ int main(int argc, const char **argv) {
      plot_type *p;
      plot_item *item;
      double *x, *y;
-     int N;
+     double *y_tot = NULL;
+     double *x_tot = NULL;
+     int N, j;
+     /* const char *keywords[] = {"WOPR:PRO1", "WOPR:PRO4"}; */
+     const char *keywords[] = {"WOPR:PRO1", "WOPR:PRO4", "WOPR:PRO5", "WOPR:PRO11", "WOPR:PRO12", "WOPR:PRO15"};
+     int nwords = 6;
+     int i;
 
      p = plot_alloc();
      plot_init(p, true, argc, argv);
 
      item = plot_item_new(p, "png", "punqs3_wopr.png");
-     collect_summary_data(&x, &y, &N);
-     plot_item_set_graph_data(p, item, x, y, N);
+     for (j = 0; j < nwords; j++) {
+	  collect_summary_data(&x, &y, &N, keywords[j]);
+	  plot_item_set_graph_data(p, item, x, y, N);
+     }
      plot_item_set_size(item, 0, 83, 0, 210);
      plot_item_set_labels(item, "Timesteps", "WOPR:PRO1", "PUNQS3 test");
+     plot_item_plot_data(p, item);
+
+     printf("--------------------------------------------\n");
+     
+     item = plot_item_new(p, "png", "punqs3_all_wopr.png");
+
+     /* Calculate total production for all wells */ 
+     for (j = 0; j < nwords; j++) {
+	  collect_summary_data(&x, &y, &N, keywords[j]);
+	  if (!y_tot && !x_tot) { 
+	     x_tot = malloc(sizeof(double) * (N + 1));
+	     y_tot = malloc(sizeof(double) * (N + 1));
+	     memset(x_tot, 0, sizeof(double) * (N + 1));
+	     memset(y_tot, 0, sizeof(double) * (N + 1));
+	  }
+	  for (i = 0; i <= N; i++) {
+	       y_tot[i] = y_tot[i] + y[i];
+	       x_tot[i] = x[i];
+	       printf("[%f] y: %f\n", x[i], y_tot[i]); 
+	  }
+	  util_safe_free(y);
+	  util_safe_free(x);
+     }
+     
+     plot_item_set_graph_data(p, item, x_tot, y_tot, N);
+     plot_item_set_size(item, 0, 85, 0, 1250);
+     plot_item_set_labels(item, "Timesteps", "WOPR, sum", "PUNQS3 test");
      plot_item_plot_data(p, item);
 
      plot_free(p);
