abc0b7348b47d6671c0cddf134ec55b941cce0f2
jgv <jgv@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-07-10 08:52:38 +0000

    git-svn-id: http://ert.nr.no/repos/ert@1018 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libtpg/src/tpgzone_test.c
+++ devel/libtpg/src/tpgzone_test.c
@@ -1,16 +1,30 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <util.h>
 #include <tpgzone.h>
 
 int main(int argc, char ** argv)
 {
+  char * config_file;
+  char * config_base;
+  char * path;
+  char * ext;
+
   if(argc != 2)
   {
     printf("Usage: tpgzone_test.x config_file\n");
     return 0;
   }
 
-  tpgzone_type * tpgzone = tpgzone_fscanf_alloc(argv[1], true);
+  util_alloc_file_components(argv[1], &path, &config_base, &ext);
+
+  if(path != NULL)
+    chdir(path);
+
+  config_file = util_strcat_realloc(config_base,".");
+  config_file = util_strcat_realloc(config_file,ext);
+  
+  tpgzone_type * tpgzone = tpgzone_fscanf_alloc(config_file, true);
 
   tpgzone_free(tpgzone);
   return 0;
