59037e2ff001454a90c4363d07282bb542771a80
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-01-29 21:17:11 +0000

    Debuggin gSETENV / UPDATE_PATH
    git-svn-id: http://ert.nr.no/repos/ert@1678 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libutil/src/util.c
+++ devel/libutil/src/util.c
@@ -3176,7 +3176,6 @@ char * util_alloc_PATH_executable(const char * executable) {
       char ** path_list;
       int     path_size , ipath;
       
-      printf("PATH:%s \n",path_env);
       ipath = 0;
       util_split_string(getenv("PATH") , ":" , &path_size , &path_list);
       while ( cont ) {
@@ -3193,7 +3192,6 @@ char * util_alloc_PATH_executable(const char * executable) {
       }
       util_free_stringlist(path_list , path_size);
     }
-    printf("Retuning:%s \n",full_path);
     return full_path;
   }
 }
@@ -3840,12 +3838,13 @@ void util_update_path_var(const char * variable, const char * value, bool append
     setenv( variable , value , 1);
   else {
     char * new_value;
+
     if (append)
       new_value = util_alloc_sprintf("%s:%s" , getenv( variable ) , value);
     else
       new_value = util_alloc_sprintf("%s:%s" , value , getenv( variable ));
 
-    unsetenv( variable );
+    //unsetenv( variable );
     setenv( variable , new_value , 1);
     free( new_value);
   }
