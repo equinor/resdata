4eec2fb71ff80a9b6dbc857e69ab675ad9d001f0
jgv <jgv@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-03-24 08:59:20 +0000

    Added Doxygen configuration for enkf.
    git-svn-id: http://ert.nr.no/repos/ert@1855 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libplot/src/plot.h
+++ devel/libplot/src/plot.h
@@ -4,28 +4,6 @@
 extern "C" {
 #endif
 
-/*! \mainpage libplot: plotting library based on Plplot
- *
- * \section intro Introduction
- *
- * This is a plotting library/wrapper for the plplot library. It is supposed to make life easier
- * when doing plots from the EnKF system.
- * Here is an example of what could be produced:
- * @image html punqs3_fopt.png 
- *
- * \section install Installation of plplot
- * As of now, plplot is installed in the directory /h/masar/plplot/.
- * For more information on how this was compiled, see the Readme file in the src/ directory.
- *
- * \subsection example1 Example 1: Plotting of the function: y = sinc(x).
- * \include simple.c
-
- * \subsection example2 Example 2: Plotting some well known functions.
- * \include plot_test.c
- * 
- * \subsection example3 Example 3: Plotting inside a gtk application, with animation.
- * \include plot_canvas_test.c
- */
 
 /**
  * @addtogroup plot_type plot_type: A new unique plot with room for n datasets.
