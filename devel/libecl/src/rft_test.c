2c60a1a33bc0f7fd8504da0d51aacce91bfca5f5
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-05-15 10:56:36 +0000

    Added file rft_test for demonstration of rft functionality II
    git-svn-id: http://ert.nr.no/repos/ert@1954 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libecl/src/rft_test.c
+++ devel/libecl/src/rft_test.c
@@ -12,12 +12,12 @@ int main (int argc , char ** argv) {
     ecl_rft_file_type * rft_file = ecl_rft_file_alloc( filename );  
     stringlist_type   * wells    = ecl_rft_file_alloc_well_list( rft_file );
     
-    printf("RFT file: %s has a total of %d entries and %d wells \n",filename , ecl_rft_file_get_size( rft_file), stringlist_get_size( wells ));
+    printf("<ECLIPSE RFT FILE>\n");
     {
       int iw;
       for (iw = 0; iw < stringlist_get_size( wells ); iw++) {
 	const char * well = stringlist_iget(wells , iw);
-	printf("  Well: %s has a total of %d RFTs in the file. \n",well , ecl_rft_file_get_well_occurences( rft_file , well ));
+	printf("    <WELL>\n");
 	{
 	  int it;
 	  for (it = 0; it < ecl_rft_file_get_well_occurences( rft_file , well ); it++) {
@@ -26,23 +26,29 @@ int main (int argc , char ** argv) {
 	    {
 	      int mday, year,month;
 	      util_set_date_values( date , &mday , &month , &year);
-	      printf("    RFT was recorded on %02d/%02d/%4d \n",mday,month,year);
+	      printf("        <RFT>\n");
+	      printf("            <DATE>%02d/%02d/%4d</DATE> \n",mday,month,year);
 	      {
 		int num_cells = ecl_rft_node_get_size( node );
 		int icell;
-		printf("    A total of %d cells have been perforated \n",num_cells);
 		for (icell = 0; icell < num_cells; icell++) {
 		  int i,j,k;
 		  ecl_rft_node_iget_ijk( node , icell , &i , &j , &k);
-		  printf("      Cell: %d (%3d,%3d,%d) has pressure/depth:  %g/%g \n",icell , i,j,k,ecl_rft_node_iget_pressure( node , icell) , ecl_rft_node_iget_depth( node , icell));
+		  printf("            <cell>\n");
+		  printf("                <PRESSURE> %g </PRESSURE> \n", ecl_rft_node_iget_pressure( node, icell));
+		  printf("                <DPETH>    %g </DEPTH>     \n" , ecl_rft_node_iget_depth( node , icell));
+		  printf("                <ijk> %3d,%3d,%3d </ijk>  \n",i,j,k);     
+		  printf("            </cell>\n");
 		}
 	      }
+	      printf("        </RFT>\n");
 	    }
 	  }
 	}
+	printf("    </WELL>\n");
       }
     }
-
+    printf("</ECLIPSE RFT FILE>\n");
     ecl_rft_file_free( rft_file );
   }
 }
