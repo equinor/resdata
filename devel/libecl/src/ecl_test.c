af52ca688ea06b793db39fdb1466bf4bed55cd51
masar <masar@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-07-10 11:35:48 +0000

    added a few functions used in the python wrapping
    git-svn-id: http://ert.nr.no/repos/ert@2049 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libecl/src/ecl_test.c
+++ devel/libecl/src/ecl_test.c
@@ -13,10 +13,49 @@
 #include <ecl_sum_data.h>
 #include <ecl_file.h>
 
-int main(int argc, char ** argv) {
-  ecl_file_type * restart50 = ecl_file_fread_alloc_unrst_section("Gurbat/EXAMPLE_01_BASE.UNRST" , 90 , true);
-  if (restart50 != NULL)
-    ecl_file_free( restart50 );
-  else
-    printf("Could not find ... \n");
+#define GRID_FILE "/d/felles/bg/scratch/masar/STRUCT/realization-0-step-0-to-358/RG01-STRUCT-0.EGRID"
+#define RESTART_FILE "/d/felles/bg/scratch/masar/STRUCT/realization-0-step-0-to-358/RG01-STRUCT-0.X0050"
+
+int
+main (int argc, char **argv)
+{
+  ecl_grid_type *ecl_grid;
+  ecl_kw_type *ecl_kw;
+  ecl_file_type *ecl_file;
+  int global_size;
+  int active_size;
+  int i;
+  int size;
+
+  ecl_grid = ecl_grid_alloc (GRID_FILE, 1);
+  global_size = ecl_grid_get_global_size (ecl_grid);
+  active_size = ecl_grid_get_active_size (ecl_grid);
+
+  ecl_file = ecl_file_fread_alloc (RESTART_FILE, 1);
+  ecl_kw = ecl_file_iget_named_kw (ecl_file, "PRESSURE", 0);
+  size = ecl_kw_get_size(ecl_kw);
+
+  for (i = 0; i < global_size; i++)
+    {
+      void *float_data;
+      double double_data;
+      int ret;
+      int x, y, z;
+
+      ret = ecl_grid_get_active_index1 (ecl_grid, i);
+      if (ret != -1)
+	{
+	  float_data = ecl_kw_iget_ptr (ecl_kw, ret);
+	  double_data = ecl_kw_iget_as_double(ecl_kw, ret);
+
+	  ecl_grid_get_ijk1A (ecl_grid, ret, &x, &y, &z);
+	  printf ("pressure: %f double: %f on index %d (%d, %d, %d)\n",
+		  ((float *) float_data)[ret], double_data, ret, x, y, z);
+	}
+
+    }
+
+  printf ("active_size: %d, global_size: %d\n", active_size, global_size);
+
+
 }
