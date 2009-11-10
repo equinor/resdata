54f4dc0e6646538b44f4b21b2816f0680c71139f
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-11-10 17:54:48 +0000

    Updated the template class to allow for replacements in the filename of the template file
    git-svn-id: http://ert.nr.no/repos/ert@2274 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libutil/src/template.c
+++ devel/libutil/src/template.c
@@ -3,22 +3,56 @@
 #include <subst.h>
 #include <template.h>
 
+
+
 #define TEMPLATE_TYPE_ID 7781045
 
 struct template_struct {
   UTIL_TYPE_ID_DECLARATION;
-  char            * template_file;
-  char            * template_buffer;
-  bool              internalize_template;
-  subst_list_type * arg_list; 
+  char            * template_file;           /* The template file - if internalize_template == false this filename can contain keys which will be replaced at instantiation time. */
+  char            * template_buffer;         /* The content of the template buffer; only has valid content if internalize_template == true. */
+  bool              internalize_template;    /* Should the template be loadad and internalized at template_alloc(). */
+  subst_list_type * arg_list;                /* Key-value mapping established at alloc time. */
 };
 
 
-static void template_load( template_type * template ) {
+
+
+/**
+   Iff the template is set up with internaliz_template == false the
+   template content is loaded at instantiation time, and in that case
+   the name of the template file can contain substitution characters -
+   i.e. in this case different instance can use different source
+   templates.
+
+   To avoid race issues this function does not set actually update the
+   state of the template object.
+*/
+
+static char * template_load( const template_type * template , const subst_list_type * ext_arg_list) {
   int buffer_size;
-  template->template_buffer = util_fread_alloc_file_content( template->template_file , &buffer_size );
+  char * template_file = util_alloc_string_copy( template->template_file );
+  char * template_buffer;
+  
+  subst_list_update_string( template->arg_list , &template_file);
+  if (ext_arg_list != NULL)
+    subst_list_update_string( ext_arg_list , &template_file);
+  
+  template_buffer = util_fread_alloc_file_content( template_file , &buffer_size );
+  free( template_file );
+  
+  return template_buffer;
 }
 
+
+
+/**
+   This function allocates a template object based on the source file
+   'template_file'. If @internalize_template is true the template
+   content will be read and internalized at boot time, otherwise that
+   is deferred to template instantiation time (in which case the
+   template file can change dynamically).
+*/
 
 
 template_type * template_alloc( const char * template_file , bool internalize_template) {
@@ -31,7 +65,8 @@ template_type * template_alloc( const char * template_file , bool internalize_te
   template->internalize_template = internalize_template;
   
   if (template->internalize_template)
-    template_load( template );
+    template->template_buffer = template_load( template , NULL );
+  
   return template;
 }
 
@@ -46,17 +81,46 @@ void template_free( template_type * template ) {
 
 
 
-void template_instansiate( template_type * template , const char * __target_file , const subst_list_type * arg_list ) {
-  char * target_file = util_alloc_string_copy( __target_file );
-  if (!template->internalize_template)
-    template_load( template );
+/**
+   This function will create the file @__target_file based on the
+   template instance. Before the target file is written all the
+   internal substitutions and then subsequently the subsititutions in
+   @arg_list will be performed. The input @arg_list can be NULL - in
+   which case this is more like copy operation.
 
+   Observe that:
+   
+    1. Substitutions will be performed on @__target_file
+
+    2. @__target_file can contain path components.
+
+    3. If internalize_template == false subsititions will be performed
+       on the filename of the file with template content.
+
+*/
+   
+
+
+void template_instansiate( const template_type * template , const char * __target_file , const subst_list_type * arg_list ) {
+  char * target_file = util_alloc_string_copy( __target_file );
+
+  /* Finding the name of the target file. */
   subst_list_update_string( template->arg_list , &target_file);
-  subst_list_update_string( arg_list , &target_file );
+  if (arg_list != NULL) subst_list_update_string( arg_list           , &target_file );
+
   {
-    char * buffer = util_alloc_string_copy( template->template_buffer );
+    char * buffer;
+    /* Loading the template - possibly expanding keys in the filename */
+    if (template->internalize_template)
+      buffer = util_alloc_string_copy( template->template_buffer);
+    else
+      buffer = template_load( template , arg_list );
+    
+    /* Substitutions on the content. */
     subst_list_update_string( template->arg_list , &buffer );
-    subst_list_update_string( arg_list , &buffer );
+    if (arg_list != NULL) subst_list_update_string( arg_list           , &buffer );
+
+    /* Write the content out. */
     {
       FILE * stream = util_mkdir_fopen( target_file , "w");
       fprintf(stream , "%s" , buffer);
@@ -64,14 +128,13 @@ void template_instansiate( template_type * template , const char * __target_file
     }
     free( buffer );
   }
-  if (!template->internalize_template) {
-    free(template->template_buffer);
-    template->template_buffer = NULL;
-  }
 }
 
 
-
+/**
+   Add an internal key_value pair. This substitution will be performed
+   before the internal substitutions.
+*/
 void template_add_arg( template_type * template , const char * key , const char * value ) {
   subst_list_insert_copy( template->arg_list , key , value );
 }
--- devel/libutil/src/template.h
+++ devel/libutil/src/template.h
@@ -8,7 +8,7 @@ typedef struct template_struct template_type;
 
 template_type * template_alloc( const char * template_file , bool internalize_template);
 void            template_free( template_type * template );
-void            template_instansiate( template_type * template , const char * __target_file , const subst_list_type * arg_list );
+void            template_instansiate( const template_type * template , const char * __target_file , const subst_list_type * arg_list );
 void            template_add_arg( template_type * template , const char * key , const char * value );
 
 #endif
