5d45c935c390390c9a695792b25e009939ca079d
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-11-12 08:42:18 +0000

    Added esummary.c
    git-svn-id: http://ert.nr.no/repos/ert@2277 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libecl/src/esummary.c
+++ devel/libecl/src/esummary.c
@@ -13,23 +13,29 @@ void install_SIGNALS(void) {
   signal(SIGTERM , util_abort_signal);    /* If killing the enkf program with SIGTERM (the default kill signal) you will get a backtrace. Killing with SIGKILL (-9) will not give a backtrace.*/
 }
 
+void usage() {
+  fprintf(stderr," The esummary.x program can be used to summary vectors from \n");
+  fprintf(stderr," an ensemble of summary files: \n\n");
+  fprintf(stderr,"    bash%% esummary.x  ECLIPSE1.DATA ECLIPSE2.DATA  KEY1  KEY2  ... \n"); 
+  exit(1);
+}
+
+
 
 
 int main(int argc , char ** argv) {
   install_SIGNALS();
   {
-    if (argc < 3) {
-      fprintf(stderr,"** ERROR ** %s ECLIPSE1.DATA ECLIPSE2.DATA ... WOPR:WGNAME \n",__func__);
-      exit(1);
-    }
+    if (argc < 3) usage(); 
 
-    /** Loading the data */
     {
       ecl_sum_type * first_ecl_sum;   /* This governs the timing */
       vector_type  * ecl_sum_list = vector_alloc_new();
       int nvars;
       char ** var_list;
+      bool *  has_var;
 
+      /** Loading the data */
       {
         int iarg     = 1;
         /*  Some sorting here???  */
@@ -45,12 +51,15 @@ int main(int argc , char ** argv) {
           vector_append_owned_ref( ecl_sum_list , ecl_sum , ecl_sum_free__ );
           iarg++;
           fprintf(stderr,"\n");
+          util_safe_free( path );
+          free( basename );
         }
       }
       nvars    =  argc - vector_get_size( ecl_sum_list ) - 1;
       if (nvars == 0) util_exit(" --- No variables \n");
       var_list = &argv[vector_get_size( ecl_sum_list ) + 1];
-      
+      has_var = util_malloc( nvars * sizeof * has_var , __func__);
+
       
       /** Checking time consistency - and discarding those with unmatching time vector. */
       {
@@ -79,16 +88,19 @@ int main(int argc , char ** argv) {
         time_t_vector_free( first_time );
       }
       
-
-      /** The actual summary lookup. */
+      
+      /* 
+         Checking that the summary files have the various variables -
+         if a variable is missing from one of the cases it is
+         completely discarded.
+      */
       {
-        bool *has_var    = util_malloc( nvars * sizeof * has_var , __func__);
-        int iens,report,ivar;
-  
+        int iens,ivar;
+        
         /* Checking the variables */
         for (ivar = 0; ivar < nvars; ivar++) 
           has_var[ivar] = true;
-          
+        
         for (iens = 0; iens < vector_get_size( ecl_sum_list ); iens++) {
           const ecl_sum_type * ecl_sum = vector_iget_const( ecl_sum_list , iens );
           for (ivar = 0; ivar < nvars; ivar++) {
@@ -100,39 +112,44 @@ int main(int argc , char ** argv) {
             }
           }
         }
+      }
 
-        {
-          int first_report = ecl_sum_get_first_report_step( first_ecl_sum );
-          int last_report  = ecl_sum_get_last_report_step( first_ecl_sum );
-          FILE * stream = stdout;
 
-          for (report = first_report; report <= last_report; report++) {
-            for (ivar = 0; ivar < nvars; ivar++) {                                      /* Iterating over the variables */
-              if (has_var[ivar]) { 
-                for (iens = 0; iens < vector_get_size( ecl_sum_list ); iens++) {            /* Iterating over the ensemble members */
-                  const ecl_sum_type * ecl_sum = vector_iget_const( ecl_sum_list , iens );  
-                  double value = 0;
-                  int ministep1 , ministep2;
-                  if (ecl_sum_has_report_step(ecl_sum , report)) {
-                    ecl_sum_report2ministep_range( ecl_sum , report , &ministep1 , &ministep2);
-                    ministep1 = ministep2;
-                    if (ecl_sum_has_ministep(ecl_sum , ministep2)) {
-                      if (ivar == 0 && iens == 0) { /* Display time info in the first columns */
-                        int day,month,year;
-                        util_set_date_values(ecl_sum_get_sim_time(ecl_sum , ministep2) , &day , &month, &year);
-                        fprintf(stream , "%7.2f   %02d/%02d/%04d   " , ecl_sum_get_sim_days(ecl_sum , ministep2) , day , month , year);
-                      }
-                      value = ecl_sum_get_general_var(ecl_sum , ministep2 , var_list[ivar]);
+      /** The actual summary lookup. */
+      {
+        int first_report = ecl_sum_get_first_report_step( first_ecl_sum );
+        int last_report  = ecl_sum_get_last_report_step( first_ecl_sum );
+        FILE * stream = stdout;
+        int iens,ivar,report;
+        
+        for (report = first_report; report <= last_report; report++) {
+          for (ivar = 0; ivar < nvars; ivar++) {                                          /* Iterating over the variables */
+            if (has_var[ivar]) { 
+              for (iens = 0; iens < vector_get_size( ecl_sum_list ); iens++) {            /* Iterating over the ensemble members */
+                const ecl_sum_type * ecl_sum = vector_iget_const( ecl_sum_list , iens );  
+                double value = 0;
+                int ministep1 , ministep2;
+                if (ecl_sum_has_report_step(ecl_sum , report)) {
+                  ecl_sum_report2ministep_range( ecl_sum , report , &ministep1 , &ministep2);
+                  ministep1 = ministep2;
+                  if (ecl_sum_has_ministep(ecl_sum , ministep2)) {
+                    if (ivar == 0 && iens == 0) {                                         /* Display time info in the first columns */
+                      int day,month,year;
+                      util_set_date_values(ecl_sum_get_sim_time(ecl_sum , ministep2) , &day , &month, &year);
+                      fprintf(stream , "%7.2f   %02d/%02d/%04d   " , ecl_sum_get_sim_days(ecl_sum , ministep2) , day , month , year);
                     }
+                    value = ecl_sum_get_general_var(ecl_sum , ministep2 , var_list[ivar]);
                   }
-                  fprintf(stream , " %12.3f " , value);
                 }
+                fprintf(stream , " %12.3f " , value);
               }
             }
-            fprintf(stream , "\n");
           }
+          fprintf(stream , "\n");
         }
       }
+      vector_free( ecl_sum_list );
+      free( has_var );
     }
   }
 }
