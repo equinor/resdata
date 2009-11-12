440b727a9a801846fff2860c84cec2a015d4ba1a
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-11-12 08:44:15 +0000

    Added ens_tar.c ..
    git-svn-id: http://ert.nr.no/repos/ert@2279 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/libutil/src/ens_tar.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <util.h>
+#include <string.h>
+#include <errno.h>
+#include <msg.h>
+
+bool tar_and_remove_dir( const char * root_path , const char * current_dir , int current_depth , void * arg) {
+  if (strncmp(current_dir , "mem" , 3) == 0) {
+    msg_type * msg = msg_safe_cast( arg );
+    {
+      char * tar_file    = util_alloc_filename(NULL , current_dir , "tar.gz");
+      char * target_file = util_alloc_filename(root_path , current_dir , "tar.gz");
+      char * text        = util_alloc_sprintf("%s: Creating %s" , root_path , tar_file );
+      msg_update( msg , text );
+      util_fork_exec("tar" , 3 , (const char *[3]) {"-czf" , tar_file , current_dir} , true , target_file , root_path , NULL , NULL , NULL);
+      free( text );
+      free( tar_file );
+      free( target_file );
+    }
+    
+    
+    {
+      char * full_path = util_alloc_filename( root_path , current_dir , NULL);
+      char * text     = util_alloc_sprintf("%s: Removing %s" , root_path , current_dir);
+      msg_update( msg , text );
+      util_unlink_path( full_path );
+      free( text );
+      free( full_path );
+    }
+  }
+  return true;
+}
+
+
+
+
+
+int main( int argc , char ** argv ) {
+  const char * mount_file = "enkf_mount_info";
+  if (util_file_exists( mount_file )) {
+    msg_type * msg = msg_alloc("");
+    char * cwd    = util_alloc_cwd();
+    
+    msg_show( msg );
+
+    util_walk_directory( cwd , NULL , NULL , tar_and_remove_dir , msg);
+    msg_free( msg , true );
+    free( cwd );
+  } else 
+    fprintf(stderr,"Hmmm - could not find file: \'%s\' - this does not seem to be a ENSPATH directory.\n",mount_file);
+}
+
+
+
+
+
+
+
