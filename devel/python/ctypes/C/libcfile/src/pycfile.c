0e7a38ee647a1a60739b71f025804fee01e911d5
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-09-26 08:07:21 +0000

    Pycfil workfs
    git-svn-id: http://ert.nr.no/repos/ert@3099 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/C/libcfile/src/pycfile.c
+++ devel/python/ctypes/C/libcfile/src/pycfile.c
@@ -2,20 +2,31 @@
 
 
 
-static FILE * cfile( PyObject * self ,  PyObject * arg) {
-  return PyFile_AsFile( arg );
+static PyObject * pycfile( PyObject * self ,  PyObject * args) {
+  PyObject * PyFile;
+  if (!PyArg_ParseTuple( args , "O" , &PyFile ))
+    return NULL;
+
+  {
+    FILE * cfile          = PyFile_AsFile( PyFile );
+
+    if (cfile != NULL) 
+      return Py_BuildValue( "l" , cfile );  /* The pointer value itself is packed as a long and returned. */
+    else
+      return Py_BuildValue( "" );  /* None */
+  }
 }
 
 
 
-static PyMethodDef cfilemethods[] = {
-  {"cfile" , cfile , METH_VARARGS , "Documentation"},
+static PyMethodDef pycfilemethods[] = {
+  {"pycfile" , pycfile , METH_VARARGS , "Documentation"},
   {NULL , NULL , 0 , NULL}
 };
 
 
 
 PyMODINIT_FUNC
-initcfile( void ) {
-  (void) Py_InitModule("cfile" , cfilemethods);
+initpycfile( void ) {
+  (void) Py_InitModule("pycfile" , pycfilemethods);
 }
