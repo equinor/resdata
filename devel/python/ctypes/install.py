8787ffcf1f1ed45c2c5fd23443752477f7f82da2
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-09-27 06:54:09 +0000

    Minor install fix
    git-svn-id: http://ert.nr.no/repos/ert@3105 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/install.py
+++ devel/python/ctypes/install.py
@@ -106,7 +106,7 @@ make_dir("%s/lib/python"  % SDP_ROOT)
 make_dir("%s/lib/python/lib"  % SDP_ROOT)
 
 install_path( "ert" , target , extensions = ["py"])
-install_file( "C/libpycfile/slib/libpyc.so"             , "%s/lib/python/lib/libpyc.so"       % SDP_ROOT)
+install_file( "C/libpycfile/slib/libpycfile.so"             , "%s/lib/python/lib/libpycfile.so"       % SDP_ROOT)
 install_file( "../../libutil/slib/libutil.so"           , "%s/lib/python/lib/libutil.so"      % SDP_ROOT)
 install_file( "../../libconfig/slib/libconfig.so"       , "%s/lib/python/lib/libconfig.so"    % SDP_ROOT)
 install_file( "../../libecl/slib/libecl.so"             , "%s/lib/python/lib/libecl.so"       % SDP_ROOT)
