7c03d9c655cf5b302b8e5bae919fde3adf1f8500
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-10-04 12:44:39 +0000

    Added grdecl_test.py
    git-svn-id: http://ert.nr.no/repos/ert@3130 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/python/ctypes/test/grdecl_test.py
@@ -0,0 +1,19 @@
+#!/prog/sdpsoft/python2.4/bin/python
+import ert
+import ert.ecl as ecl
+
+file = "data/eclipse/case/include/example_grid_sim.GRDECL"
+fileH = open( file , "r")
+
+specgrid = ecl.EclKW.grdecl_load( fileH , "SPECGRID" , ecl_type = 3)#ecl_type = ecl.ECL_INT_TYPE )
+zcorn    = ecl.EclKW.grdecl_load( fileH , "ZCORN" )
+coord    = ecl.EclKW.grdecl_load( fileH , "COORD" )
+actnum   = ecl.EclKW.grdecl_load( fileH , "ACTNUM" , ecl_type = 3)#ecl_type = ecl.ECL_INT_TYPE )
+
+
+
+print "Creating grid ...."
+grid = ecl.EclGrid.create( specgrid , zcorn , coord , actnum )
+print  grid.size
+print  grid.nx
+print  grid.nactive
