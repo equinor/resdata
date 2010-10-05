5ee04f9fca7614cbd8daa82c8b07a8937dc226be
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-10-05 13:46:22 +0000

    Added ECLIPSE.GRID to testdata
    git-svn-id: http://ert.nr.no/repos/ert@3135 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/test/grid_test.py
+++ devel/python/ctypes/test/grid_test.py
@@ -2,6 +2,11 @@
 import ert
 import ert.ecl as ecl
 
-grid_file = "data/eclipse/case/ECLIPSE.EGRID"
+egrid_file = "data/eclipse/case/ECLIPSE.EGRID"
+egrid = ecl.EclGrid( egrid_file )
+
+print "Loading grid:"
+grid_file = "data/eclipse/case/ECLIPSE.GRID"
 grid = ecl.EclGrid( grid_file )
+
 print "Grid loaded"
