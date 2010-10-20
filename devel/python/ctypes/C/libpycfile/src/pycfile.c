10b21ecd9e6ffa917e53f5fe7dd342c2335cc9b5
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-10-20 09:59:34 +0000

    Added some documentation
    git-svn-id: http://ert.nr.no/repos/ert@3153 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/C/libpycfile/src/pycfile.c
+++ devel/python/ctypes/C/libpycfile/src/pycfile.c
@@ -1,5 +1,27 @@
 #include <Python.h>
 
+/**
+   The purpose of this module is to be able to call C functions which
+   expect FILE * input with a Python file handle. This is based on the
+   normal Python/C api and not ctypes, the Python module cfile.py
+   handles the ctypes side of things.
+
+   What happens is essentially:
+   ----------------------------
+
+     1. Python filehandle is created with fileH = open( name , mode).
+
+     2. The pycfile() function below is called with this filehandle as
+        input; the pycfile function extracts the underlying FILE *
+        pointer with the PyFile_AsFile() function and returns it as an
+        opaque pointer.
+
+     3. This opaque pointer is then wrapped with the normal ctypes
+        approach in the cfile module, and passed on to C functions
+        expecting FILE * input.
+*/
+
+
 
 
 static PyObject * pycfile( PyObject * self ,  PyObject * args) {
@@ -13,20 +35,19 @@ static PyObject * pycfile( PyObject * self ,  PyObject * args) {
     if (cfile != NULL) 
       return Py_BuildValue( "l" , cfile );  /* The pointer value itself is packed as a long and returned. */
     else
-      return Py_BuildValue( "" );  /* None */
+      return Py_BuildValue( "" );           /* None */
   }
 }
 
 
 
 static PyMethodDef pycfilemethods[] = {
-  {"pycfile" , pycfile , METH_VARARGS , "Documentation"},
+  {"pycfile" , pycfile , METH_VARARGS , "Will extract and return the FILE* structure of a Python filehandle."},
   {NULL , NULL , 0 , NULL}
 };
 
 
 
-PyMODINIT_FUNC
-initpycfile( void ) {
+PyMODINIT_FUNC initpycfile( void ) {
   (void) Py_InitModule("pycfile" , pycfilemethods);
 }
--- devel/python/ctypes/ert/util/cfile.py
+++ devel/python/ctypes/ert/util/cfile.py
@@ -5,6 +5,14 @@ from   ert.util.pycfile      import pycfile
 from   ert.cwrap.cwrap       import *
 
 
+# This purpose of this class is to wrap the underlying FILE * pointer
+# of Python filehandle to be able to call C functions which expect
+# FILE * input with a Python filehandle.
+#
+# The class needs support from the small C based extension module
+# pycfile which will get a Python filehandle and return the underlying
+# FILE * pointer value - this is stored in the c_ptr attribute.
+
 class CFILE:
     def __init__( self , py_file ):
         self.c_ptr   =  ctypes.c_long( pycfile( py_file ) )
