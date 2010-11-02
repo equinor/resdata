0e30b6bd4957e14d11b9f9dc251591041b56b6e4
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-11-02 12:45:44 +0000

    Debugging blas fuck VIII
    git-svn-id: http://ert.nr.no/repos/ert@3200 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/ert/util/libutil.py
+++ devel/python/ctypes/ert/util/libutil.py
@@ -3,8 +3,9 @@ import ert.util.clib as clib
 from   ert.util.SDP  import RH_version
 
 clib.load("libz" , "libz.so.1")
+
 if RH_version() < 4:
-    clib.load("libg2c.so.0.0.0")
+    clib.load("libg2c.so.0")
 
 clib.load("libblas.so")
 clib.load("liblapack.so")
