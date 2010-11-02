6884aafd6c534578487ac7e98e546940dbd24c7c
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-11-02 10:40:25 +0000

    Added test util_test.py
    git-svn-id: http://ert.nr.no/repos/ert@3191 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/python/ctypes/test/util_test.py
@@ -0,0 +1,51 @@
+#!/prog/sdpsoft/python2.4/bin/python
+import random
+import copy
+
+from   ert.util.tvector import DoubleVector
+from   ert.util.tvector import IntVector
+import ert.util.stat    as stat
+
+v = DoubleVector( 0 )
+print v
+v[0] = 77.25
+v[1] = 123.25
+v[2] = 66.25
+v[3] = 56.25
+v[4] = 111.25
+v[5] = 99.25
+v[12] = 12
+v.printf( )
+v.clear()
+
+for i in range( 100000 ):
+    r = random.random()
+    v.append( r )
+
+
+print stat.quantile( v , 0.10 )
+print stat.quantile_sorted( v , 0.20 )
+print stat.quantile_sorted( v , 0.30 )
+print stat.quantile_sorted( v , 0.40 )
+print stat.quantile_sorted( v , 0.50 )
+
+a = v[0:10]
+print a
+print a[6]
+print a
+
+
+b = copy.deepcopy( a )
+c = a.copy(  )
+print c
+print b
+print v
+c = IntVector( 0 )
+c.append( 1 )
+c.append( 2 )
+c.append( 3 )
+c.append( 4 )
+c.append( 5 )
+
+c.rsort()
+c.printf()
