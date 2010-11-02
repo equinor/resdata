b4b046224f893b79a547130d3cc9e5ee8051e187
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-11-02 11:01:44 +0000

    Debugging blas fuck V
    git-svn-id: http://ert.nr.no/repos/ert@3197 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/ert/util/libutil.py
+++ devel/python/ctypes/ert/util/libutil.py
@@ -2,7 +2,7 @@ import ctypes
 import ert.util.clib as clib
 
 clib.load("libz" , "libz.so.1")
-clib.load("libg2c")
+clib.load("/usr/lib/libg2c.so.0.0.0")
 clib.load("libblas.so" , "/usr/lib64/libblas.so.3.0.3")
 clib.load("liblapack.so")
 lib = clib.load("libutil.so")
