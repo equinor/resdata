4ba5ba0def0bb75ecc97849a59599073aaf60aaa
eism <eism@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-11-18 12:40:13 +0000

    Minor update for RH5.5
    git-svn-id: http://ert.nr.no/repos/ert@3219 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/ert/util/libutil.py
+++ devel/python/ctypes/ert/util/libutil.py
@@ -7,6 +7,6 @@ clib.load("libz" , "libz.so.1")
 if SDP.RH_version() < 4:
     clib.load("libg2c.so.0")
 
-clib.load("libblas.so")
+clib.load("libblas.so" , "libblas.so.3")
 clib.load("liblapack.so")
 lib = clib.load("libutil.so")
