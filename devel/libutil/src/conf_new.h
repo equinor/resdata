aaa5c31750ea53c5261d6af8c65fb0ba9acec336
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-02-14 09:13:33 +0000

    Added GPL Copyright header to all source files
    git-svn-id: http://ert.nr.no/repos/ert@3359 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libutil/src/conf_new.h
+++ devel/libutil/src/conf_new.h
@@ -1,3 +1,21 @@
+/*
+   Copyright (C) 2011  Statoil ASA, Norway. 
+    
+   The file 'conf_new.h' is part of ERT - Ensemble based Reservoir Tool. 
+    
+   ERT is free software: you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation, either version 3 of the License, or 
+   (at your option) any later version. 
+    
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+   FITNESS FOR A PARTICULAR PURPOSE.   
+    
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+   for more details. 
+*/
+
 #ifndef __CONF_NEW_H__
 #define __CONF_NEW_H__
 
