66b38724b45959f2109b098548cd8b4798de8b15
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-05-03 20:12:34 +0000

    moved ecl_grav -> ecl_grav_calc.py
    git-svn-id: http://ert.nr.no/repos/ert@3440 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/ert/ecl/ecl.py
+++ devel/python/ctypes/ert/ecl/ecl.py
@@ -25,7 +25,7 @@ from   ert.cwrap.cwrap       import *
 import ert.util.ctime        
 import ecl_util
 import ecl_default
-import ecl_grav
+import ecl_grav_calc
 
 from   ecl_kw                import ECL_INT_TYPE , ECL_FLOAT_TYPE , ECL_CHAR_TYPE , ECL_BOOL_TYPE , ECL_DOUBLE_TYPE, EclKW
 from   ecl_case              import EclCase
