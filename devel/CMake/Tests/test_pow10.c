1ef3e76142fa45c1109d09a5839a479c88870d27
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-09-05 14:18:03 +0000

    More minor CMake fixes
    git-svn-id: http://ert.nr.no/repos/ert@3580 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/CMake/Tests/test_pow10.c
@@ -0,0 +1,7 @@
+#define _GNU_SOURCE   // Defined to get access to pow10() function.
+#define __USE_GNU     // Defined to get access to pow10() function.
+#include <math.h>
+
+int main(int argc, char ** argv) {
+  pow10(1);
+}
