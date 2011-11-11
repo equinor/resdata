c3b2eadd3f487d9f4aaea71ebc22515e50203d05
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-11-11 09:21:14 +0000

    Added code to load libgeometry
    git-svn-id: http://ert.nr.no/repos/ert@3657 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/ert/enkf/libenkf.py
+++ devel/python/ctypes/ert/enkf/libenkf.py
@@ -13,7 +13,13 @@
 #   
 #  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
 #  for more details. 
-
+import ert.util.libutil               
+import ert.ecl.libecl
+import ert.job_queue.libjob_queue
+import ert.rms.librms
 import ert.cwrap.clib as clib
+
+clib.load("libsched.so")
+clib.load("libanalysis.so")
 lib = clib.load("libenkf.so")
     
