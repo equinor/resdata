936e2e7419bf575282df127f8a53100e41e2cdbf
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2012-01-25 19:07:43 +0000

    Mor numpy mockup
    git-svn-id: http://ert.nr.no/repos/ert@3725 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/python/ctypes/ert/ecl/ecl_sum.py
+++ devel/python/ctypes/ert/ecl/ecl_sum.py
@@ -27,11 +27,28 @@ libecl/src directory.
 # argument. In the python code this order has been reversed.
 
 
-import numpy
+
 import libecl
 from   ert.cwrap.cwrap       import *
 from   ert.util.stringlist   import StringList
 from   ert.util.ctime        import ctime 
+
+try:
+    import  numpy
+except ImportError:
+    import sys
+    sys.stderr.write("**WARNING** Loading numpy failed - the create3D() method will fail\n")
+    class numpy:
+        int32 = 1
+        float32 = 1
+        float64 = 1
+        
+        def zeros(N, type = None):
+            return [0] * N
+
+
+
+
 #import ert.ecl_plot.sum_plot as sum_plot
 
 # The date2num function is a verbatim copy of the _to_ordinalf()
