a74a85f6183a017d634ad9c1846b88d9b4a4c006
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2012-05-23 20:59:33 +0000

    minor cleanups
    git-svn-id: http://ert.nr.no/repos/ert@3860 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libutil/src/subst_list.h
+++ devel/libutil/src/subst_list.h
@@ -23,39 +23,40 @@
 extern "C" {
 #endif
 #include <stdio.h>
+
 #include <util.h>
 #include <node_data.h>
 #include <subst_func.h>
 
-typedef struct          subst_list_struct subst_list_type;
-void                    subst_list_update_buffer( const subst_list_type * subst_list , buffer_type * buffer );
-void                    subst_list_insert_func(subst_list_type * subst_list , const char * func_name , const char * local_func_name);
-void                    subst_list_fprintf(const subst_list_type * , FILE * stream);
-void                    subst_list_set_parent( subst_list_type * subst_list , const subst_list_type * parent);
-const subst_list_type * subst_list_get_parent( const subst_list_type * subst_list );
-subst_list_type       * subst_list_alloc( const void * input_arg );
-subst_list_type       * subst_list_alloc_deep_copy(const subst_list_type * );
-void                    subst_list_free(subst_list_type *);
-void                    subst_list_clear( subst_list_type * subst_list );
-void                    subst_list_append_copy(subst_list_type *  , const char * , const char * , const char * doc_string);
-void                    subst_list_append_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
-void                    subst_list_append_owned_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
-void                    subst_list_prepend_copy(subst_list_type *  , const char * , const char * , const char * doc_string);
-void                    subst_list_prepend_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
-void                    subst_list_prepend_owned_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
-
-void                  	subst_list_filter_file(const subst_list_type * , const char * , const  char * );
-void                    subst_list_update_file(const subst_list_type * , const char * );
-void                    subst_list_update_string(const subst_list_type * , char ** );
-char                  * subst_list_alloc_filtered_string(const subst_list_type * , const char * );
-void                    subst_list_filtered_fprintf(const subst_list_type * , const char *  , FILE * );
-int                     subst_list_get_size( const subst_list_type *);
-const char            * subst_list_iget_value( const subst_list_type * subst_list , int index);
-const char            * subst_list_iget_key( const subst_list_type * subst_list , int index);
-const char            * subst_list_iget_doc_string( const subst_list_type * subst_list , int index);
-char                  * subst_list_alloc_string_representation( const subst_list_type * subst_list );
-int                     subst_list_add_from_string( subst_list_type * subst_list , const char * arg_string, bool append);
-
+  typedef struct          subst_list_struct subst_list_type;
+  void                    subst_list_update_buffer( const subst_list_type * subst_list , buffer_type * buffer );
+  void                    subst_list_insert_func(subst_list_type * subst_list , const char * func_name , const char * local_func_name);
+  void                    subst_list_fprintf(const subst_list_type * , FILE * stream);
+  void                    subst_list_set_parent( subst_list_type * subst_list , const subst_list_type * parent);
+  const subst_list_type * subst_list_get_parent( const subst_list_type * subst_list );
+  subst_list_type       * subst_list_alloc( const void * input_arg );
+  subst_list_type       * subst_list_alloc_deep_copy(const subst_list_type * );
+  void                    subst_list_free(subst_list_type *);
+  void                    subst_list_clear( subst_list_type * subst_list );
+  void                    subst_list_append_copy(subst_list_type *  , const char * , const char * , const char * doc_string);
+  void                    subst_list_append_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
+  void                    subst_list_append_owned_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
+  void                    subst_list_prepend_copy(subst_list_type *  , const char * , const char * , const char * doc_string);
+  void                    subst_list_prepend_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
+  void                    subst_list_prepend_owned_ref(subst_list_type *  , const char * , const char * , const char * doc_string);
+  
+  void                    subst_list_filter_file(const subst_list_type * , const char * , const  char * );
+  void                    subst_list_update_file(const subst_list_type * , const char * );
+  void                    subst_list_update_string(const subst_list_type * , char ** );
+  char                  * subst_list_alloc_filtered_string(const subst_list_type * , const char * );
+  void                    subst_list_filtered_fprintf(const subst_list_type * , const char *  , FILE * );
+  int                     subst_list_get_size( const subst_list_type *);
+  const char            * subst_list_iget_value( const subst_list_type * subst_list , int index);
+  const char            * subst_list_iget_key( const subst_list_type * subst_list , int index);
+  const char            * subst_list_iget_doc_string( const subst_list_type * subst_list , int index);
+  char                  * subst_list_alloc_string_representation( const subst_list_type * subst_list );
+  int                     subst_list_add_from_string( subst_list_type * subst_list , const char * arg_string, bool append);
+  
 #ifdef __cplusplus 
 }
 #endif
