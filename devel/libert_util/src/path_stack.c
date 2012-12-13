4f42a7b366d68e4d485a918bb4abd3bff57e355e
Joakim Hove <joakim.hove@gmail.com>
2012-12-13 20:19:59 +0100

    Added documentation to path_stack

--- devel/libert_util/src/path_stack.c
+++ devel/libert_util/src/path_stack.c
@@ -26,20 +26,44 @@
 #include <util.h>
 #include <path_stack.h>
 
+/**
+   This file implements the structure path_stack which is vaguely
+   inspired by the emacs-lisp scecial form (save-excursion ...). The
+   intentiation with the 'class' is to push directories onto a stack,
+   and then each time you pop from the stack the the process will
+   chdir() back to directory which is popped.
+
+   Observe that the constructor path_stack_alloc() and destructor
+   path_stack_free() do *not* alter the stack or the cwd of the
+   process.
+*/
+
+
 struct path_stack_struct {
   stringlist_type * stack;
   stringlist_type * storage;
 };
 
 
+/**
+   This will create a new path_stack instance; it will push anything
+   on the current stack of paths. 
+*/
 
 path_stack_type * path_stack_alloc() {
   path_stack_type * path_stack = util_malloc( sizeof * path_stack );
-  path_stack->stack = stringlist_alloc_new();
-  path_stack->storage = stringlist_alloc_new();
-  return path_stack;
+  path_stack->stack = stringlist_alloc_new(); 
+  path_stack->storage = stringlist_alloc_new(); 
+  return path_stack; 
 }
 
+/* 
+   This will destroy the storage taken by the current path_stack
+   instance. This function will NOT pop any elements off the stack; so
+   if you have not manully clerad the stack with the right number of
+   path_stack_pop() calls, you will (probably) destroy the path stack
+   instance with an incorrect value of cwd.  
+*/
 
 void path_stack_free( path_stack_type * path_stack ) {
   stringlist_free( path_stack->stack );
@@ -47,13 +71,17 @@ void path_stack_free( path_stack_type * path_stack ) {
 }
 
 
-
+/**
+   This will push a path on to the stack. The function will start by
+   chdir() to the input path. If the chdir() call fails the function
+   will return false, and the stack will be unmodified; if the chdir()
+   succeeds the input @path will be pushed onto the stack.
+*/
 
 bool path_stack_push( path_stack_type * path_stack , const char * path ) {
-  if (path != NULL) {
-    if (chdir( path ) != 0)
-      return false;
-  }
+  if (chdir( path ) != 0)
+    return false;
+  
   path_stack_push_cwd( path_stack );
   return true;
 }
