da65688dad780a2a49cd5fc5465a0b4d8240fe9f
Joakim Hove <joakim.hove@gmail.com>
2013-02-12 16:03:31 +0100

    ??

--- devel/libert_util/tests/ert_util_approx_equal.c
+++ devel/libert_util/tests/ert_util_approx_equal.c
@@ -32,7 +32,7 @@ int main( int argc , char ** argv) {
   test_assert_double_not_equal( 0.00000000002 , 0.000000000001 );
 
   test_assert_double_equal( 1.00000000002 , 1.000000000001 );
-  test_assert_double_equal( 0.0 , 0.0 );
+  test_assert_double_equal( 0.0 , 0.0 ); 
   test_assert_double_equal( 0.75 , asin( sin(0.75)));
   test_assert_double_equal(  2.25 , exp( log(2.25)));
   test_assert_double_equal(  2.25 , log( exp(2.25)));
