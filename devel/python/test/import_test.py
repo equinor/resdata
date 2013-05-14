09d19ef3702d5ee74dce90f67d30f8d3c911c1b9
Joakim Hove <joakim.hove@gmail.com>
2013-05-14 09:58:03 +0200

    Minor fix in import_test

--- devel/python/test/import_test.py
+++ devel/python/test/import_test.py
@@ -39,7 +39,7 @@ test_import( "ert.enkf" )
 test_import( "ert.sched" )
 test_import( "ert.well")
 
-test_import("ert.ecl.eclX")
+test_import("ert.ecl.ecl")
 test_import("ert.ecl.ecl_local")
 test_import("ert.rms.rms")
 test_import("ert.enkf.enkf")
