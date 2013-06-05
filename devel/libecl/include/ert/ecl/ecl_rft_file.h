403d8619e5bd57eb28050695bf6ffa45023acc55
Joakim Hove <joakim.hove@gmail.com>
2013-06-05 10:02:49 +0200

    Removed unused xml export function

--- devel/libecl/include/ert/ecl/ecl_rft_file.h
+++ devel/libecl/include/ert/ecl/ecl_rft_file.h
@@ -40,7 +40,6 @@ void                   ecl_rft_file_block(const ecl_rft_file_type *  , double , 
 void                   ecl_rft_file_fprintf_rft_obs(const ecl_rft_file_type  * , double , const char * , const char *, const char * , double);
 ecl_rft_node_type    * ecl_rft_file_get_node(const ecl_rft_file_type * , const char * );
 void                   ecl_rft_file_summarize(const ecl_rft_file_type * , bool );
-void                   ecl_rft_file_xml_summary( const ecl_rft_file_type * rft_file );
 
 const ecl_rft_node_type * ecl_rft_file_get_well_time_rft( const ecl_rft_file_type * rft_file , const char * well , time_t recording_time);
 
