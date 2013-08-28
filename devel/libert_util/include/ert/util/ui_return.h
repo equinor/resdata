71ceb81f28c6071ffe2b5f3c1e1b21867daac551
Joakim Hove <joakim.hove@gmail.com>
2013-08-28 13:05:58 +0200

    Using ui_return_type in functions ecl_config_set_data_file() and ecl_config_set_schedule_file()

--- devel/libert_util/include/ert/util/ui_return.h
+++ devel/libert_util/include/ert/util/ui_return.h
@@ -49,6 +49,7 @@ void ui_return_free(ui_return_type * ui_return);
 ui_return_status_enum ui_return_get_status(const ui_return_type * ui_return);
 int ui_return_get_error_count(const ui_return_type * ui_return);
 bool ui_return_add_error(ui_return_type * ui_return, const char * error_msg);
+void ui_return_add_help(ui_return_type * ui_return, const char * help_text);
 const char * ui_return_get_first_error(const ui_return_type * ui_return);
 const char * ui_return_get_last_error(const ui_return_type * ui_return);
 const char * ui_return_get_help(const ui_return_type * ui_return);
