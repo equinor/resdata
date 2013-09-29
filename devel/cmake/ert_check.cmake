7e1c9f018eebec600387abf5276376ac7e82b8a8
Joakim Hove <joakim.hove@gmail.com>
2013-09-29 11:29:07 +0200

    Added compile try_run() test to check mktime() for dates before 1970; setting symbol TIME_T_1970 for environments which can not take dates before 1970.

--- devel/cmake/ert_check.cmake
+++ devel/cmake/ert_check.cmake
@@ -128,6 +128,11 @@ if (ISREG_POSIX)
   add_definitions( -DHAVE_ISREG )
 endif()
 
+try_run( RUN_RESULT COMPILE_RESULT ${CMAKE_BINARY_DIR} ${PROJECT_SOURCE_DIR}/cmake/Tests/test_mktime1970.c )
+if (RUN_RESULT)
+   add_defintions( -DTIME_T_1970 )
+endif()
+
 set( CMAKE_C_FLAGS ${CMAKE_C_FLAGS_main} )
 set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS_main} )
 
