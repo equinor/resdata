94d0d09185803f374f72ffe22d223e38e56ffae6
Jean-Paul Balabanian <jpb@statoil.com>
2013-12-13 09:54:12 +0100

    ERT-425 - Added histograms. Initial version.

--- devel/share/gui/plots/scripts/histogram.js
+++ devel/share/gui/plots/scripts/histogram.js
@@ -18,9 +18,11 @@
 function Histogram(element) {
     var stored_data = [];
     var margin = {left: 90, right: 20, top: 20, bottom: 30};
-    var duration = 250;
-    var bins = 20;
+    var bin_count = 30;
     var histogram_style = "";
+
+    var count_format = d3.format("d");
+    var value_format = d3.format("s");
 
     var group = element.append("div")
         .attr("class", "plot");
@@ -30,35 +32,42 @@ function Histogram(element) {
         .attr("class", "histogram-title")
         .text("Histogram");
 
-    var histogram_group = group.append("svg")
-        .attr("class", "histogram-svg");
+    var histogram_area = group.append("div").attr("class", "plot-area");
+
+    var histogram_group = histogram_area.append("svg")
+        .attr("class", "plot-svg");
 
     var width = 384 - margin.left - margin.right;
     var height = 256 - margin.top - margin.bottom;
 
-    var svg = histogram_group
-        .append("g")
+    var svg = histogram_group.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .style("width", width + "px")
         .style("height", height + "px");
 
+    var canvas = histogram_area.append("canvas")
+        .attr("id", "histogram-canvas")
+        .attr("width", width)
+        .attr("height", height)
+        .style("position", "absolute")
+        .style("top", (margin.top) + "px")
+        .style("left", (margin.left) + "px")
+        .style("z-index", 5);
 
-    var x_scale = d3.time.scale().range([0, width]);
+
+    var x_scale = d3.scale.linear().range([0, width]).nice();
     var y_scale = d3.scale.linear().range([height, 0]).nice();
 
     var y_axis = d3.svg.axis()
         .scale(y_scale)
-        .ticks(10)
-        .tickPadding(10)
+        .tickFormat(count_format)
         .orient("left")
         .tickSize(-width, -width);
 
     var x_axis = d3.svg.axis()
         .scale(x_scale)
-        .ticks(10)
-        .tickPadding(10)
-        .orient("bottom")
-        .tickSubdivide(false);
+        .tickFormat(value_format)
+        .orient("bottom");
 
     histogram_group.append("g")
         .attr("class", "y axis pale")
@@ -70,50 +79,74 @@ function Histogram(element) {
         .attr("transform", "translate(" + margin.left + ", " + (height + margin.top) + ")")
         .call(x_axis);
 
+    var rect_renderer = CanvasRect().x(x_scale).y(y_scale).margin(0, 1, 0, 1);
 
-    function histogram(selection) {
-//        selection.enter().append("g").attr("class", "histogram");
-//
-//        selection.exit().transition().duration(duration).style("opacity", 0).remove();
-//
-//        selection.selectAll(".histogram").data(function(d) {return [d];});
-//
-//
-//        var area = d3.svg.area().x(x).y0(y_min).y1(y_max).interpolate("basis");
-//
-//        selection.transition()
-//            .duration(duration)
-//            .attr("d", area);
-//
-//        selection.enter()
-//            .append("path")
-//            .attr("class", histogram_style)
-//            .attr("d", area)
-//            .style("opacity", 0.0)
-//            .transition()
-//            .duration(duration)
-//            .style("opacity", 1.0);
-//
-//        selection.exit()
-//            .transition()
-//            .duration(duration)
-//            .style("opacity", 0.0)
-//            .remove();
 
-        histogram_group.select(".y.axis").transition().duration(duration).call(y_axis);
-        histogram_group.select(".x.axis").transition().duration(duration).call(x_axis);
+    var setYDomain = function(min_y, max_y) {
+        y_scale.domain([min_y, max_y]).nice();
 
+        if(y_scale.domain()[1] == max_y) {
+            y_scale.domain([min_y, max_y + 1]).nice();
+        }
+    };
+
+    var setXDomain = function(min_x, max_x) {
+        var min = min_x;
+        var max = max_x;
+
+//        if (this.custom_y_min != null) {
+//            min = this.custom_y_min;
+//        }
+//
+//        if (this.custom_y_max != null) {
+//            max = this.custom_y_max;
+//        }
+
+        x_scale.domain([min, max]).nice();
+    };
+
+
+    function histogram(data) {
+        stored_data = data;
+        title.text(data.name());
+
+        if(data.hasBoundaries()) {
+            setXDomain(data.minY(), data.maxY());
+        }
+
+        if(data.hasEnsembleData()) {
+            var context = canvas.node().getContext("2d");
+            context.save();
+            context.clearRect(0, 0, width, height);
+
+            var case_list = data.caseList();
+
+            for(var case_index = 0; case_index < case_list.length; case_index++)  {
+                var case_name = case_list[case_index];
+                var samples = data.reportStepSamples(case_name);
+                var style = STYLES[("ensemble_" + (case_index + 1))];
+
+                var bins = d3.layout.histogram().bins(x_scale.ticks(bin_count))(samples);
+                setYDomain(0, d3.max(bins, function(d) { return d.y; }));
+
+                rect_renderer.style(style);
+                for(var i = 0; i < bins.length; i++) {
+                    var value = bins[i];
+                    rect_renderer(context, value.x, 0, value.x + value.dx, value.y);
+                }
+            }
+
+            context.restore();
+        }
+
+
+        histogram_group.select(".y.axis").transition().duration(0).call(y_axis);
+        histogram_group.select(".x.axis").transition().duration(0).call(x_axis);
     }
 
     histogram.bins = function(value) {
-        if (!arguments.length) return bins;
-        bins = value;
-        return histogram;
-    };
-
-    histogram.duration = function(value) {
-        if (!arguments.length) return duration;
-        duration = value;
+        if (!arguments.length) return bin_count;
+        bin_count = value;
         return histogram;
     };
 
@@ -123,6 +156,8 @@ function Histogram(element) {
         histogram_style = value;
         return histogram;
     };
+
+
 
     histogram.resize = function(w, h) {
         w = w - 80;
@@ -134,10 +169,12 @@ function Histogram(element) {
         x_scale.range([0, width]);
         y_scale.range([height, 0]).nice();
 
-        y_axis.tickSize(-10, -10);
+        y_axis.tickSize(-width, -width);
 
         histogram_group.style("width", w + "px");
         histogram_group.style("height", h + "px");
+
+        canvas.attr("width", width).attr("height", height);
 
         svg.style("width", width + "px");
         svg.style("height", height + "px");
