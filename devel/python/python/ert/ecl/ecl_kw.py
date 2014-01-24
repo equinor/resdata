d902cadcef6be0ff165cfbc1338fa486ee39375b
Per Røe <Per.Roe@nr.no>
2014-01-24 09:28:26 +0100

    property type defined twice.

--- devel/python/python/ert/ecl/ecl_kw.py
+++ devel/python/python/ert/ecl/ecl_kw.py
@@ -810,10 +810,6 @@ class EclKW(CClass):
 
     
     @property
-    def type( self ):
-        return self.ecl_type
-
-    @property
     def type_name( self ):
         return EclUtil.type_name( self.ecl_type )
     
