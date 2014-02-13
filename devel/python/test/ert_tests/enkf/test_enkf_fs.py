a2b3c80e1e1776940e7835c2e94c7feec0959865
Joakim Hove <joakim.hove@gmail.com>
2014-02-13 10:42:56 +0100

    ERT-518: Added python testing of enkf_fs.

--- devel/python/test/ert_tests/enkf/test_enkf_fs.py
+++ devel/python/test/ert_tests/enkf/test_enkf_fs.py
@@ -1,11 +1,14 @@
 import os
-from ert.enkf.enkf_fs import EnkfFs
+from ert.enkf import EnkfFs
+from ert.enkf import EnKFMain
+from ert.enkf import ErtTestContext
 from ert_tests import ExtendedTestCase
+
 
 class EnKFFSTest(ExtendedTestCase):
     def setUp(self):
         self.mount_point = self.createTestPath("Statoil/config/with_data/storage/default")
-        
+        self.config_file = self.createTestPath("Statoil/config/with_data/config")
 
     def test_create(self):
         print self.mount_point
@@ -17,6 +20,15 @@ class EnKFFSTest(ExtendedTestCase):
         with self.assertRaises(Exception):
             fs = EnkfFs("/does/not/exist")
             
+    
+    def test_refcount(self):
+        with ErtTestContext("TEST" , self.config_file) as testContext:
+            ert = testContext.getErt()
+            self.assertTrue( isinstance( ert , EnKFMain ))
 
+            fsm = ert.getEnkfFsManager()
+            fs = fsm.getFileSystem()
+            self.assertEqual( 2 , fs.refCount())
 
+            
 
