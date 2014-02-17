f15f35d455c9b2f7e9ac64f74b88459e6a50d6b2
Joakim Hove <joakim.hove@gmail.com>
2014-02-17 10:18:21 +0100

    ERT-518: Added testing of function enkf_fs_exists().

--- devel/python/test/ert_tests/enkf/test_enkf_fs.py
+++ devel/python/test/ert_tests/enkf/test_enkf_fs.py
@@ -2,8 +2,10 @@ import os
 from ert.enkf import EnkfFs
 from ert.enkf import EnKFMain
 from ert.enkf import ErtTestContext
+from ert.enkf.enums import EnKFFSType
 from ert_tests import ExtendedTestCase
 from ert.util.test_area import TestAreaContext
+
 
 class EnKFFSTest(ExtendedTestCase):
     def setUp(self):
@@ -11,12 +13,23 @@ class EnKFFSTest(ExtendedTestCase):
         self.config_file = self.createTestPath("Statoil/config/with_data/config")
         
 
+    def test_id_enum(self):
+        self.assertEnumIsFullyDefined(EnKFFSType , "fs_driver_impl", "libenkf/include/ert/enkf/fs_types.h")
+
+
     def test_create(self):
         with TestAreaContext("create_fs") as work_area:
             work_area.copy_parent_content( self.config_file )
 
+            self.assertTrue( EnkfFs.exists( self.mount_point ))
             fs = EnkfFs( self.mount_point )
             self.assertEqual( 1 , fs.refCount() ) 
+            fs.umount()
+
+            self.assertFalse( EnkfFs.exists( "newFS" ))
+            arg = None
+            EnkfFs.createFS( "newFS" , EnKFFSType.BLOCK_FS_DRIVER_ID , arg)
+            self.assertTrue( EnkfFs.exists( "newFS" ))
             
 
         
