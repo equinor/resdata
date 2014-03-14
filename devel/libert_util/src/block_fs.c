8671c45bbb31b5cd55a989d9005b708b8720c460
Joakim Hove <joakim.hove@gmail.com>
2014-03-14 12:00:57 +0100

    ERT-530: Changed locking + read_only behaviour slightly; is use_lockfile == true you will get a lockfile even if read_only == true.

--- devel/libert_util/src/block_fs.c
+++ devel/libert_util/src/block_fs.c
@@ -649,7 +649,7 @@ static block_fs_type * block_fs_alloc_empty( const char * mount_file ,
                                              float fragmentation_limit,
                                              int fsync_interval ,
                                              bool read_only,
-                                             bool block_level_lock) {
+                                             bool use_lockfile) {
   block_fs_type * block_fs      = util_malloc( sizeof * block_fs );
   UTIL_TYPE_ID_INIT(block_fs , BLOCK_FS_TYPE_ID);
   
@@ -678,19 +678,24 @@ static block_fs_type * block_fs_alloc_empty( const char * mount_file ,
   block_fs->index_file  = NULL;
   block_fs_reinit( block_fs );
 
-  if (read_only)
-    block_fs->data_owner = false;
-  else if (block_level_lock) {
-    block_fs->data_owner = util_try_lockf( block_fs->lock_file , S_IWUSR + S_IWGRP , &block_fs->lock_fd);
-    block_fs->index_time = time( NULL );
+
+  {
+    bool lock_aquired = true;
+
+    if (use_lockfile) {
+      lock_aquired = util_try_lockf( block_fs->lock_file , S_IWUSR + S_IWGRP , &block_fs->lock_fd);
+      block_fs->index_time = time( NULL );
     
-    if (!block_fs->data_owner) 
-      fprintf(stderr," Another program has already opened filesystem read-write - this instance will be UNSYNCRONIZED read-only. Cross your fingers ....\n");
-    fflush( stderr );
-  } else {
-    block_fs->data_owner = true; //locked on fs level
+      if (!lock_aquired) 
+        fprintf(stderr," Another program has already opened filesystem read-write - this instance will be UNSYNCRONIZED read-only. Cross your fingers ....\n");
+    }
+
+    if (lock_aquired && (read_only == false))
+      block_fs->data_owner = true; 
+    else
+      block_fs->data_owner = false;
+
   }
-  
   return block_fs;
 }
   
@@ -1051,6 +1056,14 @@ bool block_fs_is_mount( const char * mount_file ) {
 }
 
 
+bool block_fs_is_readonly( const block_fs_type * bfs ) {
+  if (bfs->data_owner)
+    return false;
+  else
+    return true;
+}
+
+
 
 block_fs_type * block_fs_mount( const char * mount_file ,
                                 int block_size ,
@@ -1059,7 +1072,7 @@ block_fs_type * block_fs_mount( const char * mount_file ,
                                 int fsync_interval ,
                                 bool preload ,
                                 bool read_only,
-                                bool block_level_lock) {
+                                bool use_lockfile) {
   block_fs_type * block_fs;
   {
 
@@ -1068,7 +1081,7 @@ block_fs_type * block_fs_mount( const char * mount_file ,
       block_fs_fwrite_mount_info__( mount_file , 0 );
     {
       long_vector_type * fix_nodes = long_vector_alloc(0 , 0);
-      block_fs = block_fs_alloc_empty( mount_file , block_size , max_cache_size , fragmentation_limit , fsync_interval , read_only, block_level_lock);
+      block_fs = block_fs_alloc_empty( mount_file , block_size , max_cache_size , fragmentation_limit , fsync_interval , read_only, use_lockfile);
       /* We build up the index & free_nodes_list based on the header/index information embedded in the datafile. */
       block_fs_open_data( block_fs , false );
       if (block_fs->data_stream != NULL) {
