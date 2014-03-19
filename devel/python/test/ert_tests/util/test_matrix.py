e9c8d0cc7b1229c2b9d0bb99354045f08d26d409
jepebe <GitHubMongo80>
2014-03-19 08:54:15 +0100

    ERT-356 - Added functionality to Matrix class

--- devel/python/test/ert_tests/util/test_matrix.py
+++ devel/python/test/ert_tests/util/test_matrix.py
@@ -5,6 +5,9 @@ class MatrixTest(ExtendedTestCase):
     def test_matrix(self):
         m = Matrix(2, 3)
 
+        self.assertEqual(m.rows(), 2)
+        self.assertEqual(m.columns(), 3)
+
         self.assertEqual(m[(0, 0)], 0)
 
         m[(1, 1)] = 1.5
@@ -23,7 +26,24 @@ class MatrixTest(ExtendedTestCase):
             m[0, 3] = 0
 
 
-        #todo: random crashes happens if we do this...
-        #m[2, 2] = 2 # no exception raised...
 
+    def test_matrix_equality(self):
+        m = Matrix(2, 2)
+        m[0, 0] = 2
+        m[1, 1] = 4
 
+        s = Matrix(2, 3)
+        s[0, 0] = 2
+        s[1, 1] = 4
+
+        self.assertNotEqual(m, s)
+
+        r = Matrix(2, 2)
+        r[0, 0] = 2
+        r[1, 1] = 3
+
+        self.assertNotEqual(m, r)
+
+        r[1, 1] = 4
+
+        self.assertEqual(m, r)
