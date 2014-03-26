e2931253b994dfdd2f445265dd64dccfdcf2fe90
jepebe <jepebe@users.noreply.github.co>
2014-03-26 09:08:15 +0100

    ERT-356 - Fixed enkf_obs

--- devel/python/test/ert_tests/enkf/test_enkf_obs.py
+++ devel/python/test/ert_tests/enkf/test_enkf_obs.py
@@ -1,39 +1,36 @@
-import os
-from ert.enkf import EnkfFs
 from ert.enkf import ObsVector
-from ert.enkf import EnKFMain
-from ert.enkf import EnkfFsManager
 from ert.enkf import ErtTestContext
 from ert.util import BoolVector
 from ert_tests import ExtendedTestCase
+
 
 class EnKFObsTest(ExtendedTestCase):
     def setUp(self):
         self.config_file = self.createTestPath("Statoil/config/obs_testing/config")
 
     def testObs(self):
-        with ErtTestContext("obs_test" , self.config_file) as testContext:
-            ert = testContext.getErt()
+        with ErtTestContext("obs_test", self.config_file) as test_context:
+            ert = test_context.getErt()
             obs = ert.getObservations()
 
-            self.assertEqual(31 , len(obs))
+            self.assertEqual(31, len(obs))
             for v in obs:
-                self.assertTrue( isinstance(v , ObsVector ))
-                
-            with self.assertRaises(ValueError):
+                self.assertTrue(isinstance(v, ObsVector))
+
+            with self.assertRaises(IndexError):
                 v = obs[-1]
 
             with self.assertRaises(IndexError):
                 v = obs[40]
-                
+
             with self.assertRaises(KeyError):
                 v = obs["No-this-does-not-exist"]
-            
+
             v1 = obs["WWCT:OP_3"]
             v2 = obs["GOPT:OP"]
-            mask = BoolVector(True , ert.getEnsembleSize() )
-            currentFS = ert.getEnkfFsManager().getCurrentFileSystem()
+            mask = BoolVector(True, ert.getEnsembleSize())
+            current_fs = ert.getEnkfFsManager().getCurrentFileSystem()
 
-            self.assertTrue( v1.hasData( mask , currentFS ))
-            self.assertFalse( v2.hasData( mask , currentFS))
+            self.assertTrue(v1.hasData(mask, current_fs))
+            self.assertFalse(v2.hasData(mask, current_fs))
             
