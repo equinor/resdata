f9e94ea205cb667d9ffa407e45d89b28fa7e1573
chflo <chflo@statoil.com>
2014-04-15 15:16:46 +0200

    ERT-565: Added new method util_sscanf_percent(...)

--- devel/libert_util/src/util.c
+++ devel/libert_util/src/util.c
@@ -3053,6 +3053,29 @@ bool util_sscanf_date(const char * date_token , time_t * t) {
 }
 
 
+bool util_sscanf_percent(const char * percent_token, double * value) {
+  int    integer_number = 0;
+  float  float_number   = 0.0;
+  char * percentage     = util_alloc_sprintf("");
+  bool   ret            = false;
+
+  if (2 == sscanf(percent_token, "%f%s", &float_number, percentage)) {
+    if (0 == strcmp(percentage, "%")) {
+      *value = float_number/100.0;
+      ret = true;
+    }
+  } else if (2 == sscanf(percent_token,"%d%s", &integer_number, percentage)) {
+    if (0 == strcmp(percentage, "%")) {
+      *value = integer_number/100;
+      ret = true;
+    }
+  }
+
+  free(percentage);
+  return ret;
+}
+
+
 bool util_fscanf_date(FILE *stream , time_t *t)  {
   int init_pos = util_ftell(stream);
   char * date_token = util_fscanf_alloc_token(stream);
