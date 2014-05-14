740493d601c043d0237e14de2ea3945e20f553a6
jepebe <jepebe@users.noreply.github.co>
2014-05-14 12:17:50 +0200

    ERT-572 - Added ERT compile time version tracking

---
+++ devel/libert_util/src/version.c
@@ -0,0 +1,34 @@
+#define DOT "."
+
+#ifdef ERT_VERSION_MAJOR
+#ifdef ERT_VERSION_MINOR
+#ifdef ERT_VERSION_MICRO
+    #define ERT_VERSION ERT_VERSION_MAJOR DOT ERT_VERSION_MINOR DOT ERT_VERSION_MICRO
+#endif
+#endif
+#endif
+
+
+char* version_get_git_commit() {
+    #ifdef GIT_COMMIT
+        return GIT_COMMIT;
+    #else
+        return "Unknown git commit hash";
+    #endif
+}
+
+char* version_get_ert_version() {
+    #ifdef ERT_VERSION
+        return ERT_VERSION;
+    #else
+        return "Unknown version";
+    #endif
+}
+
+char* version_get_build_time() {
+    #ifdef COMPILE_TIME_STAMP
+        return COMPILE_TIME_STAMP;
+    #else
+        return "Unknown build time";
+    #endif
+}
