e4acdf44a19ec0e51242900d6612b64ac5937287
Kristian Flikka <kristian.flikka@gmail.com>
2014-05-28 11:06:21 +0200

    ERT-651 Added python tests for GenDataConfig, to test the active_mask when swapping cases. Updated active_list representation to be valid also when no _active file exists on disk.

---
+++ devel/python/test/ert_tests/enkf/data/test_gen_data_config.py
@@ -0,0 +1,54 @@
+from compiler.ast import Node
+from ert.cwrap import clib, CWrapper
+from ert.enkf.data.enkf_node import EnkfNode
+from ert.enkf.enums.enkf_state_type_enum import EnkfStateType
+from ert.enkf.node_id import NodeId
+from ert.test import ErtTestContext
+from ert.test.extended_testcase import ExtendedTestCase
+from ert.enkf.data import GenDataConfig
+
+test_lib  = clib.ert_load("libenkf")
+cwrapper =  CWrapper(test_lib)
+
+get_active_mask = cwrapper.prototype("bool_vector_ref gen_data_config_get_active_mask( gen_data_config )")
+
+class GenDataConfigTest(ExtendedTestCase):
+    def setUp(self):
+        self.config_file = self.createTestPath("Statoil/config/with_GEN_DATA/config")
+
+    def load_active_masks(self, case1, case2 ):
+        with ErtTestContext("gen_data_config_test", self.config_file) as test_context:
+            ert = test_context.getErt()
+
+            fs =  ert.getEnkfFsManager().getFileSystem(case1)
+            ert.getEnkfFsManager().switchFileSystem(fs) #Is necessary due to a config->fs bug in gen_data_config.c
+
+            config_node = ert.ensembleConfig().getNode("TIMESHIFT")
+            data_node = EnkfNode(config_node)
+            data_node.tryLoad(fs, NodeId(60, 0, EnkfStateType.FORECAST))
+
+            first_active_mask = get_active_mask( config_node.getDataModelConfig() )
+            first_active_mask_length = len(first_active_mask)
+            self.assertEqual(first_active_mask_length, 2560)
+
+            fs =  ert.getEnkfFsManager().getFileSystem(case2)
+            ert.getEnkfFsManager().switchFileSystem(fs) #Is necessary due to a config->fs bug in gen_data_config.c
+
+            data_node = EnkfNode(config_node)
+            data_node.tryLoad(fs, NodeId(60, 0, EnkfStateType.FORECAST))
+            second_active_mask = get_active_mask( config_node.getDataModelConfig() )
+            self.assertEqual(len(second_active_mask), 2560)
+
+            self.assertEqual(first_active_mask_length, len(second_active_mask))
+
+
+    def test_loading_case_with_active_file(self):
+        self.load_active_masks("default", "missing-active")
+        self.load_active_masks("missing-active", "default")
+
+
+
+
+
+
+
