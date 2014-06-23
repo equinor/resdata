81152306fe4281cdd20e2fce65e25d0aba8e0f7f
Joakim Hove <joakim.hove@gmail.com>
2014-06-23 13:32:41 +0200

    ERT-697: Added init_points argument to Polyline

--- devel/python/python/ert/geo/polyline.py
+++ devel/python/python/ert/geo/polyline.py
@@ -1,8 +1,13 @@
+import collections
+
+
 class Polyline(object):
-    def __init__(self, name="Unnamed"):
+    def __init__(self, name="Unnamed" , init_points = None):
         super(Polyline, self).__init__()
         self.__name = name
         self.__points = []
+        if init_points:
+            self.__loadPoints( init_points )
 
     def name(self):
         """ @rtype: str """
@@ -28,6 +33,21 @@ class Polyline(object):
         first = self[0]
         last = self[len(self) - 1]
         return first == last
+
+    def __loadPoints(self , points):
+        if not isinstance( points , collections.Iterable ):
+            raise TypeError("The input argument points must be iterable")
+            
+        for point in points:
+            x = point[0]
+            y = point[1]
+            try:
+                z = point[2]
+            except IndexError:
+                z = None
+            
+            self.addPoint(x,y,z)
+                
 
 
     def __iter__(self):
