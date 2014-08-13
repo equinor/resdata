15a096a5f886b8d04f13a9da38cd23e1274aec9b
Joakim Hove <joakim.hove@gmail.com>
2014-08-13 13:55:45 +0200

    ERT-700: misc poly changes

--- devel/python/python/ert/geo/xyz_reader.py
+++ devel/python/python/ert/geo/xyz_reader.py
@@ -16,9 +16,35 @@ class XYZReader(object):
 
         with open(path, "r") as f:
             for line in f:
-                x, y, z = map(float, line.split())
-
-                if x != 999.000000 and y != 999.000000 and z != 999.000000:
-                    polyline.addPoint(x, y, z)
+                line = line.strip()
+                if line:
+                    x, y, z = map(float, line.split())
+                
+                    if x != 999.000000 and y != 999.000000 and z != 999.000000:
+                        polyline.addPoint(x, y, z)
+                    else:
+                        break
+                else:
+                    break
 
         return polyline
+
+        
+    @staticmethod
+    def readXYFile(path):
+        """ @rtype: Polyline """
+
+        if not os.path.exists(path):
+            raise ValueError("Path does not exist '%s'!" % path)
+
+        name = os.path.basename(path)
+
+        polyline = Polyline(name=name)
+
+        with open(path, "r") as f:
+            for line in f:
+                x, y= map(float, line.split())
+                polyline.addPoint(x, y)
+
+        return polyline
+
