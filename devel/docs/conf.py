a92a9f0d40ecbd959a94647017f96a880c7eeeaa
iLoop2 <fredrik.gundersen@mac.com>
2014-08-14 19:29:40 +0200

    ERT-600 Final move of documents, added script to build documentation

--- devel/docs/conf.py
+++ devel/docs/conf.py
@@ -19,7 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../devel/python/python'))
+sys.path.insert(0, os.path.abspath('../python/python'))
 
 # -- General configuration ------------------------------------------------
 
