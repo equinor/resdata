873382bb4746091affa5bcbbc2ebd0ba4ca0d95b
chflo <chflo@statoil.com>
2014-08-26 10:10:54 +0200

    ERT-710 / RO-12: Made test independent of StatoilData, added some more fault testing

--- devel/python/test/ert_tests/enkf/data/test_gen_kw.py
+++ devel/python/test/ert_tests/enkf/data/test_gen_kw.py
@@ -1,24 +1,18 @@
-from ert.cwrap import clib, CWrapper
-from ert.enkf.data import GenKw
-from ert.enkf.data import GenKwConfig
-from ert.test import ErtTestContext
-from ert.test.extended_testcase import ExtendedTestCase
-
-test_lib  = clib.ert_load("libenkf")
-cwrapper =  CWrapper(test_lib)
+from ert.enkf.data import GenKw, GenKwConfig
+from ert.test import TestAreaContext, ExtendedTestCase
 
 
-class GenKwTest(ExtendedTestCase):
-    def setUp(self):
-        self.config_file = self.createTestPath("Statoil/config/forward/config_GEN_KW_true")
+class GenKwResOptTest(ExtendedTestCase):
 
     def test_gen_kw_get_set(self):
-        with ErtTestContext("gen_kw_test", self.config_file) as test_context:
-            ert = test_context.getErt()
+        with TestAreaContext("enkf/data/gen_kw_res_opt"):
+            parameter_file ="MULTFLT.txt"
+            with open(parameter_file, "w") as f:
+                f.write("MULTFLT  NORMAL  0   1")
 
-            enkf_config_node = ert.ensembleConfig().getNode("MULTFLT")
-            gen_kw_config = enkf_config_node.getModelConfig()
+            gen_kw_config = GenKwConfig("MULTFLT", "%s")
             self.assertIsInstance(gen_kw_config, GenKwConfig)
+            gen_kw_config.set_parameter_file(parameter_file)
 
             gen_kw = GenKw(gen_kw_config)
             self.assertIsInstance(gen_kw, GenKw)
@@ -29,6 +23,21 @@ class GenKwTest(ExtendedTestCase):
             gen_kw["MULTFLT"] = 4.0
             self.assertEqual(gen_kw["MULTFLT"], 4.0)
             self.assertEqual(gen_kw[0], 4.0)
+
+            self.assertEqual(len(gen_kw), 1)
+
+            with self.assertRaises(IndexError):
+                gen_kw[1]
+
+            with self.assertRaises(TypeError):
+                gen_kw[1.5]
+
+            with self.assertRaises(KeyError):
+                gen_kw["MULTFLT_2"]
+
+
+
+
 
 
 
