2bee0ba9af74008dcdb9523eb0103de04961ae41
chflo <chflo@statoil.com>
2014-09-17 19:19:44 +0200

    ERT-624: Added current dir argument to runTestsInDirectory call

--- devel/python/test/ert_tests/run_tests.py
+++ devel/python/test/ert_tests/run_tests.py
@@ -6,4 +6,4 @@ from ert.test import ErtTestRunner
 sys.path.append( os.path.realpath( os.path.join(os.path.dirname( os.path.abspath( __file__)) , "../") ))
 
 if __name__ == '__main__':
-    ErtTestRunner.runTestsInDirectory()
+    ErtTestRunner.runTestsInDirectory(".")
