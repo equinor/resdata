50df53fa23cbf205aff1a2c2ff019678ca0304f1
Joakim Hove <joakim.hove@gmail.com>
2014-09-26 19:43:39 +0200

    fortio: moved declaration to top of function.

--- devel/libecl/src/fortio.c
+++ devel/libecl/src/fortio.c
@@ -490,13 +490,14 @@ void fortio_data_fseek(fortio_type* fortio, offset_type data_offset, size_t data
     if(data_element < 0 || data_element >= element_count) {
         util_abort("%s: Element index is out of range: 0 <= %d < %d \n", __func__, data_element, element_count);
     }
+    {
+      int block_index = data_element / block_size;
+      int headers = (block_index + 1) * 4;
+      int trailers = block_index * 4;
+      int bytes_to_skip = data_offset + headers + trailers + (data_element * element_size);
 
-    int block_index = data_element / block_size;
-    int headers = (block_index + 1) * 4;
-    int trailers = block_index * 4;
-    int bytes_to_skip = data_offset + headers + trailers + (data_element * element_size);
-
-    fortio_fseek(fortio, bytes_to_skip, SEEK_SET);
+      fortio_fseek(fortio, bytes_to_skip, SEEK_SET);
+    }
 }
 
 
