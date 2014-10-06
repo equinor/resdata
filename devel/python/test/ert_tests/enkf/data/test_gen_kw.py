5ed5314d3079bf25313b9b458b9292903d11417d
Joakim Hove <joakim.hove@gmail.com>
2014-10-06 16:53:08 +0200

    ERT-753: Ability to set vector of GEN_KW values

--- devel/python/test/ert_tests/enkf/data/test_gen_kw.py
+++ devel/python/test/ert_tests/enkf/data/test_gen_kw.py
@@ -2,6 +2,23 @@ from ert.enkf.data import GenKw, GenKwConfig
 from ert.test import TestAreaContext, ExtendedTestCase
 
 
+def create_gen_kw():
+    parameter_file ="MULTFLT.txt"
+    template_file ="MULTFLT.tmpl"
+    with open(parameter_file, "w") as f:
+        f.write("MULTFLT1  NORMAL  0   1\n")
+        f.write("MULTFLT2  RAW \n")
+        f.write("MULTFLT3  NORMAL  0   1\n")
+
+    with open(template_file, "w") as f:
+        f.write("<MULTFLT1> <MULTFLT2> <MULTFLT3>\n")
+        f.write("/\n")
+        
+
+    gen_kw_config = GenKwConfig("MULTFLT", template_file , parameter_file)
+    gen_kw = GenKw( gen_kw_config )
+    
+    return (gen_kw_config , gen_kw)
 class GenKwTest(ExtendedTestCase):
 
     def test_gen_kw_get_set(self):
@@ -49,7 +66,24 @@ class GenKwTest(ExtendedTestCase):
 
 
 
+    def test_gen_kw_get_set_vector(self):
+        with TestAreaContext("enkf/data/gen_kwt"):
+            
+            (gen_kw_config , gen_kw) = create_gen_kw()
+            with self.assertRaises(ValueError):
+                gen_kw.setValues([0])
 
+            with self.assertRaises(TypeError):
+                gen_kw.setValues(["A","B","C"])
+                
+            gen_kw.setValues([0,1,2])
+            self.assertEqual(gen_kw[0], 0)
+            self.assertEqual(gen_kw[1], 1)
+            self.assertEqual(gen_kw[2], 2)
+
+            self.assertEqual(gen_kw["MULTFLT1"] , 0)
+            self.assertEqual(gen_kw["MULTFLT2"] , 1)
+            self.assertEqual(gen_kw["MULTFLT3"] , 2)
 
 
 
