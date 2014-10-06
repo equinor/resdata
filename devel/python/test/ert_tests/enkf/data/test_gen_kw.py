e71e171235e58cb3d317e9d366bee6191c7bbde9
Joakim Hove <joakim.hove@gmail.com>
2014-10-06 16:58:58 +0200

    ERT-753: Can instantiate gen_kw_config correctly from Python

--- devel/python/test/ert_tests/enkf/data/test_gen_kw.py
+++ devel/python/test/ert_tests/enkf/data/test_gen_kw.py
@@ -1,3 +1,4 @@
+import os.path
 from ert.enkf.data import GenKw, GenKwConfig
 from ert.test import TestAreaContext, ExtendedTestCase
 
@@ -19,30 +20,36 @@ def create_gen_kw():
     gen_kw = GenKw( gen_kw_config )
     
     return (gen_kw_config , gen_kw)
+
+
+
 class GenKwTest(ExtendedTestCase):
 
     def test_gen_kw_get_set(self):
         with TestAreaContext("enkf/data/gen_kwt"):
-            parameter_file ="MULTFLT.txt"
-            with open(parameter_file, "w") as f:
-                f.write("MULTFLT  NORMAL  0   1")
-
-            gen_kw_config = GenKwConfig("MULTFLT", "%s", "MULTFLT.txt")
-
-            gen_kw = GenKw(gen_kw_config)
+            
+            (gen_kw_config , gen_kw) = create_gen_kw()
             self.assertIsInstance(gen_kw, GenKw)
 
             gen_kw[0] = 3.0
             self.assertEqual(gen_kw[0], 3.0)
 
-            gen_kw["MULTFLT"] = 4.0
-            self.assertEqual(gen_kw["MULTFLT"], 4.0)
+            gen_kw["MULTFLT1"] = 4.0
+            self.assertEqual(gen_kw["MULTFLT1"], 4.0)
             self.assertEqual(gen_kw[0], 4.0)
 
-            self.assertEqual(len(gen_kw), 1)
+            gen_kw["MULTFLT2"] = 8.0
+            self.assertEqual(gen_kw["MULTFLT2"], 8.0)
+            self.assertEqual(gen_kw[1], 8.0)
+
+            gen_kw["MULTFLT3"] = 12.0
+            self.assertEqual(gen_kw["MULTFLT3"], 12.0)
+            self.assertEqual(gen_kw[2], 12.0)
+
+            self.assertEqual(len(gen_kw), 3)
 
             with self.assertRaises(IndexError):
-                gen_kw[1]
+                gen_kw[4]
 
             with self.assertRaises(TypeError):
                 gen_kw[1.5]
@@ -50,19 +57,7 @@ class GenKwTest(ExtendedTestCase):
             with self.assertRaises(KeyError):
                 gen_kw["MULTFLT_2"]
 
-            self.assertTrue("MULTFLT" in gen_kw )
-
-
-
-
-
-
-
-
-
-
-
-
+            self.assertTrue("MULTFLT1" in gen_kw )
 
 
 
@@ -84,6 +79,11 @@ class GenKwTest(ExtendedTestCase):
             self.assertEqual(gen_kw["MULTFLT1"] , 0)
             self.assertEqual(gen_kw["MULTFLT2"] , 1)
             self.assertEqual(gen_kw["MULTFLT3"] , 2)
+    
+
+        
+    
+
     def test_gen_kw_ecl_write(self):
         with TestAreaContext("enkf/data/gen_kwt"):
             (gen_kw_config , gen_kw) = create_gen_kw()
