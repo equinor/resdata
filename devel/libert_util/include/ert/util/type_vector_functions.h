6ac9de4690c0d8a5cb49d274cbde66a87b313ff3
Joakim Hove <joakim.hove@gmail.com>
2014-12-01 13:58:14 +0100

    ERT-815: Added function for approximate comparison of double_vector

--- devel/libert_util/include/ert/util/type_vector_functions.h
+++ devel/libert_util/include/ert/util/type_vector_functions.h
@@ -23,11 +23,13 @@ extern "C" {
 #endif
 #include <ert/util/int_vector.h>
 #include <ert/util/bool_vector.h>
+#include <ert/util/double_vector.h>
   
-  int_vector_type * bool_vector_alloc_active_list( const bool_vector_type * mask );
+  int_vector_type  * bool_vector_alloc_active_list( const bool_vector_type * mask );
   bool_vector_type * int_vector_alloc_mask( const int_vector_type * active_list );
-  int_vector_type * bool_vector_alloc_active_index_list(const bool_vector_type * mask , int default_value);
-
+  int_vector_type  * bool_vector_alloc_active_index_list(const bool_vector_type * mask , int default_value);
+  bool               double_vector_approx_equal( const double_vector_type * v1 , const double_vector_type * v12 , double epsilon);
+  
 #ifdef __cplusplus
 }
 #endif
