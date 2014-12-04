29739168599ed882b48c4e43ac0f068b8a340c8d
chflo <chflo@statoil.com>
2014-12-04 08:11:03 +0100

    ERT-791: Prepended enum values in well_type_enum with ERT_ to avoid name clash with OPM code

--- devel/python/python/ert/well/well_type_enum.py
+++ devel/python/python/ert/well/well_type_enum.py
@@ -8,10 +8,10 @@ class WellTypeEnum(BaseCEnum):
     ERT_GAS_INJECTOR = None
     ERT_OIL_INJECTOR = None
 
-WellTypeEnum.addEnum("UNDOCUMENTED_ZERO", 0)
-WellTypeEnum.addEnum("WELL_PRODUCER", 10)
-WellTypeEnum.addEnum("WATER_INJECTOR", 22)
-WellTypeEnum.addEnum("GAS_INJECTOR", 21)
-WellTypeEnum.addEnum("OIL_INJECTOR", 78)
+WellTypeEnum.addEnum("ERT_UNDOCUMENTED_ZERO", 0)
+WellTypeEnum.addEnum("ERT_PRODUCER", 10)
+WellTypeEnum.addEnum("ERT_WATER_INJECTOR", 22)
+WellTypeEnum.addEnum("ERT_GAS_INJECTOR", 21)
+WellTypeEnum.addEnum("ERT_OIL_INJECTOR", 78)
 
 WellTypeEnum.registerEnum(ECL_WELL_LIB, "well_type_enum")
