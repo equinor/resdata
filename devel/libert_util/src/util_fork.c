0af1adb7c75c52f39f79db29cb9ee5b90a3ca781
Joakim Hove <joakim.hove@gmail.com>
2014-12-19 10:26:57 +0100

    Will not build util_ping if PING_CMD is not set.

--- devel/libert_util/src/util_fork.c
+++ devel/libert_util/src/util_fork.c
@@ -238,6 +238,7 @@ char * util_alloc_filename_from_stream( FILE * input_stream ) {
    The PING_CMD is passed as -D from the build system.
 */
 
+#ifdef PING_CMD
 bool util_ping(const char *hostname) { 
   pid_t ping_pid = util_fork_exec(PING_CMD , 4 , (const char *[4]) {"-c" , "3" , "-q", hostname} , false , NULL , NULL , NULL , "/dev/null" , "/dev/null");
   int wait_status;
@@ -256,7 +257,7 @@ bool util_ping(const char *hostname) {
       return false;
   }
 }
-
+#endif
 
 
 
