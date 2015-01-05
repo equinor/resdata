ea13588faf9a085d69b8697f7a82d46facb05138
Joakim Hove <joakim.hove@gmail.com>
2015-01-05 14:32:30 +0100

    ERT-747: Overlapping segments return true for intersection

--- devel/libgeometry/src/geo_polygon.c
+++ devel/libgeometry/src/geo_polygon.c
@@ -108,18 +108,18 @@ static geo_polygon_type * geo_polygon_fload_alloc_xyz( const char * filename , b
       if (fscanf(stream , "%lg %lg %lg" , &x, &y , &z) == 3) {
         if (stop_on_999 && (x == 999) && (y == 999) && (z == 999))
           break;
-        
+
         geo_polygon_add_point( polygon , x , y );
       } else
         break;
-    } 
+    }
 
     fclose( stream );
 
     if ((double_vector_size( polygon->xcoord ) > 1) && (skip_last_point)) {
       if ((double_vector_get_last(polygon->xcoord) == double_vector_get_first(polygon->xcoord)) &&
           (double_vector_get_last(polygon->ycoord) == double_vector_get_first(polygon->ycoord))) {
-        
+
         double_vector_pop( polygon->xcoord );
         double_vector_pop( polygon->ycoord );
       }
@@ -182,44 +182,47 @@ void geo_polygon_iget_xy(const geo_polygon_type * polygon , int index , double *
 
 bool geo_polygon_segment_intersects(const geo_polygon_type * polygon , double x1 , double y1 , double x2 , double y2) {
   bool intersects = false;
-  int index = 0;
   double ** points = util_malloc( 4 * sizeof * points);
   {
     int i;
     for (i = 0; i < 4; i++)
       points[i] = util_malloc( 2 * sizeof * points[i]);
   }
-    
+
   points[0][0] = x1;
   points[1][0] = x2;
   points[0][1] = y1;
   points[1][1] = y2;
-  
-  while (true) {
-    
-    if (index >= (geo_polygon_get_size( polygon ) - 1)) 
-      break;
 
-    {
-      double xc,yc;
+  {
+    int index = 0;
+    while (true) {
 
-      points[2][0] = double_vector_iget( polygon->xcoord , index );
-      points[3][0] = double_vector_iget( polygon->xcoord , index + 1);
-      points[2][1] = double_vector_iget( polygon->ycoord , index );
-      points[3][1] = double_vector_iget( polygon->ycoord , index + 1);
-
-      {
-        geo_util_xlines_status_enum xline_status = geo_util_xsegments(( const double **) points , &xc , &yc);
-        if (xline_status == GEO_UTIL_LINES_CROSSING)
-          intersects = true;
-      }
-      
-      if (intersects)
+      if (index >= (geo_polygon_get_size( polygon ) - 1))
         break;
 
-    } 
-    
-    index++;
+      {
+        double xc,yc;
+
+        points[2][0] = double_vector_iget( polygon->xcoord , index );
+        points[3][0] = double_vector_iget( polygon->xcoord , index + 1);
+        points[2][1] = double_vector_iget( polygon->ycoord , index );
+        points[3][1] = double_vector_iget( polygon->ycoord , index + 1);
+
+        {
+          geo_util_xlines_status_enum xline_status = geo_util_xsegments(( const double **) points , &xc , &yc);
+          if ((xline_status == GEO_UTIL_LINES_CROSSING) ||
+              (xline_status == GEO_UTIL_LINES_OVERLAPPING))
+            intersects = true;
+        }
+
+        if (intersects)
+          break;
+
+      }
+
+      index++;
+    }
   }
 
   {
@@ -251,11 +254,11 @@ double geo_polygon_get_length( geo_polygon_type * polygon ) {
     double x0 = double_vector_iget( polygon->xcoord , 0 );
     double y0 = double_vector_iget( polygon->ycoord , 0 );
     int i;
-    
+
     for (i = 1; i < double_vector_size( polygon->xcoord ); i++) {
       double x1 = double_vector_iget( polygon->xcoord , i );
       double y1 = double_vector_iget( polygon->ycoord , i );
-      
+
       length += sqrt( (x1 - x0)*(x1 - x0) + (y1 - y0)*(y1 - y0));
       x0 = x1;
       y0 = y1;
@@ -269,16 +272,16 @@ double geo_polygon_get_length( geo_polygon_type * polygon ) {
   Name is ignored in the comparison.
 */
 double geo_polygon_equal( const geo_polygon_type * polygon1 , const geo_polygon_type * polygon2 ) {
-  bool equal = 
-    double_vector_equal( polygon1->xcoord , polygon2->xcoord) && 
+  bool equal =
+    double_vector_equal( polygon1->xcoord , polygon2->xcoord) &&
     double_vector_equal( polygon1->ycoord , polygon2->ycoord);
-  
-  
+
+
   if (!equal) {
-    equal = 
-      double_vector_approx_equal( polygon1->xcoord , polygon2->xcoord , 1e-8) && 
+    equal =
+      double_vector_approx_equal( polygon1->xcoord , polygon2->xcoord , 1e-8) &&
       double_vector_approx_equal( polygon1->ycoord , polygon2->ycoord , 1e-8);
   }
-  
+
   return equal;
 }
