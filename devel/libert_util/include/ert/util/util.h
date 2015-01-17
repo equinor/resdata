3bbde41305965b1b71578a0a0c94d1066a1e525d
Roland Kaufmann <roland.kaufmann@uni.no>
2015-01-17 23:46:32 +0100

    Always declare signature of function
    The signature of the function util_ping should be declared so that
    the correct code is generated at the caller site, even if the function
    itself is not available due to a missing configuration.

--- devel/libert_util/include/ert/util/util.h
+++ devel/libert_util/include/ert/util/util.h
@@ -509,9 +509,7 @@ void      util_abort_test_set_intercept_function(const char * function);
   pid_t    util_fork_exec(const char *  , int , const char ** , bool , const char * , const char *  , const char * , const char *  , const char * );
   uid_t  * util_alloc_file_users( const char * filename , int * __num_users);
   char   * util_alloc_filename_from_stream( FILE * input_stream );
-#ifdef PING_CMD
   bool     util_ping( const char * hostname);
-#endif
 #endif
 
 
