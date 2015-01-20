1f9a6dad070c2c24ee28e6a83a33142661d3ba9b
jepebe <jepebe@users.noreply.github.co>
2015-01-20 07:19:46 +0100

    ERT-797 - Misfit data added to tests related to joining and concatenating DataFrames

--- devel/python/test/ert_tests/enkf/export/test_export_join.py
+++ devel/python/test/ert_tests/enkf/export/test_export_join.py
@@ -1,4 +1,4 @@
-from ert.enkf.export import DesignMatrixReader, SummaryCollector, GenKwCollector
+from ert.enkf.export import DesignMatrixReader, SummaryCollector, GenKwCollector, MisfitCollector
 from ert.test import ExtendedTestCase, ErtTestContext
 import pandas
 import numpy
@@ -15,11 +15,13 @@ class ExportJoinTest(ExtendedTestCase):
         with ErtTestContext("python/enkf/export/export_join", self.config) as context:
             ert = context.getErt()
 
-            dm = DesignMatrixReader.loadDesignMatrix(self.design_matrix_file)
             summary_data = SummaryCollector.loadAllSummaryData(ert, "default")
             gen_kw_data = GenKwCollector.loadAllGenKwData(ert, "default")
+            misfit = MisfitCollector.loadAllMisfitData(ert, "default")
+            dm = DesignMatrixReader.loadDesignMatrix(self.design_matrix_file)
 
             result = summary_data.join(gen_kw_data, how='inner')
+            result = result.join(misfit, how='inner')
             result = result.join(dm, how='inner')
 
             self.assertFloatEqual(result["FLUID_PARAMS:SGCR"][0]["2000-02-01"], 0.295136)
@@ -37,6 +39,13 @@ class ExportJoinTest(ExtendedTestCase):
             self.assertFloatEqual(result["EXTRA_FLOAT_COLUMN"][1]["2004-12-01"], 0.07)
             self.assertEqual(result["EXTRA_INT_COLUMN"][1]["2004-12-01"], 225)
             self.assertEqual(result["EXTRA_STRING_COLUMN"][1]["2004-12-01"], "OFF")
+
+            self.assertFloatEqual(result["MISFIT:WWCT:OP_2"][0]["2004-12-01"], 3.552157351038322)
+            self.assertFloatEqual(result["MISFIT:WWCT:OP_2"][24]["2004-12-01"], 25.318572860839158)
+
+            self.assertFloatEqual(result["MISFIT:TOTAL"][0]["2000-02-01"], 1621.755076130249)
+            self.assertFloatEqual(result["MISFIT:TOTAL"][0]["2004-12-01"], 1621.755076130249)
+            self.assertFloatEqual(result["MISFIT:TOTAL"][24]["2004-12-01"], 2783.7582107191383)
 
 
             with self.assertRaises(KeyError):
