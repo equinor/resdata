bae3f3c2d7ae18de0402c0584a19e27193baad93
jepebe <jepebe@users.noreply.github.co>
2015-01-23 13:18:23 +0100

    ERT-797 - Changed how DesignMatrix test data is stored.

--- devel/python/test/ert_tests/enkf/export/test_export_join.py
+++ devel/python/test/ert_tests/enkf/export/test_export_join.py
@@ -3,22 +3,50 @@ from ert.test import ExtendedTestCase, ErtTestContext
 import pandas
 import numpy
 
+def dumpDesignMatrix(path):
+    with open(path, "w") as dm:
+        dm.write("REALIZATION	EXTRA_FLOAT_COLUMN EXTRA_INT_COLUMN EXTRA_STRING_COLUMN\n")
+        dm.write("0	0.08	125	ON\n")
+        dm.write("1	0.07	225	OFF\n")
+        dm.write("2	0.08	325	ON\n")
+        dm.write("3	0.06	425	ON\n")
+        dm.write("4	0.08	525	OFF\n")
+        dm.write("5	0.08	625	ON\n")
+        dm.write("6	0.09	725	ON\n")
+        dm.write("7	0.08	825	OFF\n")
+        dm.write("8	0.02	925	ON\n")
+        dm.write("9	0.08	125	ON\n")
+        dm.write("10	0.08	225	ON\n")
+        dm.write("11	0.05	325	OFF\n")
+        dm.write("12	0.08	425	ON\n")
+        dm.write("13	0.07	525	ON\n")
+        dm.write("14	0.08	625	UNKNOWN\n")
+        dm.write("15	0.08	725	ON\n")
+        dm.write("16	0.08	825	ON\n")
+        dm.write("17	0.08	925	OFF\n")
+        dm.write("18	0.09	125	ON\n")
+        dm.write("19	0.08	225	ON\n")
+        dm.write("20	0.06	325	OFF\n")
+        dm.write("21	0.08	425	ON\n")
+        dm.write("22	0.07	525	ON\n")
+        dm.write("23	0.08	625	OFF\n")
+        dm.write("24	0.08	725	ON\n")
 
 class ExportJoinTest(ExtendedTestCase):
 
     def setUp(self):
         self.config = self.createTestPath("Statoil/config/with_data/config")
-        self.design_matrix_file = self.createTestPath("Statoil/ert-statoil/spotfire/DesignMatrix_25.txt")
 
     def test_join(self):
 
         with ErtTestContext("python/enkf/export/export_join", self.config) as context:
+            dumpDesignMatrix("DesignMatrix.txt")
             ert = context.getErt()
 
             summary_data = SummaryCollector.loadAllSummaryData(ert, "default")
             gen_kw_data = GenKwCollector.loadAllGenKwData(ert, "default")
             misfit = MisfitCollector.loadAllMisfitData(ert, "default")
-            dm = DesignMatrixReader.loadDesignMatrix(self.design_matrix_file)
+            dm = DesignMatrixReader.loadDesignMatrix("DesignMatrix.txt")
 
             result = summary_data.join(gen_kw_data, how='inner')
             result = result.join(misfit, how='inner')
