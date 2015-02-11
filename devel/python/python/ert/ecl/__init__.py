18b35a42f4858a4aa97d1a42201d6ee7d63d65be
Joakim Hove <joakim.hove@gmail.com>
2015-02-11 08:58:16 +0100

    ERT-747: Added super thin EclRestartFile

--- devel/python/python/ert/ecl/__init__.py
+++ devel/python/python/ert/ecl/__init__.py
@@ -90,6 +90,7 @@ from .ecl_3dkw import Ecl3DKW
 from .ecl_file import EclFile , openEclFile
 from .ecl_3d_file import Ecl3DFile 
 from .ecl_init_file import EclInitFile 
+from .ecl_restart_file import EclRestartFile 
 from .ecl_grid import EclGrid
 from .ecl_region import EclRegion
 from .ecl_subsidence import EclSubsidence
