e70ef180406e205df9b5a1795307683afd866cfe
Joakim Hove <joakim.hove@gmail.com>
2015-03-09 15:19:45 +0100

    Whitespace change

--- devel/libecl/tests/ecl_fortio.c
+++ devel/libecl/tests/ecl_fortio.c
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2013  Statoil ASA, Norway. 
-    
-   The file 'ecl_fortio.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2013  Statoil ASA, Norway.
+
+   The file 'ecl_fortio.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 #include <stdlib.h>
 #include <stdbool.h>
@@ -25,7 +25,7 @@
 
 #include <ert/ecl/fortio.h>
 #include <ert/ecl/ecl_endian_flip.h>
-                  
+
 void test_existing_read(const char * filename) {
   fortio_type * fortio = fortio_open_reader( filename , false , ECL_ENDIAN_FLIP);
   test_assert_not_NULL( fortio );
@@ -103,14 +103,14 @@ void test_open_close_read( const char * filename ) {
   test_assert_true( fortio_fopen_stream( fortio ));
   test_assert_true( fortio_stream_is_open( fortio ));
   test_assert_false( fortio_fopen_stream( fortio ));
-  
+
   fortio_fclose( fortio );
 }
 
 
 int main( int argc , char ** argv) {
   const char * file = argv[1];
-  
+
   test_fortio_is_instance( file );
   test_fortio_safe_cast( file );
   test_assert_util_abort("fortio_safe_cast", test_fortio_unsafe_cast, NULL);
@@ -118,13 +118,13 @@ int main( int argc , char ** argv) {
   test_not_existing_read( );
   test_open_close_read( file );
   test_wrapper( file );
-  test_write( "/tmp/path/does/not/exist" , false );  
+  test_write( "/tmp/path/does/not/exist" , false );
   {
     test_work_area_type * work_area = test_work_area_alloc("ecl_fortio.write" );
     util_make_path("path");
     test_write( "path/file.x" , true );
     test_work_area_free( work_area );
   }
-  
+
   exit(0);
 }
