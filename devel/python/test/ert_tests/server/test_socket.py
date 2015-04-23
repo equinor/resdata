7a3a43df23d9e0947712aa1a12c356400ea92892
Joakim Hove <joakim.hove@gmail.com>
2015-04-23 12:52:06 +0200

    ERT-866: Using random port number++

--- devel/python/test/ert_tests/server/test_socket.py
+++ devel/python/test/ert_tests/server/test_socket.py
@@ -16,6 +16,7 @@
 #  for more details. 
 
 import os
+import random
 import time
 import socket 
 import os
@@ -42,9 +43,7 @@ from ert.test import ExtendedTestCase , TestAreaContext
 
 class SocketTest(ExtendedTestCase):
     def setUp(self):
-         self.config_path = self.createTestPath("local/resopt/config/simple")
-         self.config_file = "config"
-         self.port = 9125
+         self.base_port = random.randint( 1024 , 1024 * 30 )
          self.host = "localhost"
          self.pid = 0
          
@@ -53,51 +52,68 @@ class SocketTest(ExtendedTestCase):
 
 
 
-    def runCommand(self , send , expect):
-        result = ErtClient.runCommand(send , self.port , self.host )
-        #self.assertEqual( result , expect )
+    def runCommand(self , send , port , expected = None):
+        result = ErtClient.runCommand(send , port , self.host )
+        if expected:
+            self.assertEqual( result , expected )
         return result
+
+
+    # If the client sends ["QUIT"] the server listen() method should
+    # return, and the whole server process should terminate with
+    # os._exit(0). If the client does not send ["QUIT"] the tearDown()
+    # method should kill the server process.
+
+    def startServer(self , config_file , port):
+        self.pid = os.fork()
+        if self.pid == 0:
+            s = ErtSocket.connect(config_file , port , self.host , self.logger)
+            s.listen( )
+            os._exit(0) 
+        else:
+            time.sleep(0.50)
+            return 
 
     def tearDown(self):
         if self.pid != 0:
             os.kill(self.pid, signal.SIGKILL)
+
          
     def test_connect(self):
-        with TestAreaContext("server/socket") as work_area:
-            work_area.copy_directory_content(self.config_path)
-            self.pid = os.fork()
-            if self.pid == 0:
-                s = ErtSocket.connect(self.config_file , self.port , self.host , self.logger)
-                s.listen( )
-            else:
-                time.sleep(0.50)
-                
-                self.runCommand( ["STATUS"] , ["OPEN"] )
-                self.runCommand( ["QUIT"] , ["QUIT"] )
+        port = self.base_port 
+        config_path = self.createTestPath("local/resopt/config/simple")
+        with TestAreaContext("server/socket1") as work_area:
+            work_area.copy_directory_content(config_path)
+            self.startServer( "config" , port )
             
-                with self.assertRaises(Exception):
-                    self.runCommand( "INVALID" )
+            self.runCommand( ["STATUS"] , port , expected = ["READY"] )
+            
+            with self.assertRaises(Exception):
+                self.runCommand( "INVALID" , port )
                     
-                with self.assertRaises(Exception):
-                    self.runCommand( ["MISSING_COMMAND"] )
+            with self.assertRaises(Exception):
+                self.runCommand( ["MISSING_COMMAND"] , port )
+
+            self.runCommand( ["QUIT"] , port , expected = ["QUIT"] )
+            
+            # Server is closed
+            with self.assertRaises(Exception):
+                self.runCommand( ["STATUS"] , port )
 
 
 
     def test_time_map(self):
+        port = self.base_port + 1
         config_path = self.createTestPath("Statoil/config/with_data")
-        with TestAreaContext("server/socket") as work_area:
+        with TestAreaContext("server/socket2") as work_area:
             work_area.copy_directory_content(config_path)
-            self.pid = os.fork()
-            if self.pid  == 0:
-                s = ErtSocket.connect(self.config_file , self.port , self.host , self.logger)
-                s.listen( )
-            else:
-                time.sleep(0.50)
-                data = self.runCommand(["TIME_STEP"], ["OPEN"])
+            self.startServer( "config" , port )
 
-                self.assertTrue( isinstance( data[0] , datetime.datetime ))
+            data = self.runCommand(["TIME_STEP"] , port )
+            self.assertTrue( isinstance( data[0] , datetime.datetime ))
+            self.assertEqual( data[0] , datetime.datetime(2000 , 1 , 1, 0,0,0))
 
-
+            self.runCommand( ["QUIT"] , port , expected = ["QUIT"] )
 
 
 
