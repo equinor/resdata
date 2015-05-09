452aa44a627ea1397802e0b9681596c36f6d9860
Joakim Hove <joakim.hove@gmail.com>
2015-05-09 13:55:08 +0200

    ERT-868: GenData python updates

--- devel/python/test/ert_tests/enkf/test_enkf_library.py
+++ devel/python/test/ert_tests/enkf/test_enkf_library.py
@@ -14,8 +14,7 @@ class EnKFLibraryTest(ExtendedTestCase):
         self.case_directory = self.createTestPath("local/simple_config/")
 
     def test_failed_class_creation(self):
-        classes = [GenDataConfig,
-                   EnkfConfigNode, EnkfObs, EnKFState, EnsembleConfig,
+        classes = [EnkfConfigNode, EnkfObs, EnKFState, EnsembleConfig,
                    ErtTemplate, ErtTemplates, LocalConfig, ModelConfig, PlotConfig, SiteConfig]
 
         for cls in classes:
