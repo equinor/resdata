9c9ce7b7d3729bb5e9cc6d5de62e921b477860d6
Joakim Hove <joakim.hove@gmail.com>
2015-05-09 13:50:32 +0200

    ERT-868: Added Python wrapper for active_list

--- devel/python/test/ert_tests/enkf/test_enkf.py
+++ devel/python/test/ert_tests/enkf/test_enkf.py
@@ -60,7 +60,8 @@ class EnKFTest(ExtendedTestCase):
         self.assertEnumIsFullyDefined(EnkfObservationImplementationType, "obs_impl_type", "libenkf/include/ert/enkf/obs_vector.h")
         self.assertEnumIsFullyDefined(LoadFailTypeEnum, "load_fail_type", "libenkf/include/ert/enkf/summary_config.h")
         self.assertEnumIsFullyDefined(EnkfFieldFileFormatEnum, "field_file_format_type", "libenkf/include/ert/enkf/field_config.h" )
-
+        self.assertEnumIsFullyDefined(ActiveModeEnum , "active_mode_type" , "libenkf/include/ert/enkf/enkf_types.h")
+        
 
     def test_observations(self):
         with TestAreaContext("enkf_test") as work_area:
