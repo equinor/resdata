f6741e1a251ded4560e1c0bbba9ef584489b71d2
Joakim Hove <joakim.hove@gmail.com>
2015-05-09 19:24:32 +0200

    ERT-868: White space change

--- devel/libert_util/tests/ert_util_vector_test.c
+++ devel/libert_util/tests/ert_util_vector_test.c
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2012  Statoil ASA, Norway. 
-    
-   The file 'ert_util_vector_test.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2012  Statoil ASA, Norway.
+
+   The file 'ert_util_vector_test.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 #include <stdlib.h>
 #include <stdbool.h>
@@ -28,7 +28,7 @@ int test_iset( ) {
   vector_type * vector = vector_alloc_new(  );
   vector_iset_ref( vector , 2 , vector );
 
-  
+
   test_assert_true( vector_get_size( vector ) == 3 );
   test_assert_true( vector_iget( vector , 0 ) == NULL );
   test_assert_true( vector_iget( vector , 1 ) == NULL );
@@ -43,7 +43,7 @@ void test_reverse() {
   const char * val2 = "value2";
   const char * val3 = "value3";
   const char * val4 = "value4";
-  
+
   vector_type * vector1 = vector_alloc_new(  );
   vector_type * vector2 = vector_alloc_new(  );
 
