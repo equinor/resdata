efd71ec33078f3be6a76b8df7ad6eb196272993b
Jean-Paul <jepebe@users.noreply.github.com>
2015-06-02 13:59:06 +0200

    ERT-893 - Ertshell: Added support for smoother overlap_alpha and std_cutoff

--- devel/python/tests/gui/ertshell/test_ertshell_smoother.py
+++ devel/python/tests/gui/ertshell/test_ertshell_smoother.py
@@ -1,7 +1,6 @@
 from ert.enkf.export import GenKwCollector
 from ert.test.extended_testcase import ExtendedTestCase
-from .ert_shell_test_context import ErtShellTestContext
-
+from tests.gui.ertshell.ert_shell_test_context import ErtShellTestContext
 
 class ErtShellSmootherTest(ExtendedTestCase):
 
@@ -20,3 +19,22 @@ class ErtShellSmootherTest(ExtendedTestCase):
             update_data = GenKwCollector.loadAllGenKwData(ert, "test_run_update", keys=["PERLIN_PARAM:SCALE"])
 
             self.assertTrue(data["PERLIN_PARAM:SCALE"].std() > update_data["PERLIN_PARAM:SCALE"].std())
+
+
+    def test_config(self):
+        test_config = self.createTestPath("local/custom_kw/mini_config")
+
+        with ErtShellTestContext("python/ertshell/smoother_config", test_config) as shell:
+
+            analysis_config = shell.shellContext().ert().analysisConfig()
+
+            self.assertTrue(shell.invokeCommand("smoother overlap_alpha"))
+            self.assertTrue(shell.invokeCommand("smoother overlap_alpha 3.1415"))
+            self.assertAlmostEqual(3.1415, analysis_config.getEnkfAlpha())
+            self.assertFalse(shell.invokeCommand("smoother overlap_alpha threepointfourteen"))
+
+            self.assertTrue(shell.invokeCommand("smoother std_cutoff"))
+            self.assertTrue(shell.invokeCommand("smoother std_cutoff 0.1"))
+            self.assertAlmostEqual(0.1, analysis_config.getStdCutoff())
+            self.assertFalse(shell.invokeCommand("smoother std_cutoff zeropointthreefourteen"))
+
