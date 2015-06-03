aff89f1b494c38bd79141221c876ee89386f0950
Joakim Hove <joakim.hove@gmail.com>
2015-06-03 22:27:50 +0200

    ERT-896: Fixed bug in ArgLoader class

--- devel/python/tests/ert/enkf/export/test_arg_loader.py
+++ devel/python/tests/ert/enkf/export/test_arg_loader.py
@@ -14,7 +14,7 @@ class ArgLoaderTest(ExtendedTestCase):
         arg_file = self.createTestPath("Statoil/config/with_GEN_DATA_RFT/wellpath/WI_1.txt")
             
         with self.assertRaises(ValueError):
-            arg = ArgLoader.load(arg_file , column_names = ["Col1" , "Col2"])
+            arg = ArgLoader.load(arg_file , column_names = ["Col1" , "Col2" , "Col3"  ,"COl5" , "Col6"])
 
         arg = ArgLoader.load(arg_file , column_names = ["utm_x" , "utm_y" , "md" , "tvd"])
         self.assertFloatEqual( arg["utm_x"][0] , 461317.620646)
