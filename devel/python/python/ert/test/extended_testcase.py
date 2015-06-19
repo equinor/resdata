578c013bdb3f7527b227cf0b62d0f65ae71b92aa
Joakim Hove <joakim.hove@gmail.com>
2015-06-19 09:26:46 +0200

    ERT-900: Implemeted requireVersion() with skipIf

--- devel/python/python/ert/test/extended_testcase.py
+++ devel/python/python/ert/test/extended_testcase.py
@@ -13,31 +13,6 @@ from ert.util import installAbortSignals
 from ert.util import Version
 
 
-# The requireVersion decorator can be used to limit test methods to
-# run only on ert versions newer than some version. To limit the test
-# method test_xxx() to only run based on ERT versions newer than 1.9:
-#
-#    @requireVersion(1,9)
-#    def test_xxxx(self):
-#        ....
-#
-# The tests which are skipped due to version requirements will be
-# "green", but a warning will be printed on stdout.
-def requireVersion(major,minor,micro="git"):
-
-    def testMethodWrapper(test_method):
-
-        def inner(self):
-            current_version = Version.currentVersion()
-            required_version = Version(major, minor , micro)
-
-            if required_version >= current_version:
-                return test_method(self)
-            else:
-                print("** WARNING: Skipping test:%s - requires ert version: %s - current is: %s " % (test_method.__name__ , required_version , current_version))
-
-        return inner
-    return testMethodWrapper
 
 
 """
@@ -213,3 +188,15 @@ class ExtendedTestCase(TestCase):
         """
 
         return os.environ.get("SKIP_SLOW_TESTS", "False") == "True"
+
+
+    @staticmethod
+    def requireVersion(major , minor , micro = "git"):
+        required_version = Version(major, minor , micro)
+        current_version = Version.currentVersion()
+
+        if required_version < current_version:
+            return True
+        else:
+            return False
+    
