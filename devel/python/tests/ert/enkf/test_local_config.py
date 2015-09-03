59fb9c33fd5ab1f798c11cacfdd1faf90dfa7e45
Ariel Almendral Vazquez <ariel@nr.no>
2015-09-03 11:33:41 +0200

    ERT-904: Local config tests

--- devel/python/tests/ert/enkf/test_local_config.py
+++ devel/python/tests/ert/enkf/test_local_config.py
@@ -1,6 +1,11 @@
 import os.path
 from ert.test import ExtendedTestCase
 from ert.test import ErtTestContext
+from ert.enkf.local_ministep import LocalMinistep
+from ert.enkf.local_updatestep import LocalUpdateStep
+from ert.enkf.active_list import ActiveList
+from ert.enkf.local_obsdata import LocalObsdata
+from ert.enkf.local_obsdata_node import LocalObsdataNode
 
 class LocalConfigTest(ExtendedTestCase):
     
@@ -9,48 +14,121 @@ class LocalConfigTest(ExtendedTestCase):
         self.config = self.createTestPath("local/custom_kw/mini_config")
 
 
-    def test_local_config( self ):
-        
-                
-        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
-
-            
+    def testSetUp( self ):      
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:            
             main = test_context.getErt()
-            self.assertTrue(main, "Load failed")           
+            self.assertTrue(main, "Load failed")
+            
+            local_config = main.local_config()  
+
+            sfile = self.createTestPath("local/custom_kw/test_local_config.txt")                        
+            local_config.writeLocalConfigFile(sfile)            
+            self.assertTrue( os.path.exists( sfile )) 
+ 
+ 
+    def testUpdateStep( self ):                        
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
+            main = test_context.getErt()
+            self.assertTrue(main, "Load failed")
             local_config = main.local_config()                             
             
             # Update step
             updatestep = local_config.createUpdatestep("UPDATESTEP")
+            self.assertTrue(isinstance(updatestep, LocalUpdateStep))
+            
+ 
+    def testMiniStep( self ):                        
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
+            main = test_context.getErt()
+            self.assertTrue(main, "Load failed")
+            local_config = main.local_config()                             
             
             # Ministep                                      
             ministep = local_config.createMinistep("MINISTEP")
+            self.assertTrue(isinstance(ministep, LocalMinistep))                        
+                 
+    def testAttachMinistep( self ):                        
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
+            main = test_context.getErt()
+            self.assertTrue(main, "Load failed")
+            local_config = main.local_config()                             
             
+            # Update step
+            updatestep = local_config.createUpdatestep("UPDATESTEP")
+            self.assertTrue(isinstance(updatestep, LocalUpdateStep))
+ 
+            # Ministep                                      
+            ministep = local_config.createMinistep("MINISTEP")
+            self.assertTrue(isinstance(ministep, LocalMinistep))   
             
+            # Attach
+            updatestep.attachMinistep(ministep)
+
+    def testLocalDataset( self ):                        
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
+            main = test_context.getErt()
+            self.assertTrue(main, "Load failed")
+            local_config = main.local_config()                             
+                       
             # Data
             data_scale = local_config.createDataset("DATA_SCALE")
-            data_scale.addNodeWithIndex("SCALE", 0)
+            data_scale.addNode("SCALE")
+            active_list = data_scale.getActiveList("SCALE")
+            self.assertTrue(isinstance(active_list, ActiveList))
             
+            active_list.addActiveIndex(0)            
+                        
             data_offset = local_config.createDataset("DATA_OFFSET")
             data_offset.addNodeWithIndex("OFFSET", 1)
-                    
-            # ObsData
-            local_obs_data_1 = local_config.createObsdata("OBSSET_1")                                            
+            
+            
+
+    def testLocalObsdata( self ):              
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
+            main = test_context.getErt()
+            self.assertTrue(main, "Load failed")
+            local_config = main.local_config()  
+            
+            # Creating
+            local_obs_data_1 = local_config.createObsdata("OBSSET_1") 
+            self.assertTrue(isinstance(local_obs_data_1, LocalObsdata))
+                                                       
             local_obs_data_1.addNodeAndRange("GENPERLIN_1", 0, 1)
-            local_obs_data_1.addNodeAndRange("GENPERLIN_2", 0, 1)
-             
-            local_obs_data_2 = local_config.createObsdata("OBSSET_2")               
-            local_obs_data_1.addNodeAndRange("GENPERLIN_3", 0, 1)
+            local_obs_data_1.addNodeAndRange("GENPERLIN_2", 0, 1)            
             
-            # Attach         
-            ministep.attachDataset(data_scale)
-            ministep.attachDataset(data_offset)                
-            ministep.attachObsset(local_obs_data_1)                                   
-            ministep.attachObsset(local_obs_data_2)
-                                  
-            updatestep.attachMinistep(ministep)
-            local_config.installUpdatestep(updatestep, 0, 1) 
+            self.assertEqual( len(local_obs_data_1) , 2 )
+                    
+            # Delete node        
+            del local_obs_data_1["GENPERLIN_1"]
+            self.assertEqual( len(local_obs_data_1) , 1 )  
+
+            # Get node
+            node = local_obs_data_1["GENPERLIN_2"]
+            self.assertTrue(isinstance(node, LocalObsdataNode))
+
+
             
-            sfile = self.createTestPath("local/custom_kw/test_local_config.txt")                        
-            local_config.writeLocalConfigFile(sfile)            
-            self.assertTrue( os.path.exists( sfile ))                     
- 
+    def testAttachObsData( self ):          
+        with ErtTestContext("python/enkf/data/local_config", self.config) as test_context:
+            main = test_context.getErt()
+            self.assertTrue(main, "Load failed")
+            local_config = main.local_config()              
+
+            local_obs_data_1 = local_config.createObsdata("OBSSET_1") 
+            self.assertTrue(isinstance(local_obs_data_1, LocalObsdata))
+            
+            # Obsdata                                           
+            local_obs_data_1.addNodeAndRange("GENPERLIN_1", 0, 1)
+            local_obs_data_1.addNodeAndRange("GENPERLIN_2", 0, 1)  
+            
+            # Ministep                                      
+            ministep = local_config.createMinistep("MINISTEP")
+            self.assertTrue(isinstance(ministep, LocalMinistep))   
+
+            # Attach obsset                                
+            ministep.attachObsset(local_obs_data_1)                     
+                        
+            # Retrieve attached obsset            
+            local_obs_data_new = ministep.getLocalObsData()                   
+            self.assertEqual( len(local_obs_data_new) , 2 )                    
+                                
