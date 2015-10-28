f4f5651585a7aca969adbb4a492d7a1bfe79e50e
Fredrik Gundersen <fgun@statoil.com>
2015-10-28 13:58:16 +0100

    ERT-927 Added test for ExtJob

---
+++ devel/python/tests/ert/job_queue/test_ext_job.py
@@ -0,0 +1,27 @@
+from workflow_common import WorkflowCommon
+from ert.test import TestAreaContext
+from ert_statoil.testcase import TestCase
+
+from ert.job_queue.ext_job import ExtJob
+
+
+
+class ExtJobTest(TestCase):
+    def test_load_forward_model(self):
+        with TestAreaContext("python/job_queue/workflow_job",store_area=True) as work_area:
+            WorkflowCommon.createExternalDumpJob()
+            test = work_area.get_cwd()
+            try:
+                ExtJob("dump_job",test,False,"dump_job")
+            except :
+                self.fail("ExtJob() raised Exception unexpectedly!")
+
+
+    def test_load_forward_model_with_error(self):
+        with TestAreaContext("python/job_queue/workflow_job",store_area=True) as work_area:
+
+            test = work_area.get_cwd()
+
+            self.assertRaises(ValueError, ExtJob,"dump_job",test,False,"test")
+
+
