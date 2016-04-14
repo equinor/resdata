e6c94e661b735e169ac86127233190a1d86fa37b
Jean-Paul Balabanian <jpb@statoil.com>
2016-04-14 06:39:32 +0200

    Fixed a bug that occurred when ertshell testing was started with a non-existing config

--- devel/python/tests/gui/ertshell/test_ertshell_server.py
+++ devel/python/tests/gui/ertshell/test_ertshell_server.py
@@ -5,7 +5,8 @@ from tests.gui.ertshell.ert_shell_test_context import ErtShellTestContext
 class ErtShellServerTest(ExtendedTestCase):
 
     def test_server(self):
-        with ErtShellTestContext("python/ertshell/server", "", load_config=False) as shell:
+        config = self.createTestPath("local/snake_oil_no_data/snake_oil.ert")
+        with ErtShellTestContext("python/ertshell/server", config, load_config=False) as shell:
             server_settings = shell.shellContext()["server_settings"]
             self.assertTrue(shell.invokeCommand("help server"))
 
@@ -20,4 +21,3 @@ class ErtShellServerTest(ExtendedTestCase):
 
             self.assertTrue(shell.invokeCommand("server port 99"))
             self.assertEqual(server_settings._port, 99)
-
