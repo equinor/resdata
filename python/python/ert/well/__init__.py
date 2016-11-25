30af6c823babc068e26356e542ebff6f0289355d
Pål Grønås Drange <PGDR@statoil.com>
2016-11-25 08:56:06 +0100

    removed unused ECL_WELL_LIB

--- python/python/ert/well/__init__.py
+++ python/python/ert/well/__init__.py
@@ -11,8 +11,6 @@ class WellPrototype(Prototype):
     def __init__(self, prototype, bind=True):
         super(WellPrototype, self).__init__(WellPrototype.lib, prototype, bind=bind)
 
-ECL_WELL_LIB = ert.load("libecl_well")
-
 from .well_type_enum import WellTypeEnum
 from .well_connection_direction_enum import WellConnectionDirectionEnum
 from .well_connection import WellConnection
