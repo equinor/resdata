b64510896eb87964ccdd1d4e83ef07ee263f33a8
Joakim Hove <joakim.hove@gmail.com>
2017-02-15 14:50:46 +0100

    Improved output for failng test.

--- libecl/tests/ecl_grid_cell_contains_wellpath.c
+++ libecl/tests/ecl_grid_cell_contains_wellpath.c
@@ -44,14 +44,13 @@ vector_type * load_expected( const char * filename ) {
   vector_type * expected = vector_alloc_new();
 
   while (true) {
-    double x,y,z,md;
+    double x,y,z;
     int i,j,k;
 
-    if (fscanf( stream , "%lg %lg %lg %lg %d %d %d" , &x,&y,&md,&z,&i,&j,&k) == 7) {
+    if (fscanf( stream , "%lg %lg %lg %d %d %d" , &x,&y,&z,&i,&j,&k) == 6) {
       point_type * p = util_malloc( sizeof * p );
       p->x = x;
       p->y = y;
-      p->md = md;
       p->z = z;
 
       p->i = i-1;
@@ -76,9 +75,14 @@ int main(int argc , char ** argv) {
     ecl_grid_type * grid = ecl_grid_alloc( argv[1] );
     vector_type * expected = load_expected( argv[2] );
 
-    for (int i=0; i < vector_get_size( expected ); i++) {
-      const point_type * p = vector_iget_const( expected , i );
+    for (int c=0; c < vector_get_size( expected ); c++) {
+      const point_type * p = vector_iget_const( expected , c );
       int g = ecl_grid_get_global_index_from_xyz(grid , p->x, p->y , p->z , 0 );
+      if (g != ecl_grid_get_global_index3(grid, p->i,p->j, p->k)) {
+        int i,j,k;
+        ecl_grid_get_ijk1(grid, g, &i, &j, &k);
+        fprintf(stderr,"point:%d   Simulated: (%d,%d,%d)   Expected: (%d,%d,%d) \n",c , i,j,k, p->i, p->j, p->k);
+      }
       test_assert_int_equal( g , ecl_grid_get_global_index3(grid, p->i,p->j, p->k));
     }
     ecl_grid_free( grid );
