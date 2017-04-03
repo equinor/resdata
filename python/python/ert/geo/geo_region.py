f2950fe8c80c380a88106ab5c1d41ac0e4f27971
Pål Grønås Drange <PGDR@statoil.com>
2017-04-03 17:24:11 +0200

    added selection of polygons to GeoRegion

--- python/python/ert/geo/geo_region.py
+++ python/python/ert/geo/geo_region.py
@@ -16,6 +16,7 @@
 from cwrap import BaseCClass
 from ert.util import IntVector
 from ert.geo import GeoPrototype
+from .cpolyline import CPolyline
 
 class GeoRegion(BaseCClass):
     TYPE_NAME = "geo_region"
@@ -23,15 +24,15 @@ class GeoRegion(BaseCClass):
     _alloc = GeoPrototype("void* geo_region_alloc(geo_pointset, bool)", bind=False)
     _free  = GeoPrototype("void  geo_region_free(geo_region)")
     _reset = GeoPrototype("void  geo_region_reset(geo_region)")
-    #_select_inside_polygon    = GeoPrototype("void geo_region_select_inside_polygon(geo_region,  geo_polygon)")
-    #_select_outside_polygon   = GeoPrototype("void geo_region_select_outside_polygon(geo_region,  geo_polygon)")
-    #_deselect_inside_polygon  = GeoPrototype("void geo_region_deselect_inside_polygon(geo_region,  geo_polygon)")
-    #_deselect_outside_polygon = GeoPrototype("void geo_region_deselect_outside_polygon(geo_region,  geo_polygon)")
+    _get_index_list = GeoPrototype("int_vector_ref geo_region_get_index_list(geo_region)")
+    _select_inside_polygon    = GeoPrototype("void geo_region_select_inside_polygon(geo_region, geo_polygon)")
+    _select_outside_polygon   = GeoPrototype("void geo_region_select_outside_polygon(geo_region, geo_polygon)")
+    _deselect_inside_polygon  = GeoPrototype("void geo_region_deselect_inside_polygon(geo_region, geo_polygon)")
+    _deselect_outside_polygon = GeoPrototype("void geo_region_deselect_outside_polygon(geo_region, geo_polygon)")
     #_select_above_line        = GeoPrototype("void geo_region_select_above_line(geo_region, const double xcoords[2], const double ycoords[2])")
     #_select_below_line        = GeoPrototype("void geo_region_select_below_line(geo_region, const double xcoords[2], const double ycoords[2])")
     #_deselect_above_line      = GeoPrototype("void geo_region_deselect_above_line(geo_region, const double xcoords[2], const double ycoords[2])")
     #_deselect_below_line      = GeoPrototype("void geo_region_deselect_below_line(geo_region, const double xcoords[2], const double ycoords[2])")
-    _get_index_list           = GeoPrototype("int_vector_ref geo_region_get_index_list(geo_region)")
 
 
     def __init__(self, pointset, preselect=False):
@@ -41,9 +42,35 @@ class GeoRegion(BaseCClass):
             super(GeoRegion, self).__init__(c_ptr)
         else:
             raise ValueError('Could not construct GeoRegion from pointset %s.' % pointset)
+        self.select_polygon_fn = {
+            (True, True): self._select_inside_polygon,
+            (False, True): self._select_outside_polygon,
+            (True, False): self._deselect_inside_polygon,
+            (False, False): self._deselect_outside_polygon
+        }
+
 
     def getActiveList(self):
         return self._get_index_list()
+
+    def select_polygon(self, polygon, inside=True, select=True):
+        if not isinstance(polygon, CPolyline):
+            raise ValueError('Need to select with a CPolyline, not %s.'
+                             % type(polygon))
+        selector_fn = self.select_polygon_fn[(inside, select)]
+        selector_fn(polygon)
+
+    def select_inside(self, polygon):
+        self.select_polygon(polygon, inside=True, select=True)
+
+    def select_outside(self, polygon):
+        self.select_polygon(polygon, inside=False, select=True)
+
+    def deselect_inside(self, polygon):
+        self.select_polygon(polygon, inside=True, select=False)
+
+    def deselect_outside(self, polygon):
+        self.select_polygon(polygon, inside=False, select=False)
 
 
     def __len__(self):
