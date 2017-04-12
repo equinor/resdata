a1e4617e0347cb45b84e4bc7dd536d83ca5d4d5f
Pål Grønås Drange <PGDR@statoil.com>
2017-04-12 11:40:29 +0200

    whitespace

--- libert_util/src/struct_vector.c
+++ libert_util/src/struct_vector.c
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2014  Statoil ASA, Norway. 
-    
-   The file 'struct_vector.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2014  Statoil ASA, Norway.
+
+   The file 'struct_vector.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
 #include <string.h>
@@ -30,7 +30,7 @@ struct struct_vector_struct {
   int size;
   int element_size;
   int alloc_size;
-  
+
   char * data;
 };
 
@@ -63,9 +63,9 @@ struct_vector_type * struct_vector_alloc( int element_size ) {
     vector->alloc_size = 0;
     vector->element_size = element_size;
     vector->data = NULL;
-    
+
     struct_vector_resize( vector , 10 );
-    
+
     return vector;
   }
 }
