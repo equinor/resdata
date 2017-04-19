32a7b23a58103df914d9d39264605284befd63fe
Joakim Hove <joakim.hove@gmail.com>
2017-04-19 16:18:17 +0200

    Handle missing pylint gracefully.

--- python/python/ert/test/lint_test_case.py
+++ python/python/ert/test/lint_test_case.py
@@ -15,17 +15,24 @@
 #  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 #  for more details.
 
+import sys
 import fnmatch
 import os
 import unittest
-from pylint import epylint as lint
+try:
+    from pylint import epylint as lint
+except ImportError:
+    sys.stderr.write("Could not import pylint module - lint based testing will be skipped\n")
+    lint = None
 
+    
 class LintTestCase(unittest.TestCase):
     """This class is a test case for linting."""
 
     LINT_ARGS = ['-d', 'R,C,W'] + \
                 ['--extension-pkg-whitelist=numpy']
 
+    
     @staticmethod
     def _get_lintable_files(paths, whitelist=()):
         """Recursively traverses all folders in paths for *.py files"""
@@ -37,12 +44,16 @@ class LintTestCase(unittest.TestCase):
                         matches.append(os.path.join(root, filename))
         return matches
 
+    
     def assertLinted(self, paths, whitelist=()):  # noqa
         """Takes a path to a folder or a list of paths to folders and recursively finds
         all *.py files within that folder except the ones with filenames in whitelist.
 
         Will assert lint.lint(fname) == 0 for every *.py file found.
         """
+        if lint is None:
+            self.skipTest("pylint not installed")
+            
         if isinstance(paths, str):
             paths = [paths]
         files = self._get_lintable_files(paths, whitelist=whitelist)
