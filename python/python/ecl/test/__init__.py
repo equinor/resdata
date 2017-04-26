2cc0ad09e8ab8139170b104cee95b8ab933e43f0
Joakim Hove <joakim.hove@gmail.com>
2017-04-26 12:41:36 +0200

    Changes in running Python tests
     - The ctest_run executable has been moved to avoid import confusion.
     - The ctest_run executable will not modify PATH
     - When adding a test the PYTHONPATH is set as property.

--- python/python/ecl/test/__init__.py
+++ python/python/ecl/test/__init__.py
@@ -7,6 +7,8 @@ from .temp_area import TempArea , TempAreaContext
 from .ert_test_runner import ErtTestRunner
 from .path_context import PathContext
 from .lint_test_case import LintTestCase
+from .import_test_case import ImportTestCase
+
 try:
     from .ert_test_context import ErtTestContext, ErtTest
 except ImportError:
--- python/tests/global/test_import.py
+++ python/tests/global/test_import.py
@@ -14,20 +14,13 @@
 #  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 #  for more details.
 
-from ecl.test import LintTestCase
+import os
+import sys
 
-class LintErt(LintTestCase):
-    """Tests that no file in ert needs linting"""
 
-    def test_lint_ecl(self):
-        white = ['ecl_kw.py', 'ecl_type.py', 'ecl_sum.py', 'ecl_grid.py', 'ecl_npv.py']  # TODO fix issues and remove
-        self.assertLinted('ecl/ecl', whitelist=white)
+from ecl.test import ImportTestCase
 
-    def test_lint_geo(self):
-        self.assertLinted('ecl/geo')
-
-    def test_lint_util(self):
-        self.assertLinted('ecl/util')
-
-    def test_lint_well(self):
-        self.assertLinted('ecl/well')
+class ImportEcl(ImportTestCase):
+    
+    def test_import_ecl(self):
+        self.assertTrue( self.import_package( "ecl" ))
