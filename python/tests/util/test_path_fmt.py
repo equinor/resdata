7274ca5f58ccfa51febc64dbccad79c173a7ca67
Pål Grønås Drange <PGDR@statoil.com>
2017-07-04 16:04:17 +0200

    Fix PathFmtTest

--- python/tests/util/test_path_fmt.py
+++ python/tests/util/test_path_fmt.py
@@ -4,8 +4,7 @@ from ecl.util import PathFormat
 
 class PathFmtTest(ExtendedTestCase):
 
-    
     def test_create(self):
         path_fmt = PathFormat("random/path/%d-%d")
-            
-            
+        self.assertIn('random/path', repr(path_fmt))
+        self.assertTrue(str(path_fmt).startswith('PathFormat('))
