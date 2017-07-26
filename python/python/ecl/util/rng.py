d000136dd2112726c40ebcdeccc36b61564801ef
Pål Grønås Drange <PGDR@statoil.com>
2017-07-26 08:18:13 +0200

    fixup! Whitespace fix

--- python/python/ecl/util/rng.py
+++ python/python/ecl/util/rng.py
@@ -23,15 +23,15 @@ from ecl.util.enums import RngInitModeEnum, RngAlgTypeEnum
 class RandomNumberGenerator(BaseCClass):
     TYPE_NAME = "rng"
 
-    _rng_alloc = UtilPrototype("void* rng_alloc(rng_alg_type_enum, rng_init_mode_enum)" , bind = False)
-    _free = UtilPrototype("void rng_free(rng)")
-    _get_double = UtilPrototype("double rng_get_double(rng)")
-    _get_int = UtilPrototype("int rng_get_int(rng, int)")
+    _rng_alloc   = UtilPrototype("void* rng_alloc(rng_alg_type_enum, rng_init_mode_enum)", bind=False)
+    _free        = UtilPrototype("void rng_free(rng)")
+    _get_double  = UtilPrototype("double rng_get_double(rng)")
+    _get_int     = UtilPrototype("int rng_get_int(rng, int)")
     _get_max_int = UtilPrototype("uint rng_get_max_int(rng)")
-    _state_size = UtilPrototype("int rng_state_size(rng)")
-    _set_state = UtilPrototype("void rng_set_state(rng , char*)")
-    _load_state = UtilPrototype("void rng_load_state(rng , char*)")
-    _save_state = UtilPrototype("void rng_save_state(rng , char*)")
+    _state_size  = UtilPrototype("int rng_state_size(rng)")
+    _set_state   = UtilPrototype("void rng_set_state(rng, char*)")
+    _load_state  = UtilPrototype("void rng_load_state(rng, char*)")
+    _save_state  = UtilPrototype("void rng_save_state(rng, char*)")
 
     def __init__(self, alg_type=RngAlgTypeEnum.MZRAN, init_mode=RngInitModeEnum.INIT_CLOCK):
         assert isinstance(alg_type, RngAlgTypeEnum)
@@ -47,7 +47,7 @@ class RandomNumberGenerator(BaseCClass):
         state_size = self.stateSize()
         if len(seed_string) < state_size:
             raise ValueError("The seed string must be at least %d characters long" % self.stateSize())
-        self._set_state( seed_string)
+        self._set_state(seed_string)
 
     def getDouble(self):
         """ @rtype: float """
@@ -63,19 +63,18 @@ class RandomNumberGenerator(BaseCClass):
     def free(self):
         self._free()
 
-    def loadState(self , seed_file):
+    def loadState(self, seed_file):
         """
         Will seed the RNG from the file @seed_file.
         """
-        if os.path.isfile( seed_file ):
-            self._load_state( seed_file )
+        if os.path.isfile(seed_file):
+            self._load_state(seed_file)
         else:
             raise IOError("No such file: %s" % seed_file)
 
 
-    def saveState(self , seed_file):
+    def saveState(self, seed_file):
         """
         Will save the state of the rng to @seed_file
         """
-        self._save_state( seed_file )
-        
+        self._save_state(seed_file)
