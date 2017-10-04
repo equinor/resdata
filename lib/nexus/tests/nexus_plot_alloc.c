d1b72f9aeea8fc78a05ae5e0eb3bb91ea3a85da4
Joakim Hove <joakim.hove@gmail.com>
2017-10-04 18:31:42 +0200

    Started on header-test

--- lib/nexus/tests/nexus_plot_alloc.c
+++ lib/nexus/tests/nexus_plot_alloc.c
@@ -15,18 +15,36 @@
    See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
    for more details.
 */
+#include <stdio.h>
+#include <string.h>
 
-
+#include <ert/util/test_work_area.h>
 #include <ert/util/test_util.h>
+
 #include <ert/nexus/nexus_plot.h>
 
 void test_file_exist() {
-
     test_assert_NULL(nexus_plot_alloc("file_does_not_exist"));
 }
 
 
+void test_invalid_header() {
+  test_work_area_type * work_area = test_work_area_alloc("nexuse_header");
+  FILE * stream = fopen("plot_file" , "w");
+  const char * header = "xxxxINVALID_HEADER";
+  fwrite( header, 1, strlen(header), stream);
+  fclose( stream );
+
+  test_assert_NULL( nexus_plot_alloc("plot_file"));
+
+  test_work_area_free( work_area );
+}
+
+
+
+
 int main(int argc, char **argv) {
-    test_file_exist();
-    exit(0);
+  test_file_exist();
+  test_invalid_header();
+  exit(0);
 }
