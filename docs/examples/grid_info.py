47c299a37785500295b7e0412c54f35f34ccbcba
Pål Grønås Drange <PGDR@statoil.com>
2017-10-17 20:57:20 +0200

    Add main method and use cell in example

--- docs/examples/grid_info.py
+++ docs/examples/grid_info.py
@@ -3,32 +3,29 @@ import sys
 from ecl.ecl import EclGrid, EclRegion
 
 
-def volum_min_max(grid):
-    vmin = 10000000
-    vmax = 0
-    for a in range(grid.getNumActive()):
-        v = grid.cell_volume( active_index = a )
-        vmin = min(vmin,v)
-        vmax = max(vmax,v)
+def volume_min_max(grid):
+    vols = [c.volume for c in grid if c.active]
+    return min(vols), max(vols)
 
-    return vmin,vmax
+def main(grid):
+    vmin,vmax = volume_min_max(grid)
 
-
-if __name__ == "__main__":
-    case = sys.argv[1]
-    grid = EclGrid( case )
-
-    vmin,vmax = volum_min_max( grid )
-
-    dz_limit = 0.1
-    region = EclRegion( grid, False )
-    region.select_thin( dz_limit )
+    dz_limit = 0.3
+    region = EclRegion(grid, False)
+    region.select_thin(dz_limit)
 
     print "Smallest cell     : %g" % vmin
     print "Largest cell      : %g" % vmax
     print "Thin active cells : %d" % region.active_size()
 
-    for ai in region.get_active_list( ):
-        ijk = grid.get_ijk( active_index = ai )
-        print ijk
+    for ai in region.get_active_list():
+        c = grid.cell(active_index=ai)
+        print('dz(%2d, %2d, %2d) = %.3f' % (c.i, c.j, c.k, c.dz))
 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit('usage: grid_info.py path/to/file.EGRID')
+    case = sys.argv[1]
+    grid = EclGrid(case)
+    main(grid)
