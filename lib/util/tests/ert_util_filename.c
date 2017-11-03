38f8ba4acb49e86ea332cd5a717a7e75b1b74f91
Joakim Hove <joakim.hove@gmail.com>
2017-11-03 13:53:54 +0100

    Whitespace fixup.

--- lib/util/tests/ert_util_filename.c
+++ lib/util/tests/ert_util_filename.c
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2012  Statoil ASA, Norway. 
-    
-   The file 'ert_util_PATH_test.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2012  Statoil ASA, Norway.
+
+   The file 'ert_util_PATH_test.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
 #include <stdlib.h>
@@ -29,7 +29,7 @@ void test_dirname() {
   const char * src_file1 = "/some/very/long/path/file.txt";
   const char * src_file2 = "relative/path/file.txt";
   const char * src_file3 = "file.txt";
-  
+
   char * path1 = util_split_alloc_dirname( src_file1 );
   char * path2 = util_split_alloc_dirname( src_file2 );
   char * path3 = util_split_alloc_dirname( src_file3 );
@@ -48,7 +48,7 @@ void test_filename() {
   const char * src_file1 = "/some/very/long/path/file1.txt";
   const char * src_file2 = "relative/path/file2";
   const char * src_file3 = "/tmp";
-  
+
   char * file1 = util_split_alloc_filename( src_file1 );
   char * file2 = util_split_alloc_filename( src_file2 );
   char * file3 = util_split_alloc_filename( src_file3 );
@@ -65,9 +65,9 @@ void test_filename() {
 
 
 int main(int argc , char ** argv) {
-  
+
   test_dirname();
   test_filename();
   exit(0);
-  
+
 }
