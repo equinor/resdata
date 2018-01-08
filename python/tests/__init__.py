de03525f601bb7e05d746b7e5e908b779d093764
Andrea Brambilla <abramb@statoil.com>
2018-01-08 16:18:51 +0100

    Python tests - source folder set by CMake
    The previous method to detect the source folder worked only if the build
    directory was inside the source directory

--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -3,12 +3,19 @@ from ecl.test import ExtendedTestCase
 
 
 def source_root():
+    src = '@CMAKE_CURRENT_SOURCE_DIR@/../..'
+    if os.path.isdir(src):
+        return os.path.realpath(src)
+    
+    # If the file was not correctly configured by cmake, look for the source
+    # folder, assuming the build folder is inside the source folder.
     path_list = os.path.dirname(os.path.abspath(__file__)).split("/")
-    while True:
+    while len(path_list) > 0:
         git_path = os.path.join(os.sep, "/".join(path_list), ".git")
         if os.path.isdir(git_path):
             return os.path.join(os.sep, *path_list)
         path_list.pop()
+    raise RuntimeError('Cannot find the source folder')
 
 
 class EclTest(ExtendedTestCase):
