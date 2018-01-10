e240c5eb4bcaa06b92f046a7803bb7381f93b846
Steinar Foss <steinar@unicus.no>
2018-01-10 16:21:26 +0100

    removed message_level_enum.py.

--- python/ecl/util/enums/__init__.py
+++ python/ecl/util/enums/__init__.py
@@ -1,4 +1,3 @@
 from .rng_init_mode_enum import RngInitModeEnum
 from .rng_alg_type_enum import RngAlgTypeEnum
 from .llsq_result_enum import LLSQResultEnum
-from .message_level_enum import MessageLevelEnum
