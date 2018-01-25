6cd69c4f1f5e104932de7116f763e45612b5ad6b
Steinar Foss <steinar@unicus.no>
2018-01-25 12:33:00 +0100

    ecl.load reset.

--- python/ecl/__init__.py
+++ python/ecl/__init__.py
@@ -115,8 +115,11 @@ if ecl_lib_path:
 if sys.hexversion < required_version_hex:
     raise Exception("ERT Python requires Python 2.7.")
 
+def load(name):
+    return cwrapload(name, path=ecl_lib_path, so_version=ert_so_version)
+
 class EclPrototype(Prototype):
-    lib = cwrapload("libecl", path=ecl_lib_path, so_version=ert_so_version)
+    lib = load("libecl")
 
     def __init__(self, prototype, bind=True):
         super(EclPrototype, self).__init__(EclPrototype.lib, prototype, bind=bind)
