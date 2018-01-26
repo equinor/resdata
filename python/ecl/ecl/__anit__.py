5b68e8c6034545acd0243b376c8974a180b39a60
Steinar Foss <steinar@unicus.no>
2018-01-26 12:33:27 +0100

    removed functional code in ecl/ecl__init__.py.

--- python/ecl/ecl/__anit__.py
+++ python/ecl/ecl/__anit__.py
@@ -72,25 +72,3 @@ per module organization:
      namespace 'ecl'.
 
 """
-import ecl.util
-import ecl.geo
-
-
-from .cell import Cell
-from .ecl_sum_var_type import EclSumVarType
-from .ecl_sum_tstep import EclSumTStep
-from .ecl_sum import EclSum #, EclSumVector, EclSumNode, EclSMSPECNode
-from .ecl_sum_keyword_vector import EclSumKeyWordVector
-from .ecl_rft_cell import EclPLTCell, EclRFTCell
-from .ecl_rft import EclRFT, EclRFTFile
-from .ecl_3dkw import Ecl3DKW
-from .ecl_grid import EclGrid
-from .ecl_region import EclRegion
-from .ecl_subsidence import EclSubsidence
-from .ecl_grav_calc import phase_deltag, deltag
-from .ecl_grav import EclGrav
-from .ecl_sum_node import EclSumNode
-from .ecl_sum_vector import EclSumVector
-from .ecl_npv import EclNPV , NPVPriceVector
-from .ecl_cmp import EclCmp
-from .ecl_grid_generator import EclGridGenerator
