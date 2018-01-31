cb7f08e01a35c7545fbe8e6e30086d89632fb37d
Joakim Hove <joakim.hove@gmail.com>
2018-01-31 08:27:01 +0100

    White space cleanup.

--- python/tests/ecl_tests/test_ecl_util.py
+++ python/tests/ecl_tests/test_ecl_util.py
@@ -1,17 +1,17 @@
-#  Copyright (C) 2015  Statoil ASA, Norway. 
-#   
-#  The file 'sum_test.py' is part of ERT - Ensemble based Reservoir Tool. 
-#   
-#  ERT is free software: you can redistribute it and/or modify 
-#  it under the terms of the GNU General Public License as published by 
-#  the Free Software Foundation, either version 3 of the License, or 
-#  (at your option) any later version. 
-#   
-#  ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-#  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-#  FITNESS FOR A PARTICULAR PURPOSE.   
-#   
-#  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+#  Copyright (C) 2015  Statoil ASA, Norway.
+#
+#  The file 'sum_test.py' is part of ERT - Ensemble based Reservoir Tool.
+#
+#  ERT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 #  for more details.
 
 from ecl import EclTypeEnum , EclFileEnum, EclPhaseEnum, EclUnitTypeEnum, EclUtil
@@ -28,15 +28,15 @@ class EclUtilTest(EclTest):
 
         source_file_path = "lib/include/ert/ecl/ecl_type.h"
         self.assertEnumIsFullyDefined(EclTypeEnum, "ecl_type_enum", source_file_path)
-        
+
     def test_file_type(self):
         file_type , fmt , report = EclUtil.inspectExtension("CASE.X0078")
         self.assertEqual( file_type , EclFileEnum.ECL_RESTART_FILE )
-        
+
     def test_file_report_nr(self):
         report_nr = EclUtil.reportStep("CASE.X0080")
         self.assertEqual( report_nr , 80 )
 
         with self.assertRaises(ValueError):
             EclUtil.reportStep("CASE.EGRID")
-            
+
