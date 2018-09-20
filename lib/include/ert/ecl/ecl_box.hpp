880f0b86dc3af6f6ce380ddb7d69f43201edab1c
Joakim Hove <joakim.hove@gmail.com>
2018-09-20 14:14:49 +0200

    Remove old C implementation of ecl_box

--- lib/include/ert/ecl/ecl_box.hpp
+++ lib/include/ert/ecl/ecl_box.hpp
@@ -20,7 +20,6 @@
 #define ERT_ECL_BOX_H
 
 #include <vector>
-
 #include <ert/ecl/ecl_grid.hpp>
 
 
@@ -39,29 +38,4 @@ namespace ecl {
   };
 
 }
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef struct ecl_box_struct ecl_box_type;
-
-
-void           ecl_box_set_size       (ecl_box_type * , int , int , int , int , int , int );
-ecl_box_type * ecl_box_alloc(const ecl_grid_type * ecl_grid , int i1,int i2 , int j1 , int j2 , int k1, int k2);
-void           ecl_box_free            (ecl_box_type * );
-void           ecl_box_set_values(const ecl_box_type * , char * , const char * , int );
-int            ecl_box_get_total_size(const ecl_box_type * );
-int            ecl_box_get_active_size( const ecl_box_type * ecl_box );
-const int    * ecl_box_get_active_list( const ecl_box_type * ecl_box );
-int            ecl_box_get_global_size( const ecl_box_type * ecl_box );
-const int    * ecl_box_get_global_list( const ecl_box_type * ecl_box );
-bool           ecl_box_contains(const ecl_box_type * box , int i , int j , int k);
-
-UTIL_IS_INSTANCE_HEADER( ecl_box );
-UTIL_SAFE_CAST_HEADER( ecl_box );
-
-#ifdef __cplusplus
-}
-#endif
 #endif
