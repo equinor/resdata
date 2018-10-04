b7529dd10e7fecfe72ffca95a944a0e0583ce797
Joakim Hove <joakim.hove@gmail.com>
2018-10-04 17:23:47 +0200

    Pathname bug in ecl_sum
    The ecl_sum_set_case() function was confused if a directory BASE existed along
    with the BASE.xxxx files which belonged to a simulation. Fixed by using new
    purely string based path inspection functions.

--- lib/util/path.cpp
+++ lib/util/path.cpp
@@ -28,6 +28,12 @@ namespace ecl {
 
     namespace path {
 
+      /*
+        The bizarre dive down into c_str() is to avoid inadvertendtly
+        introducing a symbol which is creates a ABI compatibility issue between
+        the libstdc++ librararies. A bug in the devtoolset compiler?
+      */
+
       std::string dirname(const std::string& fname) {
         size_t last_slash = fname.rfind(UTIL_PATH_SEP_CHAR);
         if (last_slash == std::string::npos)
@@ -39,7 +45,6 @@ namespace ecl {
           return fname.substr(0, last_slash);
       }
 
-
       std::string basename(const std::string& fname) {
         size_t end_pos = fname.rfind('.');
         size_t offset = fname.rfind(UTIL_PATH_SEP_CHAR);
@@ -48,19 +53,28 @@ namespace ecl {
         else
           offset += 1;
 
-        if (end_pos == std::string::npos)
-          return fname.substr(offset);
-        else
-          return fname.substr(offset, end_pos - offset);
+        {
+          const char * c_str = fname.c_str();
+          if (end_pos == std::string::npos || end_pos < offset)
+            return util_alloc_string_copy( &c_str[offset] );
+
+          return util_alloc_substring_copy(c_str, offset, end_pos - offset);
+        }
       }
 
 
       std::string extension(const std::string& fname)  {
         size_t end_pos = fname.rfind('.');
+        size_t last_slash = fname.rfind(UTIL_PATH_SEP_CHAR);
         if (end_pos == std::string::npos)
           return "";
 
-        return fname.substr(end_pos + 1);
+        if (last_slash == std::string::npos || end_pos > last_slash) {
+          const char * c_str = fname.c_str();
+          return util_alloc_substring_copy( c_str, end_pos + 1, fname.size() - end_pos - 1);
+        }
+
+        return "";
       }
 
     }
