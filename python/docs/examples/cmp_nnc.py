4ee7966438f87e34f8ccad3ce6f72d78692428d8
Markus Blatt <markus@dr-blatt.de>
2019-05-02 16:01:13 +0200

    [bugfix] Use zero-based global index in cmp_nnc.py
    The output files use 1-based indexing (FORTRAN) and the
    python functions assume 0-based indexing (C/C++). Therefore
    we need to decrement the global index befor calling grid.get_ijk.

--- python/docs/examples/cmp_nnc.py
+++ python/docs/examples/cmp_nnc.py
@@ -23,6 +23,10 @@ if __name__ == "__main__":
 
     nnc_list = sorted(nnc_list, key = itemgetter(0))
     for (g1,g2,T) in nnc_list:
+        # grid_ijk assumes 0-based indexing, g1/g2 are 1-based (FORTRAN)
+        # Convert them to zero based ones.
+        g1 = g1 - 1
+        g2 = g2 - 1
         i1,j1,k1 = grid.get_ijk( global_index = g1 )
         i2,j2,k2 = grid.get_ijk( global_index = g2 )
 
