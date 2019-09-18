31c9be945847a4f835e242af1a7c36236fbdca1e
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2019-09-18 09:39:26 +0200

    Bugfix: do not include inside extern "C" block.

--- lib/include/ert/ecl/ecl_init_file.hpp
+++ lib/include/ert/ecl/ecl_init_file.hpp
@@ -19,16 +19,16 @@
 #ifndef ERT_ECL_INIT_FILE_H
 #define ERT_ECL_INIT_FILE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <time.h>
 
 #include <ert/ecl/fortio.h>
 #include <ert/ecl/ecl_kw.hpp>
 #include <ert/ecl/ecl_grid.hpp>
 #include <ert/ecl/ecl_util.hpp>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
   void ecl_init_file_fwrite_header( fortio_type * fortio , const ecl_grid_type * grid , const ecl_kw_type * poro , ert_ecl_unit_enum unit_system, int phases , time_t start_date);
 
