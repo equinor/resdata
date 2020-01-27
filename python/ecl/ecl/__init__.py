539b836ac8fec7f1be9f3474419f189d54e10edf
Øystein Olai Heggen <ooh@equinor.com>
2020-01-27 13:10:42 +0100

    Add deprecation warnings when import ecl.ecl or import ert.ecl

--- python/ecl/ecl/__init__.py
+++ python/ecl/ecl/__init__.py
@@ -9,4 +9,12 @@
 # Both of these legacy forms are controlled by the cmake switch
 # INSTALL_ERT_LEGACY.
 
+import warnings
+
+warnings.warn(
+    "Importing from ert.ecl, ecl.ecl or ert is deprecated and will not be available in python3." \
+    " For eclipse functionality use \'from ecl import\', for ert workflow tooling use \'from res import\'.",
+    DeprecationWarning
+)
+
 from ert.ecl import *
