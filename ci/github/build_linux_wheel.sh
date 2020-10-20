6cdc2d279bc6d5e06fe0c9537eef7f70f92e5ae5
Zohar Malamant <ZOM@equinor.com>
2020-10-20 16:03:03 +0200

    Migrate to Github Actions

---
+++ ci/github/build_linux_wheel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+case "$1" in
+    3.6) pyver=cp36-cp36m ;;
+    3.7) pyver=cp37-cp37m ;;
+    3.8) pyver=cp38-cp38 ;;
+    3.9) pyver=cp39-cp39 ;;
+    *)
+        echo "Unknown Python version $1"
+        exit 1
+        ;;
+esac
+
+# Install dependencies
+yum install -y zlib-devel
+
+# Build wheel
+cd /github/workspace
+/opt/python/$pyver/bin/pip wheel . --no-deps -w wheelhouse
+auditwheel repair wheelhouse/* -w dist
