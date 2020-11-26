dd2837e748dd0ac134c13ebbc6012ea742f1e8bc
Eivind Jahren <eivind.jahren@webstep.no>
2020-11-26 12:39:15 +0100

    Run the c tests on komodo pr
    Currently the komodo job is responsible for running the internal
    test data, so it must also run the c tests.

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -1,3 +1,13 @@
+build_and_run_ctest () {
+    pushd $CI_TEST_ROOT
+    cmake $CI_SOURCE_ROOT  -DBUILD_TESTS=ON
+    cmake --build .
+    ctest --output-on-failure
+    popd
+    # Reset
+    rm -rf $CI_TEST_ROOT
+    mkdir -p $CI_TEST_ROOT
+}
 copy_test_files () {
     pushd $CI_TEST_ROOT
     mkdir -p {.git,python}
@@ -7,4 +17,16 @@ copy_test_files () {
     ln -s -T $EQUINOR_TEST_DATA_ROOT  test-data/Equinor
     cp -R {$CI_SOURCE_ROOT,$PWD}/python/tests
     popd
+}
+run_tests () {
+    if [[ ! -z "${CI_PR_RUN:-}" ]]
+    then
+        pip install .
+    fi
+    ci_install_cmake
+    build_and_run_ctest
+    copy_test_files
+    install_test_dependencies
+    cd $CI_TEST_ROOT
+    pytest -vv
 }
