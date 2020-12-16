eeda6920286f6d0b877c4b1af292c144274e0a86
Eivind Jahren <eivind.jahren@webstep.no>
2020-12-16 14:52:20 +0100

    Rewrite ecl_alloc_cpgrid test to use catch2

--- lib/tests/test_ecl_grid.cpp
+++ lib/tests/test_ecl_grid.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 #include <ert/ecl/ecl_grid.hpp>
+#include <ert/ecl/ecl_kw.hpp>
+#include <ert/ecl/ecl_kw_magic.hpp>
 #include <vector>
 
 using namespace Catch;
@@ -76,7 +78,7 @@ TEST_CASE("Test dxv_dyv_dzv grids", "[unittest]") {
         ecl_grid_type *grid = generate_dxv_dyv_dzv_depthz_grid(
             100, 100, 10, x_width, y_width, z_width);
 
-        GIVEN("Any cell in that grid") {
+        AND_GIVEN("Any cell in that grid") {
             auto i = GENERATE(take(3, random(0, 99)));
             auto j = GENERATE(take(3, random(0, 99)));
             auto k = GENERATE(take(3, random(0, 9)));
@@ -139,5 +141,114 @@ TEST_CASE("Test grid compare", "[unittest]") {
 
         ecl_grid_free(grid1);
         ecl_grid_free(grid2);
+    }
+}
+
+/**
+ * Generates a grid, using the ecl_grid_alloc_GRDECL_kw constructor,
+ * with the x,y,z coordinates of the 0th corner of the ijkth cel is
+ * i,j,k. In other words this is a grid where the corners are on the
+ * whole numbers.
+ *
+ * Takes a vector of i,j,k,c,z tuples which changes the corner c of cell
+ * at i,j,k z value.
+ */
+ecl_grid_type *generate_coordkw_grid(
+    int num_x, int num_y, int num_z,
+    const std::vector<std::tuple<int, int, int, int, double>> &z_vector) {
+    ecl_kw_type *coord_kw =
+        ecl_kw_alloc(COORD_KW, ECL_GRID_COORD_SIZE(num_x, num_y), ECL_FLOAT);
+    ecl_kw_type *zcorn_kw = ecl_kw_alloc(
+        ZCORN_KW, ECL_GRID_ZCORN_SIZE(num_x, num_y, num_z), ECL_FLOAT);
+
+    for (int j = 0; j < num_y; j++) {
+        for (int i = 0; i < num_x; i++) {
+            int offset = 6 * (i + j * num_x);
+            ecl_kw_iset_float(coord_kw, offset, i);
+            ecl_kw_iset_float(coord_kw, offset + 1, j);
+            ecl_kw_iset_float(coord_kw, offset + 2, -1);
+
+            ecl_kw_iset_float(coord_kw, offset + 3, i);
+            ecl_kw_iset_float(coord_kw, offset + 4, j);
+            ecl_kw_iset_float(coord_kw, offset + 5, -1);
+
+            for (int k = 0; k < num_z; k++) {
+                for (int c = 0; c < 4; c++) {
+                    int zi1 = ecl_grid_zcorn_index__(num_x, num_y, i, j, k, c);
+                    int zi2 =
+                        ecl_grid_zcorn_index__(num_x, num_y, i, j, k, c + 4);
+
+                    double z1 = k;
+                    double z2 = k + 1;
+
+                    ecl_kw_iset_float(zcorn_kw, zi1, z1);
+                    ecl_kw_iset_float(zcorn_kw, zi2, z2);
+                }
+            }
+        }
+    }
+
+    for (const auto &[i, j, k, c, z] : z_vector) {
+        auto index = ecl_grid_zcorn_index__(num_x, num_y, i, j, k, c);
+        ecl_kw_iset_float(zcorn_kw, index, z);
+    }
+
+    ecl_grid_type *grid = ecl_grid_alloc_GRDECL_kw(
+        num_x, num_y, num_z, zcorn_kw, coord_kw, NULL, NULL);
+    ecl_kw_free(coord_kw);
+    ecl_kw_free(zcorn_kw);
+
+    return grid;
+}
+
+TEST_CASE("Test no twistedness", "[unittest]") {
+    GIVEN("A grid with untwisted cells") {
+        ecl_grid_type *grid = generate_coordkw_grid(10, 7, 8, {});
+
+        AND_GIVEN("Any cell in that grid") {
+            auto i = GENERATE(take(3, random(0, 9)));
+            auto j = GENERATE(take(3, random(0, 6)));
+            auto k = GENERATE(take(3, random(0, 7)));
+
+            THEN("That cell should have a twist score of 0") {
+                REQUIRE(ecl_grid_get_cell_twist3(grid, i, j, k) == 0);
+            }
+        }
+
+        ecl_grid_free(grid);
+    }
+}
+
+TEST_CASE("Test simple twistedness", "[unittest]") {
+    GIVEN("A grid with the first cell twisted") {
+        auto z_vector = std::vector<std::tuple<int, int, int, int, double>>{
+            {0, 0, 0, 0, 0.0},
+            {0, 0, 0, 4, -0.25},
+        };
+        ecl_grid_type *grid = generate_coordkw_grid(10, 7, 8, z_vector);
+
+        THEN("That cell should have a twist score of 1") {
+            REQUIRE(ecl_grid_get_cell_twist3(grid, 0, 0, 0) == 1);
+        }
+
+        ecl_grid_free(grid);
+    }
+}
+
+TEST_CASE("Test double twistedness", "[unittest]") {
+    GIVEN("A grid with the first cell doubly twisted") {
+        auto z_vector = std::vector<std::tuple<int, int, int, int, double>>{
+            {0, 0, 0, 0, 0.0},
+            {0, 0, 0, 4, -0.25},
+            {0, 0, 0, 3, 0.0},
+            {0, 0, 0, 7, -0.25},
+        };
+        ecl_grid_type *grid = generate_coordkw_grid(10, 7, 8, z_vector);
+
+        THEN("That cell should have a twist score of 2") {
+            REQUIRE(ecl_grid_get_cell_twist3(grid, 0, 0, 0) == 2);
+        }
+
+        ecl_grid_free(grid);
     }
 }
