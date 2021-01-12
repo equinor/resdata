8d223db64e97f489550b2ef07998272953aa8197
Zohar Malamant <ZOM@equinor.com>
2021-01-12 09:57:25 +0100

    Add missing utils to Python package

---
+++ python/tests/test_bin.py
@@ -0,0 +1,35 @@
+import sys
+import pytest
+import subprocess
+import signal
+
+
+@pytest.mark.skipif(
+    sys.platform != "linux", reason="ecl utils are only available on Linux at this time"
+)
+@pytest.mark.parametrize(
+    "name,returncode,stderr",
+    [
+        ("CF_dump", -signal.SIGSEGV, b""),
+        ("convert.x", 1, b"Usage: convert.x"),
+        ("ecl_pack.x", 0, b""),
+        ("ecl_unpack.x", 1, b"ecl_unpack UNIFIED_FILE1"),
+        ("grdecl_grid", -signal.SIGABRT, b"util_fopen: failed to open:(null)"),
+        ("grdecl_test.x", -signal.SIGABRT, b"util_fopen: failed to open:(null)"),
+        ("grid_dump.x", 1, b"grid_dump.x: filename"),
+        ("grid_dump_ascii.x", 1, b"grid_dump_ascii.x: filename"),
+        ("grid_info.x", 1, b"grid_info.x: filename"),
+        ("kw_extract", 0, b"src_file target_file kw1 kw2 kw3"),
+        ("kw_list.x", 0, b""),
+        ("load_test.x", 0, b""),
+        ("make_grid", 1, b"make_grid: basename nx ny nz"),
+        ("ri_well_test", 1, b""),
+        ("segment_info", -signal.SIGSEGV, b""),
+        ("select_test.x", 0, b""),
+        ("summary.x", 1, b""),
+    ],
+)
+def test_exec(name: str, returncode: int, stderr: str) -> None:
+    status = subprocess.run([name], stderr=subprocess.PIPE)
+    assert status.returncode == returncode
+    assert stderr in status.stderr
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+import sys
 import skbuild
 import setuptools
 from setuptools_scm import get_version
@@ -9,6 +9,38 @@ version = get_version(relative_to=__file__, write_to="python/ecl/version.py")
 
 with open("README.md") as f:
     long_description = f.read()
+
+
+def utility_wrappers():
+    """
+    Wrappers around ecl's "application" utilities. These are only supported on
+    Linux at this time so only create the wrapper when on Linux.
+    """
+    if sys.platform != "linux":
+        return []
+
+    return [
+        name + " = ecl.bin:main"
+        for name in (
+            "CF_dump",
+            "convert.x",
+            "ecl_pack.x",
+            "ecl_unpack.x",
+            "grdecl_grid",
+            "grdecl_test.x",
+            "grid_dump.x",
+            "grid_dump_ascii.x",
+            "grid_info.x",
+            "kw_extract",
+            "kw_list.x",
+            "load_test.x",
+            "make_grid",
+            "ri_well_test",
+            "segment_info",
+            "select_test.x",
+            "summary.x",
+        )
+    ]
 
 
 skbuild.setup(
@@ -34,14 +66,11 @@ skbuild.setup(
         "six"
     ],
     entry_points={
-        "console_scripts": [
-            "ecl_pack.x = ecl.bin:ecl_pack",
-            "ecl_unpack.x = ecl.bin:ecl_unpack",
-        ]
+        "console_scripts": utility_wrappers()
     },
     cmake_args=[
         "-DECL_VERSION=" + version,
-        "-DBUILD_APPLICATIONS=ON",
+        "-DBUILD_APPLICATIONS=" + ("ON" if sys.platform == "linux" else "OFF"),
         "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
         "-DCMAKE_INSTALL_BINDIR=python/ecl/.bin",
         "-DCMAKE_INSTALL_LIBDIR=python/ecl/.libs",
