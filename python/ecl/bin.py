0da8a6c6c43f7d5f816ecd14ec6117bb44e9407c
Zohar Malamant <ZOM@equinor.com>
2021-01-12 09:57:25 +0100

    Add missing utils to Python package

--- python/ecl/bin.py
+++ python/ecl/bin.py
@@ -2,14 +2,7 @@ import os
 import sys
 
 
-def _exec(prog):
-    prog_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".bin", prog))
-    os.execl(prog_path, prog_path, *sys.argv[1:])
-
-
-def ecl_pack():
-    _exec("ecl_pack.x")
-
-
-def ecl_unpack():
-    _exec("ecl_unpack.x")
+def main():
+    prog = os.path.basename(sys.argv[0])
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".bin", prog))
+    os.execl(path, path, *sys.argv[1:])
