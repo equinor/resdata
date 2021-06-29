1fe38776cb5081527665ae996c790315de5dc590
Peter Verveer <pieter.verveer@tno.nl>
2021-06-29 14:53:56 +0200

    Remove the deprecated ert package

--- python/ecl/ecl/__init__.py
+++ python/ecl/ecl/__init__.py
@@ -8,13 +8,41 @@
 #
 # Both of these legacy forms are controlled by the cmake switch
 # INSTALL_ERT_LEGACY.
+#
+# EDIT: The ert package has been removed, the ecl.ecl option is still supported.
 
 import warnings
 
+from ecl import EclFileEnum, EclFileFlagEnum, EclPhaseEnum, EclUnitTypeEnum , EclUtil
+from ecl import EclTypeEnum, EclDataType
+from ecl.summary import EclSumVarType
+from ecl.summary import EclSumTStep
+from ecl.summary import EclSum #, EclSumVector, EclSumNode, EclSMSPECNode
+from ecl.summary import EclSumKeyWordVector
+from ecl.rft import EclPLTCell, EclRFTCell
+from ecl.rft import EclRFT, EclRFTFile
+from ecl.eclfile import FortIO, openFortIO
+from ecl.eclfile import EclKW
+from ecl.eclfile import Ecl3DKW
+from ecl.eclfile import EclFileView
+from ecl.eclfile import EclFile , openEclFile
+from ecl.eclfile import Ecl3DFile
+from ecl.eclfile import EclInitFile
+from ecl.eclfile import EclRestartFile
+from ecl.grid import EclGrid
+from ecl.grid import EclRegion
+from ecl.gravimetry import EclSubsidence
+from ecl.gravimetry import phase_deltag, deltag
+from ecl.gravimetry import EclGrav
+from ecl.summary import EclSumNode
+from ecl.summary import EclSumVector
+from ecl.summary import EclNPV , NPVPriceVector
+from ecl.summary import EclCmp
+from ecl.grid import EclGridGenerator
+
+
 warnings.warn(
-    "Importing from ert.ecl, ecl.ecl or ert is deprecated and will not be available in python3." \
-    " For eclipse functionality use \'from ecl import\', for ert workflow tooling use \'from res import\'.",
+    "Importing from ecl.ecl is deprecated and will not be available in python3." \
+    " For eclipse functionality use \'from ecl import\'.",
     DeprecationWarning
 )
-
-from ert.ecl import *
--- python/ert/ecl/faults/__init__.py
+++
@@ -1,7 +0,0 @@
-from ecl.grid.faults import Layer
-from ecl.grid.faults import FaultCollection
-from ecl.grid.faults import Fault
-from ecl.grid.faults import FaultLine
-from ecl.grid.faults import FaultSegment , SegmentMap
-from ecl.grid.faults import FaultBlock , FaultBlockCell
-from ecl.grid.faults import FaultBlockLayer
