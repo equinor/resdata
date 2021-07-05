f6d6bf447171062141a054e0067970b0d2a08db6
Zohar Malamant <ZOM@equinor.com>
2021-07-05 13:41:11 +0200

    Auto-reformat all CMake sources using cmake-format

--- cmake/Modules/FindCXX11Features.cmake
+++ cmake/Modules/FindCXX11Features.cmake
@@ -3,17 +3,20 @@
 #
 
 include(CheckCXXCompilerFlag)
-if (NOT MSVC)
-    CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-    CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+if(NOT MSVC)
+  check_cxx_compiler_flag("-std=c++11" COMPILER_SUPPORTS_CXX11)
+  check_cxx_compiler_flag("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
 
-    if(COMPILER_SUPPORTS_CXX11)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    elseif(COMPILER_SUPPORTS_CXX0X)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-    else()
-        message(SEND_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
-    endif()
+  if(COMPILER_SUPPORTS_CXX11)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+  elseif(COMPILER_SUPPORTS_CXX0X)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+  else()
+    message(
+      SEND_ERROR
+        "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler."
+    )
+  endif()
 endif()
 
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
--- cmake/ert_api_check.cmake
+++ cmake/ert_api_check.cmake
@@ -1,12 +1,10 @@
-# This file contains feature checks which affect the API of the final
-# product; i.e. if the test for zlib fails the function
-# buffer_fwrite_compressed() will not be available in the final
-# installation.
-# 
-# The results of these tests will be assembled in the
-# ert/util/ert_api_config.h header; all the symbols in that header will
-# have a ERT_ prefix. The generated header is part of the api and can be
-# included by other header files in the ert source.
+# This file contains feature checks which affect the API of the final product;
+# i.e. if the test for zlib fails the function buffer_fwrite_compressed() will
+# not be available in the final installation.
+#
+# The results of these tests will be assembled in the ert/util/ert_api_config.h
+# header; all the symbols in that header will have a ERT_ prefix. The generated
+# header is part of the api and can be included by other header files in the ert
+# source.
 
-#-----------------------------------------------------------------
-
+# -----------------------------------------------------------------
