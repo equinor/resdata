a46ab8b2f018dd30f264c61b2d639ac68bbc23ec
Sondre Sortland <sonso@equinor.com>
2021-10-11 15:36:41 +0200

    Add support for Python 3.10

--- ci/github/build_linux_wheel.sh
+++ ci/github/build_linux_wheel.sh
@@ -6,6 +6,7 @@ case "$1" in
     3.7) pyver=cp37-cp37m ;;
     3.8) pyver=cp38-cp38 ;;
     3.9) pyver=cp39-cp39 ;;
+    3.10) pyver=cp310-cp310 ;;
     *)
         echo "Unknown Python version $1"
         exit 1
