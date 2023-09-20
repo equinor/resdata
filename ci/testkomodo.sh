c01e205ce94e5f2a4ce50e48ea5ce0d329cd252e
Håvard Berland <havb@equinor.com>
2023-09-20 13:22:09 +0200

    Move ci functions to this repo
    Only ecl is using these two functions from the komodo repository.
    Move them into here so that they can be purged from upstream.

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -1,3 +1,25 @@
+function ci_install_cmake {
+    pip install cmake ninja
+
+    local root=${KOMODO_ROOT}/${CI_KOMODO_RELEASE}/root
+
+    export CMAKE_GENERATOR=Ninja
+    export CMAKE_PREFIX_PATH=$root
+
+    # Colour!
+    export CFLAGS="${CFLAGS:-} -fdiagnostics-color=always"
+    export CXXFLAGS="${CXXFLAGS:-} -fdiagnostics-color=always"
+    export LDFLAGS="${LDFLAGS:-} -fdiagnostics-color=always"
+
+    export LD_LIBRARY_PATH=$root/lib:$root/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+}
+function ci_install_conan {
+    pip install "conan<2"
+
+    # Conan v1 bundles its own certs due to legacy reasons, so we point it
+    # to the system's certs instead.
+    export CONAN_CACERT_PATH=/etc/pki/tls/cert.pem
+}
 build_and_run_ctest () {
     pushd $CI_TEST_ROOT
     cmake $CI_SOURCE_ROOT                                  \
