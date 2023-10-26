4a79e50aecca9f326ce1028ca18a5795ee2ae6d3
Eivind Jahren <ejah@equinor.com>
2023-10-26 09:17:34 +0200

    Remove deprecated ENABLE_PYTHON

--- cmake/ert_lib_check.cmake
+++
@@ -1,3 +0,0 @@
-# This file contains various checks which will append to the list
-# $ERT_EXTERNAL_UTIL_LIBS which should contain all the external library
-# dependencies. Observe that all library dependencies go transitively
