7b269d929c8de5b05a24120912d83bc965351c84
Eivind Jahren <ejah@equinor.com>
2023-10-27 13:17:08 +0200

    Remove broken ecl_grav_calc
    _pase_deltag attempted to bind ecl_grav_phase_deltag from the
    c code, but because ecl_grav_calc.hpp was not included in
    ecl_grav_calc.cpp, ecl_grav_phase_deltag was given cpp name
    mangling and not found by cwrap. This has likely not ever
    worked when compiled with cpp compiler, which it has since
    4ff7f76d272f0cd113eda14ddba18421506a3882

--- python/ecl/gravimetry/__init__.py
+++ python/ecl/gravimetry/__init__.py
@@ -7,6 +7,5 @@
      functionality.
 """
 
-from .ecl_subsidence import EclSubsidence
-from .ecl_grav_calc import phase_deltag, deltag
 from .ecl_grav import EclGrav
+from .ecl_subsidence import EclSubsidence
