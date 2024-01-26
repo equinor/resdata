a84eab1f2623e63cabb8f07278dd15eede1008f3
Eivind Jahren <ejah@equinor.com>
2024-01-26 12:44:31 +0100

    Remove unused string_util code

--- lib/include/ert/util/string_util.hpp
+++ lib/include/ert/util/string_util.hpp
@@ -8,22 +8,10 @@
 extern "C" {
 #endif
 
-bool string_util_init_active_list(const char *range_string,
-                                  int_vector_type *active_list);
-bool string_util_update_active_list(const char *range_string,
-                                    int_vector_type *active_list);
 int_vector_type *string_util_alloc_active_list(const char *range_string);
-
-bool string_util_init_active_mask(const char *range_string,
-                                  bool_vector_type *active_mask);
 bool string_util_update_active_mask(const char *range_string,
                                     bool_vector_type *active_mask);
 bool_vector_type *string_util_alloc_active_mask(const char *range_string);
-
-bool string_util_update_value_list(const char *range_string,
-                                   int_vector_type *value_list);
-bool string_util_init_value_list(const char *range_string,
-                                 int_vector_type *value_list);
 int_vector_type *string_util_alloc_value_list(const char *range_string);
 
 #ifdef __cplusplus
