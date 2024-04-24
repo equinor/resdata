8f443d1b2771239c9661b98c60769956ef6c0c8c
Eivind Jahren <ejah@equinor.com>
2024-04-24 09:37:32 +0200

    Fix seq_hdr type in test

--- python/tests/rd_tests/test_geertsma.py
+++ python/tests/rd_tests/test_geertsma.py
@@ -24,7 +24,7 @@ def create_init(grid, case):
 
 def create_restart(grid, case, p1, p2=None, rporv1=None, rporv2=None):
     with openFortIO("%s.UNRST" % case, mode=FortIO.WRITE_MODE) as f:
-        seq_hdr = ResdataKW("SEQNUM", 1, ResDataType.RD_FLOAT)
+        seq_hdr = ResdataKW("SEQNUM", 1, ResDataType.RD_INT)
         seq_hdr[0] = 10
         p = ResdataKW("PRESSURE", grid.getNumActive(), ResDataType.RD_FLOAT)
         for i in range(len(p1)):
