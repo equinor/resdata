dc93cb1bfb2fafbbabbba811dea2573d19536fca
Eivind Jahren <ejah@equinor.com>
2024-08-14 09:18:35 +0200

    Remove unused c functions in rd_file_kw

--- lib/include/resdata/rd_file_kw.hpp
+++ lib/include/resdata/rd_file_kw.hpp
@@ -24,18 +24,13 @@ rd_file_kw_type *rd_file_kw_alloc(const rd_kw_type *rd_kw, offset_type offset);
 rd_file_kw_type *rd_file_kw_alloc0(const char *header, rd_data_type data_type,
                                    int size, offset_type offset);
 void rd_file_kw_free(rd_file_kw_type *file_kw);
-void rd_file_kw_free__(void *arg);
 rd_kw_type *rd_file_kw_get_kw(rd_file_kw_type *file_kw, fortio_type *fortio,
                               inv_map_type *inv_map);
 rd_kw_type *rd_file_kw_get_kw_ptr(rd_file_kw_type *file_kw);
-rd_file_kw_type *rd_file_kw_alloc_copy(const rd_file_kw_type *src);
 const char *rd_file_kw_get_header(const rd_file_kw_type *file_kw);
 int rd_file_kw_get_size(const rd_file_kw_type *file_kw);
 rd_data_type rd_file_kw_get_data_type(const rd_file_kw_type *);
 offset_type rd_file_kw_get_offset(const rd_file_kw_type *file_kw);
-bool rd_file_kw_ptr_eq(const rd_file_kw_type *file_kw, const rd_kw_type *rd_kw);
-void rd_file_kw_replace_kw(rd_file_kw_type *file_kw, fortio_type *target,
-                           rd_kw_type *new_kw);
 bool rd_file_kw_fskip_data(const rd_file_kw_type *file_kw, fortio_type *fortio);
 void rd_file_kw_inplace_fwrite(rd_file_kw_type *file_kw, fortio_type *fortio);
 
