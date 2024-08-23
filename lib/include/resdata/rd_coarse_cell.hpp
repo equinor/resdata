5d85efc940f6c0faf6bd5cdb72d85c7d415f6a85
Eivind Jahren <ejah@equinor.com>
2024-08-23 13:38:52 +0200

    Remove unused c code in rd_coarse_cell

--- lib/include/resdata/rd_coarse_cell.hpp
+++ lib/include/resdata/rd_coarse_cell.hpp
@@ -15,17 +15,9 @@ void rd_coarse_cell_update(rd_coarse_cell_type *coarse_cell, int i, int j,
 void rd_coarse_cell_free(rd_coarse_cell_type *coarse_cell);
 void rd_coarse_cell_free__(void *arg);
 
-int rd_coarse_cell_get_i1(const rd_coarse_cell_type *coarse_cell);
-int rd_coarse_cell_get_j1(const rd_coarse_cell_type *coarse_cell);
-int rd_coarse_cell_get_k1(const rd_coarse_cell_type *coarse_cell);
-int rd_coarse_cell_get_i2(const rd_coarse_cell_type *coarse_cell);
-int rd_coarse_cell_get_j2(const rd_coarse_cell_type *coarse_cell);
-int rd_coarse_cell_get_k2(const rd_coarse_cell_type *coarse_cell);
 const int *rd_coarse_cell_get_box_ptr(const rd_coarse_cell_type *coarse_cell);
 
 int rd_coarse_cell_get_size(const rd_coarse_cell_type *coarse_cell);
-int rd_coarse_cell_iget_cell_index(rd_coarse_cell_type *coarse_cell,
-                                   int group_index);
 const int *rd_coarse_cell_get_index_ptr(rd_coarse_cell_type *coarse_cell);
 const int_vector_type *
 rd_coarse_cell_get_index_vector(rd_coarse_cell_type *coarse_cell);
@@ -42,9 +34,6 @@ int rd_coarse_cell_iget_active_cell_index(
 int rd_coarse_cell_iget_active_value(const rd_coarse_cell_type *coarse_cell,
                                      int index);
 int rd_coarse_cell_get_num_active(const rd_coarse_cell_type *coarse_cell);
-void rd_coarse_cell_fprintf(const rd_coarse_cell_type *coarse_cell,
-                            FILE *stream);
-
 #ifdef __cplusplus
 }
 #endif
