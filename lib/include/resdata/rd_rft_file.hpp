adf2c7c99faed551596968955f4749222d0bd5a8
Eivind Jahren <ejah@equinor.com>
2025-11-06 11:52:22 +0100

    Use per function extern "C" and pragma once for rft
    This means some functions can use cpp calling convention

--- lib/include/resdata/rd_rft_file.hpp
+++ lib/include/resdata/rd_rft_file.hpp
@@ -1,9 +1,4 @@
-#ifndef ERT_RD_RFT_FILE_H
-#define ERT_RD_RFT_FILE_H
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <stdbool.h>
+#pragma once
 
 #include <ert/util/stringlist.hpp>
 
@@ -13,21 +8,17 @@ extern "C" {
 
 typedef struct rd_rft_file_struct rd_rft_file_type;
 
-rd_rft_file_type *rd_rft_file_alloc_case(const char *case_input);
-bool rd_rft_file_case_has_rft(const char *case_input);
+extern "C" rd_rft_file_type *rd_rft_file_alloc_case(const char *case_input);
+extern "C" bool rd_rft_file_case_has_rft(const char *case_input);
 rd_rft_file_type *rd_rft_file_alloc(const char *);
-void rd_rft_file_free(rd_rft_file_type *);
-int rd_rft_file_get_size__(const rd_rft_file_type *rft_file,
-                           const char *well_pattern, time_t recording_time);
+extern "C" void rd_rft_file_free(rd_rft_file_type *);
+extern "C" int rd_rft_file_get_size__(const rd_rft_file_type *rft_file,
+                                      const char *well_pattern,
+                                      time_t recording_time);
 int rd_rft_file_get_size(const rd_rft_file_type *rft_file);
-rd_rft_node_type *
+extern "C" rd_rft_node_type *
 rd_rft_file_get_well_time_rft(const rd_rft_file_type *rft_file,
                               const char *well, time_t recording_time);
-rd_rft_node_type *rd_rft_file_iget_node(const rd_rft_file_type *rft_file,
-                                        int index);
-int rd_rft_file_get_num_wells(const rd_rft_file_type *rft_file);
-
-#ifdef __cplusplus
-}
-#endif
-#endif
+extern "C" rd_rft_node_type *
+rd_rft_file_iget_node(const rd_rft_file_type *rft_file, int index);
+extern "C" int rd_rft_file_get_num_wells(const rd_rft_file_type *rft_file);
