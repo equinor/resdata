f89bc9835f62e86e14e050ba102376631820d6fd
Eivind Jahren <ejah@equinor.com>
2025-11-14 11:44:32 +0100

    Add test of that segment is not implement

--- python/tests/rd_tests/test_rft_file.py
+++ python/tests/rd_tests/test_rft_file.py
@@ -1,5 +1,5 @@
 import datetime
-from resdata.rft import ResdataRFTFile, ResdataRFTCell
+from resdata.rft import ResdataRFTFile, ResdataRFTCell, ResdataPLTCell
 from tests import source_root
 import pytest
 import shutil
@@ -134,14 +134,14 @@ float_arr = partial(np.array, dtype=np.float32)
 int_arr = partial(np.array, dtype=np.int32)
 
 
-def cell_start(date=(1, 1, 2000), *args, **kwargs):
+def cell_start(date=(1, 1, 2000), ijks=((1, 1, 1), (2, 1, 2)), *args, **kwargs):
     return [
         ("TIME    ", float_arr([24.0])),
         ("DATE    ", int_arr(date)),
         ("WELLETC ", well_etc(*args, **kwargs)),
-        ("CONIPOS ", int_arr([1, 2])),
-        ("CONJPOS ", int_arr([1, 1])),
-        ("CONKPOS ", int_arr([1, 2])),
+        ("CONIPOS ", int_arr([i for i, _, _ in ijks])),
+        ("CONJPOS ", int_arr([j for _, j, _ in ijks])),
+        ("CONKPOS ", int_arr([k for _, _, k in ijks])),
     ]
 
 
@@ -389,3 +389,183 @@ def test_that_msw_cells_are_sorted_by_connection_start(tmp_path):
     assert not rft_file[1].is_MSW()
     assert rft_file[1][0].wrat == 2.0
     assert rft_file[1][1].wrat == 3.0
+
+
+def test_that_segment_data_is_not_implemented(tmp_path):
+    rft_file = make_rft_file(
+        tmp_path,
+        [
+            *cell_start(data_category=b"S"),
+            ("SEGPRES ", float_arr([3.0, 1.0])),
+        ],
+    )
+
+    if len(rft_file) > 0:
+        print(rft_file[0])
+
+
+@pytest.mark.parametrize("categories", [b"PS", b"RS", b"RPS"])
+def test_that_segment_data_is_ignored(tmp_path, categories):
+    rft_file = make_rft_file(
+        tmp_path,
+        [
+            *cell_start(data_category=categories),
+            ("SEGPRES ", float_arr([3.0, 1.0])),
+            ("CONLENST", float_arr([33.0, 34.0])),
+            ("CONLENEN", float_arr([35.0, 36.0])),
+            ("PRESSURE", float_arr([101.0, 201.0])),
+            ("SWAT    ", float_arr([199.0, 2198.0])),
+            ("SGAS    ", float_arr([140.0, 141.0])),
+            ("DEPTH   ", float_arr([21.0, 31.0])),
+            *plt_fields(),
+        ],
+    )
+    assert len(rft_file) == 1
+    assert not rft_file[0].is_SEGMENT()
+    if b"P" in categories:
+        assert rft_file[0].is_PLT()
+        assert isinstance(rft_file[0][0], ResdataPLTCell)
+    else:
+        assert rft_file[0].is_RFT()
+        assert isinstance(rft_file[0][0], ResdataRFTCell)
+
+
+def test_that_rft_with_single_cell_works_correctly(tmp_path):
+    rft_file = make_rft_file(
+        tmp_path,
+        [
+            ("TIME    ", float_arr([24.0])),
+            ("DATE    ", int_arr([1, 1, 2000])),
+            ("WELLETC ", well_etc()),
+            ("CONIPOS ", int_arr([5])),
+            ("CONJPOS ", int_arr([3])),
+            ("CONKPOS ", int_arr([2])),
+            ("PRESSURE", float_arr([150.0])),
+            ("SWAT    ", float_arr([0.25])),
+            ("SGAS    ", float_arr([0.15])),
+            ("DEPTH   ", float_arr([2500.0])),
+        ],
+    )
+    assert len(rft_file) == 1
+    assert len(rft_file[0]) == 1
+    assert rft_file[0][0].pressure == 150.0
+    assert rft_file[0][0].swat == pytest.approx(0.25)
+    assert rft_file[0][0].sgas == pytest.approx(0.15)
+    assert rft_file[0][0].soil == pytest.approx(0.6)
+    assert rft_file[0][0].get_ijk() == (4, 2, 1)
+
+
+def test_that_plt_with_single_cell_works_correctly(tmp_path):
+    rft_file = make_rft_file(
+        tmp_path,
+        [
+            ("TIME    ", float_arr([24.0])),
+            ("DATE    ", int_arr([1, 1, 2000])),
+            ("WELLETC ", well_etc(data_category=b"P")),
+            ("CONIPOS ", int_arr([5])),
+            ("CONJPOS ", int_arr([3])),
+            ("CONKPOS ", int_arr([2])),
+            ("CONWRAT ", float_arr([5.0])),
+            ("CONGRAT ", float_arr([6.0])),
+            ("CONORAT ", float_arr([7.0])),
+            ("CONDEPTH", float_arr([2500.0])),
+            ("CONVTUB ", float_arr([8.0])),
+            ("CONOTUB ", float_arr([9.0])),
+            ("CONGTUB ", float_arr([10.0])),
+            ("CONWTUB ", float_arr([11.0])),
+            ("CONPRES ", float_arr([100.0])),
+        ],
+    )
+    assert len(rft_file) == 1
+    assert len(rft_file[0]) == 1
+    assert rft_file[0][0].pressure == 100.0
+    assert rft_file[0][0].wrat == 5.0
+    assert rft_file[0][0].grat == 6.0
+    assert rft_file[0][0].orat == 7.0
+
+
+def test_that_empty_rft_file_has_zero_size(tmp_path):
+    file = tmp_path / "EMPTY.RFT"
+    resfo.write(file, [])
+    rft_file = ResdataRFTFile(str(file))
+    assert len(rft_file) == 0
+    assert rft_file.size() == 0
+    assert rft_file.get_num_wells() == 0
+    assert rft_file.get_headers() == []
+
+
+def test_that_mixed_msw_and_non_msw_plt_cells_maintain_correct_order(tmp_path):
+    rft_file = make_rft_file(
+        tmp_path,
+        [
+            *cell_start(
+                data_category=b"P",
+                well_name=b"MSW_WELL",
+                ijks=[(2, 2, 1), (2, 2, 1), (2, 2, 2)],
+            ),
+            ("CONLENST", float_arr([30.0, 10.0, 20.0])),
+            ("CONLENEN", float_arr([40.0, 20.0, 30.0])),
+            ("CONWRAT ", float_arr([1.0, 2.0, 3.0])),
+            ("CONGRAT ", float_arr([4.0, 5.0, 6.0])),
+            ("CONORAT ", float_arr([7.0, 8.0, 9.0])),
+            ("CONDEPTH", float_arr([2000.0, 3000.0, 4000.0])),
+            ("CONVTUB ", float_arr([10.0, 11.0, 12.0])),
+            ("CONOTUB ", float_arr([13.0, 14.0, 15.0])),
+            ("CONGTUB ", float_arr([16.0, 17.0, 18.0])),
+            ("CONWTUB ", float_arr([19.0, 20.0, 21.0])),
+            ("CONPRES ", float_arr([100.0, 200.0, 300.0])),
+            *cell_start(
+                data_category=b"P",
+                well_name=b"REGULAR_WELL",
+                ijks=[(1, 1, 1), (1, 1, 1), (1, 1, 2)],
+            ),
+            ("CONWRAT ", float_arr([5.0, 6.0, 7.0])),
+            ("CONGRAT ", float_arr([8.0, 9.0, 10.0])),
+            ("CONORAT ", float_arr([11.0, 12.0, 13.0])),
+            ("CONDEPTH", float_arr([1000.0, 1500.0, 2000.0])),
+            ("CONVTUB ", float_arr([14.0, 15.0, 16.0])),
+            ("CONOTUB ", float_arr([17.0, 18.0, 19.0])),
+            ("CONGTUB ", float_arr([20.0, 21.0, 22.0])),
+            ("CONWTUB ", float_arr([23.0, 24.0, 25.0])),
+            ("CONPRES ", float_arr([50.0, 60.0, 70.0])),
+        ],
+    )
+
+    msw_well = rft_file[0]
+    assert msw_well.is_MSW()
+    assert msw_well[0].conn_start == 10.0
+    assert msw_well[0].wrat == 2.0
+    assert msw_well[1].conn_start == 20.0
+    assert msw_well[1].wrat == 3.0
+    assert msw_well[2].conn_start == 30.0
+    assert msw_well[2].wrat == 1.0
+
+    regular_well = rft_file[1]
+    assert not regular_well.is_MSW()
+    assert regular_well[0].wrat == 5.0
+    assert regular_well[1].wrat == 6.0
+    assert regular_well[2].wrat == 7.0
+
+
+def test_that_multiple_rfts_for_same_well_and_date_returns_first(tmp_path):
+    file = tmp_path / "CASE.RFT"
+    resfo.write(
+        file,
+        [
+            *cell_start(well_name=b"WELL1", date=(1, 1, 2000)),
+            ("PRESSURE", float_arr([100.0, 200.0])),
+            ("SWAT    ", float_arr([0.1, 0.2])),
+            ("SGAS    ", float_arr([0.3, 0.4])),
+            ("DEPTH   ", float_arr([20.0, 30.0])),
+            *cell_start(well_name=b"WELL1", date=(1, 1, 2000)),
+            ("PRESSURE", float_arr([999.0, 888.0])),
+            ("SWAT    ", float_arr([0.5, 0.6])),
+            ("SGAS    ", float_arr([0.1, 0.2])),
+            ("DEPTH   ", float_arr([25.0, 35.0])),
+        ],
+    )
+    rft_file = ResdataRFTFile(str(file))
+    assert rft_file.size() == 2
+    result = rft_file.get("WELL1", datetime.date(2000, 1, 1))
+    assert result[0].pressure == 100.0
+    assert result[1].pressure == 200.0
