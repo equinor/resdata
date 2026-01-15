cab0c30127871f71ab13e542c176f8c98f3ba31e
Eivind Jahren <ejah@equinor.com>
2026-01-15 09:39:28 +0100

    Fix memory leak in util_abort
    Calling util_abort with abort_flag set will longjmp to the
    setjump location. This breaks cpp stack unwinding so trace
    is never freed. This is fixed by creating trace inside
    a block, and calling longjmp only after trace is freed.
    
    This changes the behavior of util_abort with abort_flag as
    that will print the abort_dump. However, this should have
    no effect as abort_flag is only set in tests, and in any
    case, the effect is just that the trace will be printed.

--- lib/util/util_abort.cpp
+++ lib/util/util_abort.cpp
@@ -63,8 +63,8 @@ extern "C" [[noreturn]] void util_abort__(const char *file,
         std::abort();
 
     /* Get trace */
-    std::string trace;
     {
+        std::string trace;
         backward::StackTrace stacktrace;
         stacktrace.load_here();
 
@@ -72,52 +72,53 @@ extern "C" [[noreturn]] void util_abort__(const char *file,
         backward::Printer printer;
         printer.print(stacktrace, pretty_trace);
         trace = pretty_trace.str();
+
+        /* Get formatted error message */
+        va_list ap;
+        char message[4096];
+        va_start(ap, fmt);
+        vsnprintf(message, sizeof(message), fmt, ap);
+        va_end(ap);
+
+        /* User-defined abort handler */
+        if (abort_handler)
+            abort_handler(file, line, function, message, trace.c_str());
+
+        FILE *abort_dump = nullptr;
+#ifndef WIN32
+        std::string abort_dump_path;
+        if (!getenv("ERT_SHOW_BACKTRACE")) {
+            abort_dump_path = make_dump_path();
+            abort_dump = fopen(abort_dump_path.c_str(), "w");
+        }
+#endif
+        if (abort_dump == nullptr)
+            abort_dump = stderr;
+
+        print(abort_dump,
+              "\n\nAbort called from: {} ({}:{})\n\nError message: {}\n\n",
+              function, file, line, message);
+        print(abort_dump, "{}\n", std::string(80, '-'));
+        fputs(trace.c_str(), abort_dump);
+        print(abort_dump, "{}\n\n", std::string(80, '-'));
+
+#ifndef WIN32
+        if (abort_dump != stderr) {
+            print(stderr,
+                  "\nError message: {}\nSee file: {} for more details of the "
+                  "crash.\nSetting the environment variable "
+                  "\"ERT_SHOW_BACKTRACE\" "
+                  "will show the backtrace on stderr.\n",
+                  message, abort_dump_path);
+        }
+#endif
     }
-
-    /* Get formatted error message */
-    va_list ap;
-    char message[4096];
-    va_start(ap, fmt);
-    vsnprintf(message, sizeof(message), fmt, ap);
-    va_end(ap);
-
-    /* User-defined abort handler */
-    if (abort_handler)
-        abort_handler(file, line, function, message, trace.c_str());
 
     /* Exit out of util_abort without aborting. Used in tests. */
     if (test_intercept_function == function) {
         abort_flag = false; // reset abort condition
         longjmp(test_jmp_buf, 0);
     }
-
-    FILE *abort_dump = nullptr;
-#ifndef WIN32
-    std::string abort_dump_path;
-    if (!getenv("ERT_SHOW_BACKTRACE")) {
-        abort_dump_path = make_dump_path();
-        abort_dump = fopen(abort_dump_path.c_str(), "w");
-    }
-#endif
-    if (abort_dump == nullptr)
-        abort_dump = stderr;
-
-    print(abort_dump,
-          "\n\nAbort called from: {} ({}:{})\n\nError message: {}\n\n",
-          function, file, line, message);
-    print(abort_dump, "{}\n", std::string(80, '-'));
-    fputs(trace.c_str(), abort_dump);
-    print(abort_dump, "{}\n\n", std::string(80, '-'));
-
-#ifndef WIN32
-    if (abort_dump != stderr) {
-        print(stderr,
-              "\nError message: {}\nSee file: {} for more details of the "
-              "crash.\nSetting the environment variable \"ERT_SHOW_BACKTRACE\" "
-              "will show the backtrace on stderr.\n",
-              message, abort_dump_path);
-    }
-#endif
 
     std::abort();
 }
