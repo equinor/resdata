8a33514eb5e8705140e3ed80d23216c3b79fcaac
Eivind Jahren <ejah@equinor.com>
2026-04-27 07:30:44 +0200

    Avoid manual memory management in generate_coordkw_grid

--- lib/tests/grid_fixtures.hpp
+++ lib/tests/grid_fixtures.hpp
@@ -12,6 +12,17 @@
 #include <resdata/rd_kw.hpp>
 #include <resdata/rd_kw_magic.hpp>
 
+inline void set_pillar(rd_kw_type *coord_kw, int pillar, float tx, float ty,
+                       float tz, float bx, float by, float bz) {
+    const int off = 6 * pillar;
+    rd_kw_iset_float(coord_kw, off + 0, tx);
+    rd_kw_iset_float(coord_kw, off + 1, ty);
+    rd_kw_iset_float(coord_kw, off + 2, tz);
+    rd_kw_iset_float(coord_kw, off + 3, bx);
+    rd_kw_iset_float(coord_kw, off + 4, by);
+    rd_kw_iset_float(coord_kw, off + 5, bz);
+}
+
 /**
  * Generates a grid, using the rd_grid_alloc_GRDECL_kw constructor,
  * with the x,y,z coordinates of the 0th corner of the ijkth cel is
@@ -21,24 +32,17 @@
  * Takes a vector of i,j,k,c,z tuples which changes the corner c of cell
  * at i,j,k z value.
  */
-inline rd_grid_type *generate_coordkw_grid(
+inline rd_grid_ptr generate_coordkw_grid(
     int num_x, int num_y, int num_z,
     const std::vector<std::tuple<int, int, int, int, double>> &z_vector) {
-    rd_kw_type *coord_kw =
-        rd_kw_alloc(COORD_KW, RD_GRID_COORD_SIZE(num_x, num_y), RD_FLOAT);
-    rd_kw_type *zcorn_kw = rd_kw_alloc(
-        ZCORN_KW, RD_GRID_ZCORN_SIZE(num_x, num_y, num_z), RD_FLOAT);
+    auto coord_kw =
+        make_rd_kw(COORD_KW, RD_GRID_COORD_SIZE(num_x, num_y), RD_FLOAT);
+    auto zcorn_kw =
+        make_rd_kw(ZCORN_KW, RD_GRID_ZCORN_SIZE(num_x, num_y, num_z), RD_FLOAT);
 
     for (int j = 0; j < num_y; j++) {
         for (int i = 0; i < num_x; i++) {
-            int offset = 6 * (i + j * num_x);
-            rd_kw_iset_float(coord_kw, offset, i);
-            rd_kw_iset_float(coord_kw, offset + 1, j);
-            rd_kw_iset_float(coord_kw, offset + 2, -1);
-
-            rd_kw_iset_float(coord_kw, offset + 3, i);
-            rd_kw_iset_float(coord_kw, offset + 4, j);
-            rd_kw_iset_float(coord_kw, offset + 5, -1);
+            set_pillar(coord_kw.get(), i + j * num_x, i, j, -1, i, j, -1);
 
             for (int k = 0; k < num_z; k++) {
                 for (int c = 0; c < 4; c++) {
@@ -49,8 +53,8 @@ inline rd_grid_type *generate_coordkw_grid(
                     double z1 = k;
                     double z2 = k + 1;
 
-                    rd_kw_iset_float(zcorn_kw, zi1, z1);
-                    rd_kw_iset_float(zcorn_kw, zi2, z2);
+                    rd_kw_iset_float(zcorn_kw.get(), zi1, z1);
+                    rd_kw_iset_float(zcorn_kw.get(), zi2, z2);
                 }
             }
         }
@@ -58,15 +62,10 @@ inline rd_grid_type *generate_coordkw_grid(
 
     for (const auto &[i, j, k, c, z] : z_vector) {
         auto index = rd_grid_zcorn_index__(num_x, num_y, i, j, k, c);
-        rd_kw_iset_float(zcorn_kw, index, z);
+        rd_kw_iset_float(zcorn_kw.get(), index, z);
     }
 
-    rd_grid_type *grid = rd_grid_alloc_GRDECL_kw(num_x, num_y, num_z, zcorn_kw,
-                                                 coord_kw, NULL, NULL);
-    rd_kw_free(coord_kw);
-    rd_kw_free(zcorn_kw);
-
-    return grid;
+    return {rd_grid_alloc_GRDECL_kw(num_x, num_y, num_z, zcorn_kw.get(),
+                                    coord_kw.get(), nullptr, nullptr),
+            &rd_grid_free};
 }
-
-
