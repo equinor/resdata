9e20877de0d5de94ce3941cc5152a8fb1c4f3e31
Eivind Jahren <ejah@equinor.com>
2026-04-27 07:29:13 +0200

    Move generate_coordkw_grid into grid_fixtures.hpp

--- lib/tests/test_rd_grid_regular.cpp
+++ lib/tests/test_rd_grid_regular.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "tmpdir.hpp"
+#include "grid_fixtures.hpp"
 
 using namespace Catch;
 using namespace Matchers;
@@ -23,63 +24,6 @@ TEST_CASE("Test unfractured grids", "[unittest]") {
 
         rd_grid_free(grid);
     }
-}
-
-/**
- * Generates a grid, using the rd_grid_alloc_GRDECL_kw constructor,
- * with the x,y,z coordinates of the 0th corner of the ijkth cel is
- * i,j,k. In other words this is a grid where the corners are on the
- * whole numbers.
- *
- * Takes a vector of i,j,k,c,z tuples which changes the corner c of cell
- * at i,j,k z value.
- */
-rd_grid_type *generate_coordkw_grid(
-    int num_x, int num_y, int num_z,
-    const std::vector<std::tuple<int, int, int, int, double>> &z_vector) {
-    rd_kw_type *coord_kw =
-        rd_kw_alloc(COORD_KW, RD_GRID_COORD_SIZE(num_x, num_y), RD_FLOAT);
-    rd_kw_type *zcorn_kw = rd_kw_alloc(
-        ZCORN_KW, RD_GRID_ZCORN_SIZE(num_x, num_y, num_z), RD_FLOAT);
-
-    for (int j = 0; j < num_y; j++) {
-        for (int i = 0; i < num_x; i++) {
-            int offset = 6 * (i + j * num_x);
-            rd_kw_iset_float(coord_kw, offset, i);
-            rd_kw_iset_float(coord_kw, offset + 1, j);
-            rd_kw_iset_float(coord_kw, offset + 2, -1);
-
-            rd_kw_iset_float(coord_kw, offset + 3, i);
-            rd_kw_iset_float(coord_kw, offset + 4, j);
-            rd_kw_iset_float(coord_kw, offset + 5, -1);
-
-            for (int k = 0; k < num_z; k++) {
-                for (int c = 0; c < 4; c++) {
-                    int zi1 = rd_grid_zcorn_index__(num_x, num_y, i, j, k, c);
-                    int zi2 =
-                        rd_grid_zcorn_index__(num_x, num_y, i, j, k, c + 4);
-
-                    double z1 = k;
-                    double z2 = k + 1;
-
-                    rd_kw_iset_float(zcorn_kw, zi1, z1);
-                    rd_kw_iset_float(zcorn_kw, zi2, z2);
-                }
-            }
-        }
-    }
-
-    for (const auto &[i, j, k, c, z] : z_vector) {
-        auto index = rd_grid_zcorn_index__(num_x, num_y, i, j, k, c);
-        rd_kw_iset_float(zcorn_kw, index, z);
-    }
-
-    rd_grid_type *grid = rd_grid_alloc_GRDECL_kw(num_x, num_y, num_z, zcorn_kw,
-                                                 coord_kw, NULL, NULL);
-    rd_kw_free(coord_kw);
-    rd_kw_free(zcorn_kw);
-
-    return grid;
 }
 
 TEST_CASE_METHOD(Tmpdir, "Test case loading", "[unittest]") {
