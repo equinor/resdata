6b85b29843388eb7e040eee8549b86c44e71ccac
Eivind Jahren <ejah@equinor.com>
2026-05-07 11:22:42 +0200

    Use py::ssize_t for comparing against buffer.size

--- lib/resdata/rd_grid_pybind.cpp
+++ lib/resdata/rd_grid_pybind.cpp
@@ -389,7 +389,7 @@ PYBIND11_MODULE(_grid, m) {
               std::fill(data_ptr, data_ptr + data_buffer.size, fill_value);
 
               int *input = rd_kw_get_int_ptr(rd_kw);
-              for (size_t i = 0; i < idx_buffer.size; i++) {
+              for (py::ssize_t i = 0; i < idx_buffer.size; i++) {
                   int32_t di = idx_ptr[i];
                   if (di >= 0)
                       data_ptr[i] = input[di];
@@ -407,7 +407,7 @@ PYBIND11_MODULE(_grid, m) {
               double *data_ptr = static_cast<double *>(data_buffer.ptr);
               std::fill(data_ptr, data_ptr + data_buffer.size, fill_value);
 
-              for (size_t i = 0; i < idx_buffer.size; i++) {
+              for (py::ssize_t i = 0; i < idx_buffer.size; i++) {
                   int32_t di = idx_ptr[i];
                   if (di >= 0)
                       data_ptr[i] = rd_kw_iget_as_double(rd_kw, di);
@@ -423,7 +423,7 @@ PYBIND11_MODULE(_grid, m) {
         py::array_t<double> data(idx_buffer.size);
         auto data_ptr = data.mutable_data();
 
-        for (size_t i = 0; i < idx_buffer.size; i++) {
+        for (py::ssize_t i = 0; i < idx_buffer.size; i++) {
             int32_t g = index_ptr[i];
             data_ptr[i] = rd_grid_get_cell_volume1(rd_grid, g);
         }
@@ -439,7 +439,7 @@ PYBIND11_MODULE(_grid, m) {
         py::array_t<double> data(shape);
         auto data_ptr = data.mutable_data();
 
-        for (size_t i = 0; i < idx_buffer.size; i++) {
+        for (py::ssize_t i = 0; i < idx_buffer.size; i++) {
             int32_t g = index_ptr[i];
             size_t j = 3 * i;
             rd_grid_get_xyz1(rd_grid, g, &data_ptr[j], &data_ptr[j + 1],
@@ -462,7 +462,7 @@ PYBIND11_MODULE(_grid, m) {
         double x[8], y[8], z[8];
         size_t pos = 0;
 
-        for (size_t i = 0; i < idx_buffer.size; i++) {
+        for (py::ssize_t i = 0; i < idx_buffer.size; i++) {
             int32_t g = index_ptr[i];
             rd_grid_export_cell_corners1(rd_grid, g, x, y, z);
             for (int j = 0; j < 8; j++) {
