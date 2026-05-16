6b98fdba5a0ad5fd0c0e40eb915ebb45cc4b0aa7
Eivind Jahren <ejah@equinor.com>
2026-05-16 21:19:14 +0200

    Use RAII in rd_pack.cpp

--- applications/resdata/rd_pack.cpp
+++ applications/resdata/rd_pack.cpp
@@ -25,18 +25,28 @@
 #include <resdata/rd_util.hpp>
 #include <resdata/rd_endian_flip.hpp>
 #include <resdata/rd_type.hpp>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+bool fname_cmp(const std::string &f1, const std::string &f2) {
+    return rd_fname_report_cmp(f1.c_str(), f2.c_str()) < 0;
+}
 
 int main(int argc, char **argv) {
     int num_files = argc - 1;
     if (num_files >= 1) {
         /* File type and formatted / unformatted is determined from the first argument on the command line. */
-        char *rd_base;
-        char *path;
+        fs::path filepath(argv[1]);
+        std::string filename = filepath.string();
         rd_file_enum file_type, target_type;
         bool fmt_file;
 
         /** Look at the first command line argument to determine type and formatted/unformatted status. */
-        file_type = rd_get_file_type(argv[1], &fmt_file, NULL);
+        file_type = rd_get_file_type(filename.c_str(), &fmt_file, NULL);
         if (file_type == RD_SUMMARY_FILE)
             target_type = RD_UNIFIED_SUMMARY_FILE;
         else if (file_type == RD_RESTART_FILE)
@@ -46,64 +56,49 @@ int main(int argc, char **argv) {
                       "restart files or summary files.\n");
             target_type = RD_OTHER_FILE;
         }
-        util_alloc_file_components(argv[1], &path, &rd_base, NULL);
+        /*
+         * Will pack to cwd, even though the source files might be
+         * somewhere else. To unpack to the same directory as the source
+         * files, just send in @path as first argument when creating the
+         * target_file.
+         */
 
-        /**
-       Will pack to cwd, even though the source files might be
-       somewhere else. To unpack to the same directory as the source
-       files, just send in @path as first argument when creating the
-       target_file.
-    */
+        fs::path target_file =
+            rd::filename(filepath.stem(), target_type, fmt_file, -1);
 
-        {
-            int i, report_step, prev_report_step;
-            char *target_file_name =
-                rd_alloc_filename(NULL, rd_base, target_type, fmt_file, -1);
-            stringlist_type *filelist =
-                stringlist_alloc_argv_copy((const char **)&argv[1], num_files);
-            rd_kw_type *seqnum_kw = NULL;
-            fortio_type *target =
-                fortio_open_writer(target_file_name, fmt_file, RD_ENDIAN_FLIP);
+        std::vector<std::string> filelist(argv + 1, argv + argc);
+        std::sort(filelist.begin(), filelist.end(), fname_cmp);
 
-            if (target_type == RD_UNIFIED_RESTART_FILE) {
-                int dummy;
-                seqnum_kw = rd_kw_alloc_new("SEQNUM", 1, RD_INT, &dummy);
-            }
+        rd_kw_ptr seqnum_kw(nullptr, &rd_kw_free);
+        ERT::FortIO target(target_file, std::ios_base::out, fmt_file);
 
-            stringlist_sort(filelist, rd_fname_report_cmp);
-            prev_report_step = -1;
-            for (i = 0; i < num_files; i++) {
-                rd_file_enum this_file_type;
-                this_file_type = rd_get_file_type(stringlist_iget(filelist, i),
-                                                  NULL, &report_step);
-                if (this_file_type == file_type) {
-                    if (report_step == prev_report_step)
-                        util_exit(
-                            "Tried to write same report step twice: %s / %s \n",
-                            stringlist_iget(filelist, i - 1),
-                            stringlist_iget(filelist, i));
-
-                    prev_report_step = report_step;
-                    {
-                        rd_file_type *src_file =
-                            rd_file_open(stringlist_iget(filelist, i), 0);
-                        if (target_type == RD_UNIFIED_RESTART_FILE) {
-                            /* Must insert the SEQNUM keyword first. */
-                            rd_kw_iset_int(seqnum_kw, 0, report_step);
-                            rd_kw_fwrite(seqnum_kw, target);
-                        }
-                        rd_file_fwrite_fortio(src_file, target, 0);
-                        rd_file_close(src_file);
-                    }
-                } /* Else skipping file of incorrect type. */
-            }
-            fortio_fclose(target);
-            free(target_file_name);
-            stringlist_free(filelist);
-            if (seqnum_kw != NULL)
-                rd_kw_free(seqnum_kw);
+        if (target_type == RD_UNIFIED_RESTART_FILE) {
+            int dummy;
+            seqnum_kw.reset(rd_kw_alloc_new("SEQNUM", 1, RD_INT, &dummy));
         }
-        free(rd_base);
-        free(path);
+
+        int prev_report_step = -1;
+        for (int i = 0; i < num_files; i++) {
+            rd_file_enum this_file_type;
+            int report_step;
+            this_file_type =
+                rd_get_file_type(filelist.at(i).c_str(), NULL, &report_step);
+            if (this_file_type == file_type) {
+                if (report_step == prev_report_step)
+                    util_exit(
+                        "Tried to write same report step twice: %s / %s \n",
+                        filelist.at(i - 1).c_str(), filelist.at(i).c_str());
+
+                prev_report_step = report_step;
+                rd_file_ptr src_file(rd_file_open(filelist.at(i).c_str(), 0),
+                                     &rd_file_close);
+                if (target_type == RD_UNIFIED_RESTART_FILE) {
+                    /* Must insert the SEQNUM keyword first. */
+                    rd_kw_iset_int(seqnum_kw.get(), 0, report_step);
+                    rd_kw_fwrite(seqnum_kw.get(), target.get());
+                }
+                rd_file_fwrite_fortio(src_file.get(), target.get(), 0);
+            } /* Else skipping file of incorrect type. */
+        }
     }
 }
