38f2c8ab78c5332777eb5f1fa1a17d15b78d73c2
Eivind Jahren <ejah@equinor.com>
2026-05-16 18:23:13 +0200

    Use RAII in rd_unpack.x

--- applications/resdata/rd_unpack.cpp
+++ applications/resdata/rd_unpack.cpp
@@ -23,12 +23,16 @@
 #include <resdata/rd_kw.hpp>
 #include <resdata/rd_endian_flip.hpp>
 #include <resdata/rd_kw_magic.hpp>
+#include <filesystem>
 
-void unpack_file(const char *filename) {
+namespace fs = std::filesystem;
+
+static void unpack_file(const fs::path &filepath) {
+    std::string filename = filepath;
     rd_file_enum target_type = RD_OTHER_FILE;
     rd_file_enum file_type;
     bool fmt_file;
-    file_type = rd_get_file_type(filename, &fmt_file, NULL);
+    file_type = rd_get_file_type(filename.c_str(), &fmt_file, NULL);
     if (file_type == RD_UNIFIED_SUMMARY_FILE)
         target_type = RD_SUMMARY_FILE;
     else if (file_type == RD_UNIFIED_RESTART_FILE)
@@ -40,73 +44,50 @@ void unpack_file(const char *filename) {
         printf("** Warning: when unpacking unified summary files it as "
                "ambigous - starting with 0001  -> \n");
     }
-    {
-        rd_file_type *src_file = rd_file_open(filename, 0);
-        int size;
-        int offset;
-        int report_step = 0;
-        int block_index = 0;
-        char *path;
-        char *base;
-        util_alloc_file_components(filename, &path, &base, NULL);
+    rd_file_ptr src_file(rd_file_open(filename.c_str(), 0), &rd_file_close);
+    int size;
+    int offset;
+    int report_step = 0;
 
-        if (target_type == RD_SUMMARY_FILE)
-            size = rd_file_get_num_named_kw(src_file, "SEQHDR");
-        else
-            size = rd_file_get_num_named_kw(src_file, "SEQNUM");
+    if (target_type == RD_SUMMARY_FILE)
+        size = rd_file_get_num_named_kw(src_file.get(), "SEQHDR");
+    else
+        size = rd_file_get_num_named_kw(src_file.get(), "SEQNUM");
 
-        while (true) {
-            rd_file_view_type *active_view;
+    for (int block_index = 0; block_index < size; block_index++) {
+        rd_file_view_type *active_view;
 
-            if (block_index == size)
-                break;
-
-            if (target_type == RD_SUMMARY_FILE) {
-                active_view = rd_file_get_global_blockview(src_file, SEQHDR_KW,
-                                                           block_index);
-                report_step += 1;
-                offset = 0;
-            } else {
-                rd_kw_type *seqnum_kw;
-                active_view = rd_file_get_global_blockview(src_file, SEQNUM_KW,
-                                                           block_index);
-                seqnum_kw =
-                    rd_file_view_iget_named_kw(active_view, SEQNUM_KW, 0);
-                report_step = rd_kw_iget_int(seqnum_kw, 0);
-                offset = 1;
-            }
-
-            /**
-         Will unpack to cwd, even though the source files might be
-         somewhere else. To unpack to the same directory as the source
-         files, just send in @path as first argument when creating the
-         target_file.
-      */
-
-            {
-                char *target_file = rd_alloc_filename(NULL, base, target_type,
-                                                      fmt_file, report_step);
-                fortio_type *fortio_target =
-                    fortio_open_writer(target_file, fmt_file, RD_ENDIAN_FLIP);
-                rd_file_view_fwrite(active_view, fortio_target, offset);
-
-                fortio_fclose(fortio_target);
-                free(target_file);
-            }
-            block_index++;
+        if (target_type == RD_SUMMARY_FILE) {
+            active_view = rd_file_get_global_blockview(src_file.get(),
+                                                       SEQHDR_KW, block_index);
+            report_step += 1;
+            offset = 0;
+        } else {
+            rd_kw_type *seqnum_kw;
+            active_view = rd_file_get_global_blockview(src_file.get(),
+                                                       SEQNUM_KW, block_index);
+            seqnum_kw = rd_file_view_iget_named_kw(active_view, SEQNUM_KW, 0);
+            report_step = rd_kw_iget_int(seqnum_kw, 0);
+            offset = 1;
         }
-        rd_file_close(src_file);
-        free(path);
-        free(base);
+
+        /*
+        Will unpack to cwd, even though the source files might be
+        somewhere else. To unpack to the same directory as the source
+        files, just send in @path as first argument when creating the
+        target_file.
+        */
+
+        fs::path target_file =
+            rd::filename(filepath.stem(), target_type, fmt_file, report_step);
+        ERT::FortIO fortio_target(target_file, std::ios_base::out, fmt_file);
+        rd_file_view_fwrite(active_view, fortio_target.get(), offset);
     }
 }
 
 int main(int argc, char **argv) {
     if (argc == 1)
         util_exit("rd_unpack UNIFIED_FILE1   UNIFIED_FILE2   ...\n");
-    {
-        int iarg;
-        for (iarg = 1; iarg < argc; iarg++)
-            unpack_file(argv[iarg]);
-    }
+    for (int i = 1; i < argc; i++)
+        unpack_file(fs::path(argv[i]));
 }
