8558185f39b40726e1a33244aab85a548138980a
Eivind Jahren <ejah@equinor.com>
2026-05-17 07:52:38 +0200

    Use RAII in rd_dualp test

--- lib/resdata/tests/rd_dualp.cpp
+++ lib/resdata/tests/rd_dualp.cpp
@@ -6,27 +6,34 @@
 #include <resdata/rd_file.hpp>
 #include <resdata/rd_kw.hpp>
 #include <resdata/rd_kw_magic.hpp>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main(int argc, char **argv) {
-    const char *case_path = argv[1];
-    char *grid_file =
-        rd_alloc_filename(NULL, case_path, RD_EGRID_FILE, false, 0);
-    char *init_file =
-        rd_alloc_filename(NULL, case_path, RD_INIT_FILE, false, 0);
-    char *rst_file =
-        rd_alloc_filename(NULL, case_path, RD_RESTART_FILE, false, 0);
+    fs::path case_path(argv[1]);
+    std::string grid_file =
+        rd::filename(case_path, RD_EGRID_FILE, false, 0).string();
+    std::string init_file =
+        rd::filename(case_path, RD_INIT_FILE, false, 0).string();
+    std::string rst_file =
+        rd::filename(case_path, RD_RESTART_FILE, false, 0).string();
 
-    rd_grid_type *rd_grid = rd_grid_alloc(grid_file);
-    rd_file_type *RST_file = rd_file_open(rst_file, 0);
-    rd_file_type *INIT_file = rd_file_open(init_file, 0);
-    rd_file_type *GRID_file = rd_file_open(grid_file, 0);
+    rd_grid_ptr rd_grid = read_grid(grid_file);
+    rd_file_ptr RST_file =
+        rd_file_ptr(rd_file_open(rst_file.c_str(), 0), &rd_file_close);
+    rd_file_ptr INIT_file =
+        rd_file_ptr(rd_file_open(init_file.c_str(), 0), &rd_file_close);
+    rd_file_ptr GRID_file =
+        rd_file_ptr(rd_file_open(grid_file.c_str(), 0), &rd_file_close);
 
     {
-        rd_kw_type *actnum = rd_file_iget_named_kw(GRID_file, "ACTNUM", 0);
-        rd_kw_type *swat = rd_file_iget_named_kw(RST_file, "SWAT", 0);
-        rd_kw_type *permx = rd_file_iget_named_kw(INIT_file, "PERMX", 0);
-        int fracture_size = rd_grid_get_nactive_fracture(rd_grid);
-        int matrix_size = rd_grid_get_nactive(rd_grid);
+        rd_kw_type *actnum =
+            rd_file_iget_named_kw(GRID_file.get(), "ACTNUM", 0);
+        rd_kw_type *swat = rd_file_iget_named_kw(RST_file.get(), "SWAT", 0);
+        rd_kw_type *permx = rd_file_iget_named_kw(INIT_file.get(), "PERMX", 0);
+        int fracture_size = rd_grid_get_nactive_fracture(rd_grid.get());
+        int matrix_size = rd_grid_get_nactive(rd_grid.get());
 
         test_assert_int_equal(fracture_size + matrix_size,
                               rd_kw_get_size(swat));
@@ -38,31 +45,28 @@ int main(int argc, char **argv) {
             int matrix_index = 0;
             int fracture_index = 0;
 
-            for (gi = 0; gi < rd_grid_get_global_size(rd_grid); gi++) {
+            for (gi = 0; gi < rd_grid_get_global_size(rd_grid.get()); gi++) {
                 if (rd_kw_iget_int(actnum, gi) & CELL_ACTIVE_MATRIX) {
                     test_assert_int_equal(
-                        rd_grid_get_active_index1(rd_grid, gi), matrix_index);
+                        rd_grid_get_active_index1(rd_grid.get(), gi),
+                        matrix_index);
                     test_assert_int_equal(
-                        rd_grid_get_global_index1A(rd_grid, matrix_index), gi);
+                        rd_grid_get_global_index1A(rd_grid.get(), matrix_index),
+                        gi);
                     matrix_index++;
                 }
 
                 if (rd_kw_iget_int(actnum, gi) & CELL_ACTIVE_FRACTURE) {
                     test_assert_int_equal(
-                        rd_grid_get_active_fracture_index1(rd_grid, gi),
+                        rd_grid_get_active_fracture_index1(rd_grid.get(), gi),
                         fracture_index);
-                    test_assert_int_equal(
-                        rd_grid_get_global_index1F(rd_grid, fracture_index),
-                        gi);
+                    test_assert_int_equal(rd_grid_get_global_index1F(
+                                              rd_grid.get(), fracture_index),
+                                          gi);
                     fracture_index++;
                 }
             }
         }
     }
-
-    rd_file_close(RST_file);
-    rd_file_close(INIT_file);
-    rd_grid_free(rd_grid);
-
     exit(0);
 }
