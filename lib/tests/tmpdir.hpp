321472d5269fd3ad1d41cfb0b6cbb18ab2b67d1c
Eivind Jahren <ejah@equinor.com>
2026-05-22 15:53:55 +0200

    Reduce length of tmpdir subdirectory
    On mac you get a long tmpdir and the restartwriter does not
    accept longer than 72 characters so we have to ensure that
    it is not too long.
    
    This also resolves a number of race conditions in the Tmpdir fixture:
    
    * A TOCTOU bug from checking that the directory exists to the creation
      of the directory.
    * remove_all could leave file entry which causes create_directory to
      raise rather than return false.

--- lib/tests/tmpdir.hpp
+++ lib/tests/tmpdir.hpp
@@ -1,31 +1,67 @@
-#ifndef LIB_TESTS_TMPDIR_HPP
-#define LIB_TESTS_TMPDIR_HPP
+#pragma once
 #include <catch2/catch_message.hpp>
 #include <cstdlib>
-#include <iostream>
+#include <stdexcept>
 #include <string>
 
 #include <filesystem>
+#include <system_error>
+
 namespace fs = std::filesystem;
 
-class Tmpdir {
-public:
+// We do not expect more than single-digit subdirs,
+// so we set a maximum number to guarantee a bounded
+// loop as the fixture creates directories in a loop.
+// We set it to the max subdirs in ext3 which is the
+// most limited among reasonably new filesystems.
+constexpr int MAX_SUBDIRS = 31998;
+
+/**
+ * A tmpdir fixture that creates a temporary directory
+ * dirname. Optionally deletes the file in ~Tmpdir if
+ * delete_temporary_files is set to true.
+ */
+struct Tmpdir {
     static bool delete_temporary_files;
     fs::path dirname;
     Tmpdir() {
-        auto parentdir = fs::temp_directory_path() / "rd_test";
+        // All tmpdirs are created in parendir. We
+        // create it if it doesn't exist already
+        auto parentdir = fs::temp_directory_path() / "rdtest";
+        fs::create_directory(parentdir);
+        bool created = false;
 
-        do {
-            dirname = parentdir / std::to_string(rand());
-        } while (fs::exists(dirname));
+        // The instance of Tmpdir that has fs::create_directory
+        // return true takes ownership of it.
+        for (int i = 0; i < MAX_SUBDIRS &&
+                        !(created = fs::create_directory(
+                              dirname = (parentdir / std::to_string(i))));
+             ++i)
+            ;
 
-        fs::create_directories(dirname);
-        UNSCOPED_INFO("Writing files to " << dirname << "\n");
+        if (!created)
+            throw std::runtime_error(
+                "Tmpdir ran out of available subdir names");
     }
     ~Tmpdir() {
         if (delete_temporary_files) {
-            fs::remove_all(dirname);
+            // Rename to a garbage name first, so the original
+            // name slot is freed immediately and the slow
+            // recursive delete happens on the garbage name.
+            // rename is atomic on same-filesystem renames
+            // (POSIX) and same-directory renames (NTFS).
+            auto garbage = dirname.string() + ".garbage";
+
+            std::error_code ec;
+            fs::rename(dirname, garbage, ec);
+
+            if (!ec) {
+                fs::remove_all(garbage);
+            } else {
+                // Rename failed (shouldn't happen on same filesystem)
+                // falls back to direct removal
+                fs::remove_all(dirname);
+            }
         }
     }
 };
-#endif
