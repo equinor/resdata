ecef94de1f7ceb35ac28f92543ecf959a1ff1569
Eivind Jahren <ejah@equinor.com>
2026-05-31 20:04:06 +0200

    Make Catch2 a test only dependency
    This speeds up pip install . considerably.
    
    Also drops running c tests on windows, this was actually never done
    as it would just build and report "No tests were found!!!".

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,28 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeDeps, CMakeToolchain
 
 
 class ResdataConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    options = {"build_tests": [True, False]}
+    default_options = {"build_tests": False}
 
     def requirements(self):
         self.requires("backward-cpp/1.6")
-        self.requires("catch2/3.14.0")
         self.requires("fmt/8.0.1")
         self.requires("pybind11/3.0.1")
 
+        if self.options.build_tests:
+            self.requires("catch2/3.14.0")
+
     def configure(self):
-        self.options["catch2"].with_main = True
+        if self.options.build_tests:
+            self.options["catch2"].with_main = True
         if self.settings.os == "Macos":
             self.options["backward-cpp"].stack_details = "backtrace_symbol"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["BUILD_TESTS"] = "ON" if self.options.build_tests else "OFF"
+        tc.generate()
+        CMakeDeps(self).generate()
