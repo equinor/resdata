b4c4008598b0d9c575c9e14e2397697ab9831862
Eivind Jahren <ejah@equinor.com>
2026-06-17 09:27:58 +0200

    Use RAII for FortIO in tests/test_transactions.cpp

--- lib/resdata/tests/test_transactions.cpp
+++ lib/resdata/tests/test_transactions.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <unistd.h>
 
+#include <ios>
+
 #include <ert/util/test_util.hpp>
 #include <ert/util/util.hpp>
 #include <ert/util/test_work_area.hpp>
@@ -11,6 +13,8 @@
 #include <resdata/rd_file.hpp>
 #include <resdata/rd_file_view.hpp>
 #include <resdata/rd_kw.hpp>
+#include <resdata/rd_file_kw.hpp>
+#include <resdata/rd_type.hpp>
 
 /*
   This test is slightly awkward beacuse it tests quite internal implementation details.
@@ -21,29 +25,28 @@ void test_transaction() {
     rd::util::TestArea ta("index_testing");
     {
         const char *file_name = "data_file";
-        fortio_type *fortio =
-            fortio_open_writer(file_name, false, RD_ENDIAN_FLIP);
+        ERT::FortIO fortio(file_name, std::ios_base::out);
 
         //creating the data file
         int data_size = 10;
         rd_kw_type *kw1 = rd_kw_alloc("TEST1_KW", data_size, RD_INT);
         for (int i = 0; i < data_size; ++i)
             rd_kw_iset_int(kw1, i, 537 + i);
-        rd_kw_fwrite(kw1, fortio);
+        rd_kw_fwrite(kw1, fortio.get());
 
         data_size = 5;
         rd_kw_type *kw2 = rd_kw_alloc("TEST2_KW", data_size, RD_FLOAT);
         for (int i = 0; i < data_size; ++i)
             rd_kw_iset_float(kw2, i, 0.15 * i);
-        rd_kw_fwrite(kw2, fortio);
+        rd_kw_fwrite(kw2, fortio.get());
 
         data_size = 3;
         rd_kw_type *kw3 = rd_kw_alloc("TEST3_KW", data_size, RD_FLOAT);
         for (int i = 0; i < data_size; i++)
             rd_kw_iset_float(kw3, i, 0.45 * i);
-        rd_kw_fwrite(kw3, fortio);
+        rd_kw_fwrite(kw3, fortio.get());
+        fortio_fflush(fortio.get());
 
-        fortio_fclose(fortio);
         //finished creating data file
 
         rd_file_type *file = rd_file_open(file_name, 0);
