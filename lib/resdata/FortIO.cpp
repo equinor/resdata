27113dc843a7d5b9e969efc95da9a0eaffed1c41
Eivind Jahren <ejah@equinor.com>
2026-06-17 10:15:40 +0200

    Move FortIO method implementations to after static functions

--- lib/resdata/FortIO.cpp
+++ lib/resdata/FortIO.cpp
@@ -19,47 +19,6 @@
 
 #include <resdata/FortIO.hpp>
 
-namespace ERT {
-
-FortIO::FortIO(const std::string &filename, std::ios_base::openmode mode,
-               bool fmt_file, bool endian_flip_header) {
-    open(filename, mode, fmt_file, endian_flip_header);
-}
-
-void FortIO::open(const std::string &filename, std::ios_base::openmode mode,
-                  bool fmt_file, bool endian_flip_header) {
-    if (mode == std::ios_base::in) {
-        if (util_file_exists(filename.c_str())) {
-            fortio_type *c_ptr = fortio_open_reader(filename.c_str(), fmt_file,
-                                                    endian_flip_header);
-            m_fortio.reset(c_ptr);
-        } else
-            throw std::invalid_argument("File " + filename + " does not exist");
-    } else if (mode == std::ios_base::app) {
-        fortio_type *c_ptr =
-            fortio_open_append(filename.c_str(), fmt_file, endian_flip_header);
-        m_fortio.reset(c_ptr);
-    } else {
-        fortio_type *c_ptr =
-            fortio_open_writer(filename.c_str(), fmt_file, endian_flip_header);
-        m_fortio.reset(c_ptr);
-    }
-}
-
-void FortIO::close() {
-    if (m_fortio)
-        m_fortio.reset();
-}
-
-fortio_type *FortIO::get() const { return m_fortio.get(); }
-
-void FortIO::fflush() const { fortio_fflush(m_fortio.get()); }
-
-bool FortIO::ftruncate(offset_type new_size) {
-    return fortio_ftruncate(m_fortio.get(), new_size);
-}
-} // namespace ERT
-
 #define FORTIO_ID 345116
 
 #define READ_MODE_TXT "r"
@@ -228,6 +187,48 @@ static bool fortio_is_fortran_stream__(FILE *stream, bool endian_flip) {
     util_fseek(stream, init_pos, SEEK_SET);
     return is_fortran_stream;
 }
+
+namespace ERT {
+
+FortIO::FortIO(const std::string &filename, std::ios_base::openmode mode,
+               bool fmt_file, bool endian_flip_header) {
+    open(filename, mode, fmt_file, endian_flip_header);
+}
+
+void FortIO::open(const std::string &filename, std::ios_base::openmode mode,
+                  bool fmt_file, bool endian_flip_header) {
+    if (mode == std::ios_base::in) {
+        if (util_file_exists(filename.c_str())) {
+            fortio_type *c_ptr = fortio_open_reader(filename.c_str(), fmt_file,
+                                                    endian_flip_header);
+            m_fortio.reset(c_ptr);
+        } else
+            throw std::invalid_argument("File " + filename + " does not exist");
+    } else if (mode == std::ios_base::app) {
+        fortio_type *c_ptr =
+            fortio_open_append(filename.c_str(), fmt_file, endian_flip_header);
+        m_fortio.reset(c_ptr);
+    } else {
+        fortio_type *c_ptr =
+            fortio_open_writer(filename.c_str(), fmt_file, endian_flip_header);
+        m_fortio.reset(c_ptr);
+    }
+}
+
+void FortIO::close() {
+    if (m_fortio)
+        m_fortio.reset();
+}
+
+fortio_type *FortIO::get() const { return m_fortio.get(); }
+
+void FortIO::fflush() const { fortio_fflush(m_fortio.get()); }
+
+bool FortIO::ftruncate(offset_type new_size) {
+    return fortio_ftruncate(m_fortio.get(), new_size);
+}
+} // namespace ERT
+
 
 /**
    This function tries (using some heuristic) to guess whether a
