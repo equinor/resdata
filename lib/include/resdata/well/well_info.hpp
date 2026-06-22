cd3e684114a0eda51c2ad333923568a070cf9bcf
Eivind Jahren <ejah@equinor.com>
2026-06-22 15:08:38 +0200

    Remove extern C around well_info

--- lib/include/resdata/well/well_info.hpp
+++ lib/include/resdata/well/well_info.hpp
@@ -1,14 +1,9 @@
-#ifndef ERT_WELL_INFO_H
-#define ERT_WELL_INFO_H
+#pragma once
 
 #include <resdata/rd_file.hpp>
 #include <resdata/rd_grid.hpp>
 
 #include <resdata/well/well_ts.hpp>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 typedef struct well_info_struct well_info_type;
 
@@ -26,8 +21,3 @@ int well_info_get_num_wells(const well_info_type *well_info);
 const char *well_info_iget_well_name(const well_info_type *well_info,
                                      int well_index);
 bool well_info_has_well(well_info_type *well_info, const char *well_name);
-#ifdef __cplusplus
-}
-#endif
-
-#endif
