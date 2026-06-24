b5d68410a3101bc40eef60025ad44551cd453771
Eivind Jahren <ejah@equinor.com>
2026-06-24 07:29:57 +0200

    Add test of restart head time handling

--- tests/rd_tests/test_restart_head.py
+++ tests/rd_tests/test_restart_head.py
@@ -1,12 +1,13 @@
 import datetime
 import os.path
+import time
 
-from resdata import FileMode, ResDataType
+import hypothesis.strategies as st
+import pytest
+from hypothesis import given
+from resdata import ResDataType
 from resdata.grid import Grid
 from resdata.resfile import (
-    FortIO,
-    Resdata3DKW,
-    ResdataFile,
     ResdataKW,
     ResdataRestartFile,
 )
@@ -57,6 +58,63 @@ def test_restart_headers():
 def test_restart_headers_from_kw():
     intehead = ResdataKW("INTEHEAD", 100, ResDataType.RD_INT)
     doubhead = ResdataKW("DOUBHEAD", 100, ResDataType.RD_DOUBLE)
-    logihead = ResdataKW("DOUBHEAD", 100, ResDataType.RD_BOOL)
+    logihead = ResdataKW("LOGIHEAD", 100, ResDataType.RD_BOOL)
     header = ResdataRestartHead(kw_arg=(1, intehead, doubhead, logihead))
     assert header.get_report_step() == 1
+
+
+def restart_interpreted_date(year: int, month: int, day: int) -> datetime.datetime:
+    intehead = ResdataKW("INTEHEAD", 100, ResDataType.RD_INT)
+    intehead[64] = day
+    intehead[65] = month
+    intehead[66] = year
+    doubhead = ResdataKW("DOUBHEAD", 100, ResDataType.RD_DOUBLE)
+    logihead = ResdataKW("LOGIHEAD", 100, ResDataType.RD_BOOL)
+    header = ResdataRestartHead(kw_arg=(1, intehead, doubhead, logihead))
+    return header.get_sim_date()
+
+
+@given(
+    year=st.integers(min_value=1970, max_value=2600),
+    month=st.integers(min_value=1, max_value=12),
+    day=st.integers(min_value=1, max_value=27),
+)
+def test_that_date_roundtrips(year, month, day):
+    dt = restart_interpreted_date(year, month, day)
+    assert dt.year == year
+    assert dt.month == month
+    assert dt.day == day
+    assert dt.hour == 0
+    assert dt.second == 0
+    assert dt.microsecond == 0
+
+
+@given(
+    year=st.integers(min_value=1970, max_value=2600),
+    month=st.integers(min_value=1, max_value=12),
+    day=st.integers(min_value=1, max_value=27),
+)
+def test_that_restart_interpreted_timestamp_has_no_tzinfo(year, month, day):
+    assert restart_interpreted_date(year, month, day).tzinfo is None
+
+
+@given(
+    year=st.integers(min_value=1970, max_value=2600),
+    month=st.integers(min_value=1, max_value=12),
+    day=st.integers(min_value=1, max_value=27),
+)
+def test_that_restart_interpreted_timestamp_is_not_affected_by_local_timezone(
+    year, month, day
+):
+    try:
+        with pytest.MonkeyPatch.context() as monkeypatch:
+            monkeypatch.setenv("TZ", "America/New_York")
+            time.tzset()
+            result_tz_ny = restart_interpreted_date(year, month, day)
+
+            monkeypatch.setenv("TZ", "UTC")
+            time.tzset()
+            result_tz_utc = restart_interpreted_date(year, month, day)
+            assert result_tz_ny == result_tz_utc
+    finally:
+        time.tzset()
