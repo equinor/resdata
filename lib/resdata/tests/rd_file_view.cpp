0f532f52a6d8da10ddf2a5e7c09c3fbac87ad2ba
Eivind Jahren <ejah@equinor.com>
2026-07-08 10:15:42 +0200

    Use operator== for rd_file_kw

--- lib/resdata/tests/rd_file_view.cpp
+++ lib/resdata/tests/rd_file_view.cpp
@@ -19,12 +19,12 @@ void test_file_kw_equal() {
     rd_file_kw_type kw5 = rd_file_kw_type(66, RD_FLOAT, 10, "PRESSURE");
     rd_file_kw_type kw6 = rd_file_kw_type(67, RD_FLOAT, 1000, "PRESSURE");
 
-    test_assert_true(rd_file_kw_equal(&kw1, &kw1));
-    test_assert_true(rd_file_kw_equal(&kw1, &kw2));
-    test_assert_false(rd_file_kw_equal(&kw1, &kw3));
-    test_assert_false(rd_file_kw_equal(&kw1, &kw4));
-    test_assert_false(rd_file_kw_equal(&kw1, &kw5));
-    test_assert_false(rd_file_kw_equal(&kw1, &kw6));
+    test_assert_true(kw1 == kw1);
+    test_assert_true(kw1 == kw2);
+    test_assert_false(kw1 == kw3);
+    test_assert_false(kw1 == kw4);
+    test_assert_false(kw1 == kw5);
+    test_assert_false(kw1 == kw6);
 }
 
 void test_create_file_kw() {
@@ -48,7 +48,7 @@ void test_create_file_kw() {
             FILE *istream = util_fopen("file_kw", "r");
             auto kws = rd_file_kw_fread(istream, 1);
             rd_file_kw_type *disk_kw = kws.at(0).get();
-            test_assert_true(rd_file_kw_equal(&file_kw0, disk_kw));
+            test_assert_true(file_kw0 == *disk_kw);
 
             /* Beyond the end of stream - return NULL */
             test_assert_throw(rd_file_kw_fread(istream, 1), std::exception);
@@ -66,9 +66,9 @@ void test_create_file_kw() {
         {
             FILE *istream = util_fopen("file_kw", "r");
             auto disk_kw = rd_file_kw_fread(istream, 3);
-            test_assert_true(rd_file_kw_equal(&file_kw0, disk_kw[0].get()));
-            test_assert_true(rd_file_kw_equal(&file_kw1, disk_kw[1].get()));
-            test_assert_true(rd_file_kw_equal(&file_kw2, disk_kw[2].get()));
+            test_assert_true(file_kw0 == *disk_kw[0]);
+            test_assert_true(file_kw1 == *disk_kw[1]);
+            test_assert_true(file_kw2 == *disk_kw[2]);
 
             fclose(istream);
         }
