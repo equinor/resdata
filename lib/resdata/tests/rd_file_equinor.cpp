985b57e3e0b2886abe2eceb49bba07930fab7266
Eivind Jahren <ejah@equinor.com>
2026-07-15 13:43:24 +0200

    Remove load_all functions only used in tests

--- lib/resdata/tests/rd_file_equinor.cpp
+++ lib/resdata/tests/rd_file_equinor.cpp
@@ -11,32 +11,6 @@
 #include <resdata/rd_file_flag.hpp>
 #include <resdata/rd_kw.hpp>
 
-void test_close_stream2(const char *src_file, const char *target_file) {
-    util_copy_file(src_file, target_file);
-    auto rd_file = rd::File::open(target_file, FileMode::CLOSE_STREAM);
-
-    rd_file_load_all(rd_file.get());
-    unlink(target_file);
-    rd_kw_type *kw2 = rd_file_iget_kw(rd_file.get(), 2);
-    test_assert_not_NULL(kw2);
-}
-
-void test_loadall(const char *src_file, const char *target_file) {
-    util_copy_file(src_file, target_file);
-    {
-        auto rd_file = rd::File::open(target_file, FileMode::CLOSE_STREAM);
-
-        test_assert_true(rd_file_load_all(rd_file.get()));
-    }
-
-    {
-        auto rd_file = rd::File::open(target_file, FileMode::CLOSE_STREAM);
-        unlink(target_file);
-
-        test_assert_false(rd_file_load_all(rd_file.get()));
-    }
-}
-
 void test_close_stream1(const char *src_file, const char *target_file) {
     util_copy_file(src_file, target_file);
 
@@ -85,10 +59,8 @@ int main(int argc, char **argv) {
         rd::util::TestArea ta("file_equinor");
 
         ta.copy_file(src_file);
-        test_loadall(src_file, target_file);
 
         test_close_stream1(src_file, target_file);
-        test_close_stream2(src_file, target_file);
         test_writable(src_file);
     }
     exit(0);
