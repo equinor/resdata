176b6382df7916fd5214897d1b6b1432cde03a8c
Eivind Jahren <ejah@equinor.com>
2026-07-16 08:21:56 +0200

    Remove no longer used includes

--- lib/include/resdata/rd_file.hpp
+++ lib/include/resdata/rd_file.hpp
@@ -7,7 +7,6 @@
 #include <resdata/rd_kw.hpp>
 #include <resdata/rd_file_view.hpp>
 #include <resdata/FortIO.hpp>
-#include <resdata/rd_util.hpp>
 #include <resdata/rd_file_flag.hpp>
 #include <utility>
 
