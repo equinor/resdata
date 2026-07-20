203a110a8ebc67bdab0542abe6b6ccd5bfa5dd08
Eivind Jahren <ejah@equinor.com>
2026-07-20 15:07:59 +0200

    Avoid manual memory management in geo_pointset.cpp
    This involved changing geo_surface slightly as we needed
    to handle its loop better.

--- lib/geometry/geo_pointset.cpp
+++ lib/geometry/geo_pointset.cpp
@@ -2,113 +2,88 @@
 #include <cstdlib>
 #include <cstring>
 
-#include <ert/util/util.hpp>
+#include <stdexcept>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <optional>
 
 #include <ert/geometry/geo_pointset.hpp>
 
-#define INIT_SIZE 256
-
 struct geo_pointset_struct {
-    int size;
-    int alloc_size;
-    bool internal_z;
-
-    double *xcoord;
-    double *ycoord;
-    double *zcoord; // Can be NULL
+    std::vector<double> xcoord;
+    std::vector<double> ycoord;
+    std::optional<std::vector<double>> zcoord;
 };
 
-static void geo_pointset_resize(geo_pointset_type *pointset,
-                                int new_alloc_size) {
-    pointset->xcoord = (double *)util_realloc(
-        pointset->xcoord, new_alloc_size * sizeof *pointset->xcoord);
-    pointset->ycoord = (double *)util_realloc(
-        pointset->ycoord, new_alloc_size * sizeof *pointset->ycoord);
-    if (pointset->internal_z)
-        pointset->zcoord = (double *)util_realloc(
-            pointset->zcoord, new_alloc_size * sizeof *pointset->zcoord);
-
-    pointset->alloc_size = new_alloc_size;
-}
-
 geo_pointset_type *geo_pointset_alloc(bool internal_z) {
-    geo_pointset_type *pointset =
-        (geo_pointset_type *)util_malloc(sizeof *pointset);
-    pointset->xcoord = NULL;
-    pointset->ycoord = NULL;
-    pointset->zcoord = NULL;
-    pointset->internal_z = internal_z;
-    pointset->size = 0;
-    geo_pointset_resize(pointset, INIT_SIZE);
+    auto pointset = new geo_pointset_type();
+    if (internal_z)
+        pointset->zcoord = std::vector<double>();
     return pointset;
 }
 
 void geo_pointset_memcpy(const geo_pointset_type *src,
                          geo_pointset_type *target, bool copy_zdata) {
-    if (src->internal_z != target->internal_z)
-        util_abort("%s: when copying geo_poitset they must have equal value "
-                   "for internal_z\n",
-                   __func__);
+    if (src->zcoord.has_value() != target->zcoord.has_value())
+        throw std::invalid_argument("when copying geo_pointset they must have "
+                                    "equal value for internal_z");
 
-    geo_pointset_resize(target, src->size);
-    target->size = src->size;
-    memcpy(target->xcoord, src->xcoord, src->size * sizeof *src->xcoord);
-    memcpy(target->ycoord, src->ycoord, src->size * sizeof *src->ycoord);
+    target->xcoord.assign(src->xcoord.begin(), src->xcoord.end());
+    target->ycoord.assign(src->ycoord.begin(), src->ycoord.end());
     if (copy_zdata) {
-        if (src->internal_z)
-            memcpy(target->zcoord, src->zcoord,
-                   src->size * sizeof *src->zcoord);
+        if (!target->zcoord.has_value() || !src->zcoord.has_value()) {
+            target->zcoord = src->zcoord;
+        } else {
+            target->zcoord->assign(src->zcoord->begin(), src->zcoord->end());
+        }
+    } else {
+        if (src->zcoord.has_value())
+            target->zcoord = std::vector(src->zcoord->size(), 0.0);
         else
-            util_abort("%s: can not pass copy_zdata = true for pointset with "
-                       "shared z\n",
-                       __func__);
-    } else if (target->internal_z) {
-        for (int i = 0; i < target->size; i++)
-            target->zcoord[i] = 0;
+            target->zcoord = std::nullopt;
     }
 }
 
 void geo_pointset_add_xyz(geo_pointset_type *pointset, double x, double y,
                           double z) {
-    if (pointset->size == pointset->alloc_size)
-        geo_pointset_resize(pointset, 1 + pointset->alloc_size * 2);
 
-    pointset->xcoord[pointset->size] = x;
-    pointset->ycoord[pointset->size] = y;
-    if (pointset->internal_z)
-        pointset->zcoord[pointset->size] = z;
-
-    pointset->size++;
+    pointset->xcoord.push_back(x);
+    pointset->ycoord.push_back(y);
+    if (pointset->zcoord.has_value())
+        pointset->zcoord->push_back(z);
 }
 
-void geo_pointset_free(geo_pointset_type *pointset) {
-    free(pointset->xcoord);
-    free(pointset->ycoord);
-    free(pointset->zcoord);
-    free(pointset);
-}
+void geo_pointset_free(geo_pointset_type *pointset) { delete pointset; }
 
 int geo_pointset_get_size(const geo_pointset_type *pointset) {
-    return pointset->size;
+    return pointset->xcoord.size();
 }
 
-const double *geo_pointset_get_zcoord(const geo_pointset_type *pointset) {
-    return pointset->zcoord;
+const std::vector<double> &
+geo_pointset_get_zcoord(const geo_pointset_type *pointset) {
+    if (!pointset->zcoord)
+        throw std::runtime_error("z coordinate not set");
+    return *pointset->zcoord;
 }
 
 static void geo_pointset_assert_index(const geo_pointset_type *pointset,
                                       int index) {
-    if ((index < 0) || (index >= pointset->size))
-        util_abort("%s: invalid pointset index. Size:%d \n", __func__, index);
+    if ((index < 0) || (static_cast<size_t>(index) >= pointset->xcoord.size()))
+        throw std::out_of_range(
+            "invalid pointset index " + std::to_string(index) +
+            ", size: " + std::to_string(pointset->xcoord.size()));
 }
 
 static void geo_pointset_assert_zindex(const geo_pointset_type *pointset,
                                        int index) {
-    if ((index < 0) || (index >= pointset->size))
-        util_abort("%s: invalid pointset index. Size:%d \n", __func__, index);
+    if ((index < 0) || (static_cast<size_t>(index) >= pointset->xcoord.size()))
+        throw std::out_of_range(
+            "invalid pointset index " + std::to_string(index) +
+            ", size: " + std::to_string(pointset->xcoord.size()));
 
-    if (pointset->zcoord == NULL)
-        util_abort("%s: z coordinate not set\n", __func__);
+    if (!pointset->zcoord)
+        throw std::runtime_error("z coordinate not set");
 }
 
 void geo_pointset_iget_xy(const geo_pointset_type *pointset, int index,
@@ -120,84 +95,89 @@ void geo_pointset_iget_xy(const geo_pointset_type *pointset, int index,
 
 double geo_pointset_iget_z(const geo_pointset_type *pointset, int index) {
     geo_pointset_assert_zindex(pointset, index);
-    return pointset->zcoord[index];
+    return (*pointset->zcoord)[index];
 }
 
 void geo_pointset_iset_z(geo_pointset_type *pointset, int index, double value) {
     geo_pointset_assert_zindex(pointset, index);
-    pointset->zcoord[index] = value;
+    auto &zs = pointset->zcoord.value();
+    zs[index] = value;
 }
 
 bool geo_pointset_equal(const geo_pointset_type *pointset1,
                         const geo_pointset_type *pointset2) {
-    bool equal = false;
-
-    if (pointset1->size == pointset2->size) {
-        size_t byte_size = pointset2->size * sizeof *pointset2->xcoord;
-        if ((memcmp(pointset1->xcoord, pointset2->xcoord, byte_size) == 0) &&
-            (memcmp(pointset1->ycoord, pointset2->ycoord, byte_size) == 0)) {
-
-            if (pointset1->internal_z == pointset2->internal_z) {
-                if (pointset1->zcoord) {
-                    if (memcmp(pointset1->zcoord, pointset2->zcoord,
-                               byte_size) == 0)
-                        equal = true;
-                } else
-                    equal = true;
-            }
-        }
-    }
-    return equal;
+    return std::tie(pointset1->xcoord, pointset1->ycoord, pointset1->zcoord) ==
+           std::tie(pointset2->xcoord, pointset2->ycoord, pointset2->zcoord);
 }
 
 void geo_pointset_assign_z(geo_pointset_type *pointset, double value) {
-    for (int i = 0; i < pointset->size; i++)
-        pointset->zcoord[i] = value;
+    if (!pointset->zcoord.has_value())
+        return;
+    auto &zs = pointset->zcoord.value();
+    for (double &z : zs)
+        z = value;
 }
 
 void geo_pointset_shift_z(geo_pointset_type *pointset, double value) {
-    for (int i = 0; i < pointset->size; i++)
-        pointset->zcoord[i] += value;
+    if (!pointset->zcoord.has_value())
+        return;
+    auto &zs = pointset->zcoord.value();
+    for (double &z : zs)
+        z += value;
 }
 
 void geo_pointset_scale_z(geo_pointset_type *pointset, double value) {
-    for (int i = 0; i < pointset->size; i++)
-        pointset->zcoord[i] *= value;
+    if (!pointset->zcoord.has_value())
+        return;
+    auto &zs = pointset->zcoord.value();
+    for (double &z : zs)
+        z *= value;
 }
 
 void geo_pointset_iadd(geo_pointset_type *self,
                        const geo_pointset_type *other) {
-    int index;
-    if (self->size == other->size) {
-        for (index = 0; index < self->size; index++)
-            self->zcoord[index] += other->zcoord[index];
+    if (self->xcoord.size() == other->xcoord.size()) {
+        if (!self->zcoord || !other->zcoord)
+            return;
+        auto &self_zs = self->zcoord.value();
+        auto &other_zs = other->zcoord.value();
+        for (size_t index = 0; index < self_zs.size(); index++)
+            self_zs[index] += other_zs[index];
     } else
-        util_abort("%s: size mismatch \n", __func__);
+        throw std::invalid_argument("size mismatch");
 }
 
 void geo_pointset_isub(geo_pointset_type *self,
                        const geo_pointset_type *other) {
-    int index;
-    if (self->size == other->size) {
-        for (index = 0; index < self->size; index++)
-            self->zcoord[index] -= other->zcoord[index];
+    if (self->xcoord.size() == other->xcoord.size()) {
+        if (!self->zcoord || !other->zcoord)
+            return;
+        auto &self_zs = self->zcoord.value();
+        auto &other_zs = other->zcoord.value();
+        for (size_t index = 0; index < self_zs.size(); index++)
+            self_zs[index] -= other_zs[index];
     } else
-        util_abort("%s: size mismatch \n", __func__);
+        throw std::invalid_argument("size mismatch");
 }
 
 void geo_pointset_imul(geo_pointset_type *self,
                        const geo_pointset_type *other) {
-    int index;
-    if (self->size == other->size) {
-        for (index = 0; index < self->size; index++)
-            self->zcoord[index] *= other->zcoord[index];
+    if (self->xcoord.size() == other->xcoord.size()) {
+        if (!self->zcoord || !other->zcoord)
+            return;
+        auto &self_zs = self->zcoord.value();
+        auto &other_zs = other->zcoord.value();
+        for (size_t index = 0; index < self_zs.size(); index++)
+            self_zs[index] *= other_zs[index];
     } else
-        util_abort("%s: size mismatch \n", __func__);
+        throw std::invalid_argument("size mismatch");
 }
 
 void geo_pointset_isqrt(geo_pointset_type *pointset) {
-    for (int i = 0; i < pointset->size; i++)
-        pointset->zcoord[i] = sqrt(pointset->zcoord[i]);
+    if (!pointset->zcoord.has_value())
+        return;
+    for (size_t i = 0; i < pointset->zcoord->size(); i++)
+        (*pointset->zcoord)[i] = sqrt((*pointset->zcoord)[i]);
 }
 
 geo_pointset_ptr make_geo_pointset(bool external_z) {
