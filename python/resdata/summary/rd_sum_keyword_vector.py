97f7646686f8cdc48ee726062cb411b340c952b0
eivind <ejah@equinor.com>
2026-07-24 13:12:00 +0200

    Use pybind for rd_sum_vector

--- python/resdata/summary/rd_sum_keyword_vector.py
+++ python/resdata/summary/rd_sum_keyword_vector.py
@@ -5,25 +5,14 @@
 
 from cwrap import BaseCClass
 
-from resdata import ResdataPrototype
+import resdata.summary._rd_sum_keyword_vector as _rd_sum_keyword_vector
 
 
 class SummaryKeyWordVector(BaseCClass):
     TYPE_NAME = "rd_sum_vector"
-    _alloc = ResdataPrototype("void* rd_sum_vector_alloc(rd_sum, bool)", bind=False)
-    _alloc_copy = ResdataPrototype(
-        "rd_sum_vector_obj rd_sum_vector_alloc_layout_copy(rd_sum_vector, rd_sum)"
-    )
-    _free = ResdataPrototype("void rd_sum_vector_free(rd_sum_vector)")
-    _add = ResdataPrototype("bool rd_sum_vector_add_key(rd_sum_vector,  char*)")
-    _add_multiple = ResdataPrototype(
-        "void rd_sum_vector_add_keys(rd_sum_vector,  char*)"
-    )
-    _get_size = ResdataPrototype("int rd_sum_vector_get_size(rd_sum_vector)")
-    _iget_key = ResdataPrototype("char* rd_sum_vector_iget_key(rd_sum_vector, int)")
 
     def __init__(self, rd_sum, add_keywords=False):
-        c_pointer = self._alloc(rd_sum, add_keywords)
+        c_pointer = _rd_sum_keyword_vector._alloc(rd_sum, add_keywords)
         super().__init__(c_pointer)
 
     def __getitem__(self, index):
@@ -33,24 +22,26 @@ class SummaryKeyWordVector(BaseCClass):
         if index >= len(self):
             raise IndexError("Out of range")
 
-        return self._iget_key(index)
+        return _rd_sum_keyword_vector._iget_key(self, index)
 
     def __len__(self):
-        return self._get_size()
+        return _rd_sum_keyword_vector._get_size(self)
 
     def free(self):
-        self._free()
+        _rd_sum_keyword_vector._free(self)
 
     def add_keyword(self, keyword):
-        success = self._add(keyword)
+        success = _rd_sum_keyword_vector._add(self, keyword)
         if not success:
             raise KeyError("Failed to add keyword to vector")
 
     def add_keywords(self, keyword_pattern):
-        self._add_multiple(keyword_pattern)
+        _rd_sum_keyword_vector._add_multiple(self, keyword_pattern)
 
     def __repr__(self):
         return self._create_repr("len=%d" % len(self))
 
     def copy(self, rd_sum):
-        return self._alloc_copy(rd_sum)
+        return SummaryKeyWordVector.createPythonObject(
+            _rd_sum_keyword_vector._alloc_copy(self, rd_sum)
+        )
