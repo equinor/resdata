cb7d5795dc33439d894747aec13d9a4f574200ff
eivind <ejah@equinor.com>
2026-07-24 13:53:29 +0200

    Consistently use optional<string> for region name

--- lib/tests/test_rd_region.cpp
+++ lib/tests/test_rd_region.cpp
@@ -44,9 +44,9 @@ TEST_CASE("rd_region", "[rd_region]") {
 
         SECTION("Set and get name") {
             rd_region_set_name(region, "TestRegion");
-            const char *name = rd_region_get_name(region);
-            REQUIRE(name != NULL);
-            REQUIRE(std::string(name) == "TestRegion");
+            auto name = rd_region_get_name(region);
+            REQUIRE(name.has_value());
+            REQUIRE(*name == "TestRegion");
         }
 
         SECTION("Reset region") {
