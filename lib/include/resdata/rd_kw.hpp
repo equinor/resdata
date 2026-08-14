77cd53e7701ddb94e179c84f2feb6702aed79251
eivind <ejah@equinor.com>
2026-08-14 08:54:37 +0200

    Add missing includes in rd_kw.hpp

--- lib/include/resdata/rd_kw.hpp
+++ lib/include/resdata/rd_kw.hpp
@@ -7,6 +7,8 @@
 #include <string>
 
 #include <ert/util/type_macros.hpp>
+#include <ert/util/int_vector.hpp>
+#include <ert/util/util.hpp>
 
 #include <resdata/FortIO.hpp>
 #include <resdata/rd_util.hpp>
