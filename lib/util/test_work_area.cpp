a518c9ae008005e61e5968a602bbcdf4517287c8
eivind <ejah@equinor.com>
2026-08-25 14:24:01 +0200

    Replace util_fread_dev_urandom with std::rand
    The only user of util_fread_dev_urandom was create_test_path in
    test_work_area, which needs an arbitrary suffix for a scratch
    directory rather than cryptographically secure randomness.

--- lib/util/test_work_area.cpp
+++ lib/util/test_work_area.cpp
@@ -1,3 +1,13 @@
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+#include <system_error>
+#include <filesystem>
+#include <random>
+
 #include <ert/util/ert_api_config.hpp>
 
 #ifdef ERT_HAVE_GETUID
@@ -6,12 +16,6 @@
 #include <pwd.h>
 #include <paths.h>
 #endif
-
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-
-#include <filesystem>
 
 #include <ert/util/util.hpp>
 #include <ert/util/test_work_area.hpp>
@@ -130,13 +134,13 @@ static bool test_work_area_copy_parent__(const TestArea *work_area,
         return false;
     }
 }
+static std::mt19937 generator(std::random_device{}());
 
 static char *create_test_path(const std::string &test_name, const char *prefix,
                               const char *user_name) {
     const int MAX_TRIES = 10;
     for (int try_count = 0; try_count < MAX_TRIES; try_count++) {
-        unsigned int random_int;
-        util_fread_dev_urandom(sizeof random_int, (char *)&random_int);
+        unsigned int random_int = static_cast<unsigned int>(generator());
         random_int = random_int % 100000000;
 
         char *test_path = util_alloc_sprintf(TEST_PATH_FMT, user_name,
