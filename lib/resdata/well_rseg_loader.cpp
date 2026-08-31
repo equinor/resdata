1bf354e89564447230ca77320037bdb53e54cca1
eivind <ejah@equinor.com>
2026-08-31 11:41:09 +0200

    Use vector<int> for fload_indexed

--- lib/resdata/well_rseg_loader.cpp
+++ lib/resdata/well_rseg_loader.cpp
@@ -1,6 +1,9 @@
+#include <array>
 #include <cstdlib>
+#include <memory>
+#include <vector>
+
 #include <ert/util/util.hpp>
-#include <ert/util/int_vector.hpp>
 
 #include <resdata/rd_file_view.hpp>
 #include <resdata/rd_kw_magic.hpp>
@@ -11,57 +14,44 @@
 
 struct well_rseg_loader_struct {
     rd::FileView *rst_view;
-    int_vector_type *relative_index_map;
-    int_vector_type *absolute_index_map;
-    char *buffer;
+    std::vector<int> relative_index_map;
+    std::vector<int> absolute_index_map;
+    std::array<char, 4 * sizeof(double)> buffer;
     const char *kw;
 };
 
 well_rseg_loader_type *well_rseg_loader_alloc(rd::FileView *rst_view) {
-    well_rseg_loader_type *loader =
-        (well_rseg_loader_type *)util_malloc(sizeof *loader);
-
-    int element_count = 4;
+    std::unique_ptr<well_rseg_loader_type, decltype(&well_rseg_loader_free)>
+        loader{new well_rseg_loader_type, &well_rseg_loader_free};
 
     loader->rst_view = rst_view;
-    loader->relative_index_map = int_vector_alloc(0, 0);
-    loader->absolute_index_map = int_vector_alloc(0, 0);
-    loader->buffer = (char *)util_malloc(element_count * sizeof(double));
     loader->kw = RSEG_KW;
 
-    int_vector_append(loader->relative_index_map, RSEG_DEPTH_INDEX);
-    int_vector_append(loader->relative_index_map, RSEG_LENGTH_INDEX);
-    int_vector_append(loader->relative_index_map, RSEG_TOTAL_LENGTH_INDEX);
-    int_vector_append(loader->relative_index_map, RSEG_DIAMETER_INDEX);
+    loader->relative_index_map.push_back(RSEG_DEPTH_INDEX);
+    loader->relative_index_map.push_back(RSEG_LENGTH_INDEX);
+    loader->relative_index_map.push_back(RSEG_TOTAL_LENGTH_INDEX);
+    loader->relative_index_map.push_back(RSEG_DIAMETER_INDEX);
 
-    return loader;
+    return loader.release();
 }
 
 void well_rseg_loader_free(well_rseg_loader_type *loader) {
 
     if (loader->rst_view->has_flags(FileMode::CLOSE_STREAM))
         loader->rst_view->close();
-
-    int_vector_free(loader->relative_index_map);
-    int_vector_free(loader->absolute_index_map);
-    free(loader->buffer);
-    free(loader);
+    delete loader;
 }
 
-double *well_rseg_loader_load_values(const well_rseg_loader_type *loader,
+double *well_rseg_loader_load_values(well_rseg_loader_type *loader,
                                      int rseg_offset) {
-    int_vector_type *index_map = loader->absolute_index_map;
-
-    int index = 0;
-    int_vector_resize(index_map, int_vector_size(loader->relative_index_map),
-                      0);
-    for (index = 0; index < int_vector_size(loader->relative_index_map);
-         index++) {
-        int relative_index = int_vector_iget(loader->relative_index_map, index);
-        int_vector_iset(index_map, index, relative_index + rseg_offset);
+    loader->absolute_index_map.resize(loader->relative_index_map.size(), 0);
+    for (size_t index = 0; index < loader->relative_index_map.size(); index++) {
+        int relative_index = loader->relative_index_map[index];
+        loader->absolute_index_map[index] = relative_index + rseg_offset;
     }
 
-    loader->rst_view->index_fload_kw(loader->kw, 0, index_map, loader->buffer);
+    loader->rst_view->index_fload_kw(loader->kw, 0, loader->absolute_index_map,
+                                     loader->buffer.data());
 
-    return (double *)loader->buffer;
+    return (double *)loader->buffer.data();
 }
