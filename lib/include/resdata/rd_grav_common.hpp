cf2ead01c6849c43c4d94aea478888ada79d1d16
eivind <ejah@equinor.com>
2026-09-01 07:46:15 +0200

    Remove extern "C" around rd_grav_common header

--- lib/include/resdata/rd_grav_common.hpp
+++ lib/include/resdata/rd_grav_common.hpp
@@ -1,12 +1,6 @@
-#ifndef ERT_RD_GRAV_COMMON_H
-#define ERT_RD_GRAV_COMMON_H
-
+#pragma once
 #include <resdata/rd_file.hpp>
 #include "detail/resdata/rd_grid_cache.hpp"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 bool *rd_grav_common_alloc_aquifer_cell(const rd::rd_grid_cache &grid_cache,
                                         const rd::File *init_file);
@@ -22,9 +16,3 @@ double rd_grav_common_eval_geertsma(const rd::rd_grid_cache &grid_cache,
                                     const double *weight, double utm_x,
                                     double utm_y, double depth,
                                     double poisson_ratio, double seabed);
-
-#ifdef __cplusplus
-}
-
-#endif
-#endif
